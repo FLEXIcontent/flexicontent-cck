b981e58cd2ab5eba52ba4f199f633a20541da091
emmanuel.danan <emmanuel.danan@gmail.com>
2010-04-10 13:15:34 +0000

    # something i missed in the items view... strange :-(

--- components/com_flexicontent/views/items/view.html.php
+++ components/com_flexicontent/views/items/view.html.php
@@ -96,12 +96,14 @@ class FlexicontentViewItems extends JView
 			return JError::raiseError( 404, JText::sprintf( 'MANU ITEM #%d NOT FOUND', $id ) );
 		}
 		
-		$fields			= & $this->get( 'Extrafields' );
+		$fields		=& $item->fields;
 		// Add html to field object trought plugins
+/*
 		foreach ($fields as $field)
 		{
 			$results = $dispatcher->trigger('onDisplayField', array( &$field, $row ));
 		}
+*/
 
 		// Pathway need to be improved
 		$cats		= new flexicontent_cats($cid);
