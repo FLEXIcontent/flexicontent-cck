109f6f45d6b6b95d2d8fbb12d0b2cf59b606c3ce
emmanuel.danan <emmanuel.danan@82e9b126-fb80-11de-b07f-13ea4a3b38a3>
2010-06-25 13:14:56 +0000

    + Add the sh404sef plugin to the trunk (missed some files)

---
+++ trunk/components/com_flexicontent/sef_ext/lang/com_flexicontent.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @version 1.0 $Id: com_quickfaq.php 137 2008-08-04 20:57:18Z schlu $
+ * @package Joomla
+ * @subpackage QuickFAQ sh404sef plugin
+ * @copyright (C) 2005 - 2008 Christoph Lukes
+ * @license GNU/GPL, see LICENSE.php
+ * QuickFAQ is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+
+ * QuickFAQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with QuickFAQ; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+// no direct access
+defined ( '_JEXEC' ) or die ( 'Restricted access' );
+
+// english
+$sh_LANG ['en'] ['_SH404SEF_FLEXICONTENT_FAVOURITES'] = 'My favourites';
+$sh_LANG ['en'] ['_SH404SEF_FLEXICONTENT'] = 'FAQ';
+$sh_LANG ['en'] ['_SH404SEF_FLEXICONTENT_EDIT'] = 'Edit';
+$sh_LANG ['en'] ['_SH404SEF_FLEXICONTENT_ADD'] = 'Add';
+
+// french
+$sh_LANG ['fr'] ['_SH404SEF_FLEXICONTENT_FAVOURITES'] = 'Mes favoris';
+$sh_LANG ['fr'] ['_SH404SEF_FLEXICONTENT'] = 'Annuaire';
+$sh_LANG ['fr'] ['_SH404SEF_FLEXICONTENT_EDIT'] = 'Editer';
+$sh_LANG ['fr'] ['_SH404SEF_FLEXICONTENT_ADD'] = 'Ajouter';
+
+// german
+$sh_LANG ['de'] ['_SH404SEF_FLEXICONTENT_FAVOURITES'] = 'Meine Lesezeichen';
+$sh_LANG ['de'] ['_SH404SEF_FLEXICONTENT'] = 'FAQ';
+$sh_LANG ['de'] ['_SH404SEF_FLEXICONTENT_EDIT'] = 'bearbeiten';
+$sh_LANG ['de'] ['_SH404SEF_FLEXICONTENT_ADD'] = 'hinzufügen';
+?>
