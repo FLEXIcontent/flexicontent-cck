5e5c697d4ca54d20ca1414ee0d6ab6f1deb7cfad
emmanuel.danan <emmanuel.danan@gmail.com>
2010-06-27 07:06:46 +0000

    # Bug: Last bugs on the sh404sef plugin ;) Now it seems to be perfect

--- components/com_flexicontent/sef_ext/com_flexicontent.php
+++ components/com_flexicontent/sef_ext/com_flexicontent.php
@@ -21,19 +21,14 @@ defined ( '_JEXEC' ) or die ( 'Restricted access' );
 
 // ------------------  standard plugin initialize function - don't change ---------------------------
 
-
 global $sh_LANG, $sefConfig, $globalcats;
-
-//dump($globalcats,'$globalcats');
 
 $shLangName 	= '';
 $shLangIso 		= '';
 $title 			= array ( );
 $shItemidString = '';
 $dosef	 		= shInitializePlugin ( $lang, $shLangName, $shLangIso, $option );
-$layout 		= JRequest::getString('layout', '');
-$typeid 		= JRequest::getString('typeid', '');
-$task 			= JRequest::getString('task', '');
+$layout 		= JRequest::getVar('layout', '');
 
 if ($dosef == false) {
 	return;
@@ -48,75 +43,76 @@ $shLangIso = shLoadPluginLanguage ( 'com_flexicontent', $shLangIso, '_SH404SEF_F
 
 // ------------------  /load language file - adjust as needed ----------------------------------------
 
-$view 			= isset ( $view ) ? @$view : null;
-$Itemid 		= isset ( $Itemid ) ? @$Itemid : null;
+$view 		= isset ($view) ? @$view : null;
+$Itemid		= isset ($Itemid) ? @$Itemid : null;
+$task 		= isset($task) ? @$task : null;
 
-
-switch ( $view) {
+switch ($view) {
 	
 	case 'items' :
-		
-if ($typeid || $task == 'download' || $task == 'weblink') {
-	$dosef = false;
-	return;
-} else {
-/*
-	shRemoveFromGETVarsList ( 'id' );
-	shRemoveFromGETVarsList ( 'cid' );
-	shRemoveFromGETVarsList ( 'task' );
-	shRemoveFromGETVarsList ( 'option' );
-	shRemoveFromGETVarsList ( 'lang' );
-	shRemoveFromGETVarsList ( 'view' );
-	shRemoveFromGETVarsList ( 'layout' );
-	shRemoveFromGETVarsList ( 'typeid' );
-*/
-}
 
-		isset ( $task ) ? $task : $task = '';
+		if (!empty($id)) {
 		
-		if (!empty($id) && !empty($cid) && empty($task) ) {
+			if (!$task) {
 			
-			$query	= 'SELECT i.id, i.title, c.title AS cattitle, ty.alias AS typealias'
-					. ' FROM #__content AS i'
-					. ' LEFT JOIN #__flexicontent_items_ext AS ie ON ie.item_id = i.id'
-					. ' LEFT JOIN #__flexicontent_types AS ty ON ie.type_id = ty.id'
-					. ' LEFT JOIN #__flexicontent_cats_item_relations AS rel ON rel.itemid = i.id'
-					. ' LEFT JOIN #__categories AS c ON c.id = rel.catid'
-					. ' WHERE i.id = ' . ( int ) $id;
-			$database->setQuery ( $query );
-			
-			if (shTranslateURL ( $option, $shLangName )) {
-				$row = $database->loadObject ();
-			} else {
+				$query	= 'SELECT i.id, i.title, c.title AS cattitle, ty.alias AS typealias'
+						. ' FROM #__content AS i'
+						. ' LEFT JOIN #__flexicontent_items_ext AS ie ON ie.item_id = i.id'
+						. ' LEFT JOIN #__flexicontent_types AS ty ON ie.type_id = ty.id'
+						. ' LEFT JOIN #__flexicontent_cats_item_relations AS rel ON rel.itemid = i.id'
+						. ' LEFT JOIN #__categories AS c ON c.id = rel.catid'
+						. ' WHERE i.id = ' . ( int ) $id;
+				$database->setQuery ( $query );
+
+				// Do not translate the items url
 				$row = $database->loadObject ( null, false );
+/*
+				if (shTranslateURL ( $option, $shLangName )) {
+					$row = $database->loadObject ();
+				} else {
+					$row = $database->loadObject ( null, false );
+				}
+*/
+				
+				if ($database->getErrorNum ()) {
+					die ( $database->stderr () );
+				} elseif ($row) {
+					
+					if ($row->title && !empty($cid)) {
+						if ($globalcats[$cid]->ancestorsarray) {
+							$ancestors = $globalcats[$cid]->ancestorsarray;
+							foreach ($ancestors as $ancestor) {
+								if (shTranslateURL ( $option, $shLangName )) {
+									$query	= 'SELECT id, title, alias FROM #__categories WHERE id = ' . $ancestor;
+									$database->setQuery ( $query );
+									$row = $database->loadObject ();
+									$title[] = $row->title . '/';
+								} else {
+									$title[] = $globalcats[$ancestor]->title . '/';
+								}
+							}		
+						}
+						
+						$title [] = $row->title;
+					}
+				}	
+			
+			} elseif ($task == 'edit') {
+				$title [] = $sh_LANG[$shLangIso]['_SH404SEF_FLEXICONTENT_EDIT'];
 			}
 			
-			if ($database->getErrorNum ()) {
-				die ( $database->stderr () );
-			} elseif ($row) {
-				if ($row->title) {
-					if ($globalcats[$cid]->ancestorsarray) {
-						$ancestors = $globalcats[$cid]->ancestorsarray;
-						foreach ($ancestors as $ancestor) {
-							$title[] = $globalcats[$ancestor]->title . '/';
-						}		
-					}
-					
-					$title [] = $row->title;
-				}
-			}
-
-		shRemoveFromGETVarsList ( 'id' );
-		shRemoveFromGETVarsList ( 'cid' );
-//		shRemoveFromGETVarsList ( 'task' );
-		shRemoveFromGETVarsList ( 'option' );
-		shRemoveFromGETVarsList ( 'lang' );
-		shRemoveFromGETVarsList ( 'view' );
-		shRemoveFromGETVarsList ( 'layout' );
-//		shRemoveFromGETVarsList ( 'typeid' );
-
+		} else {
+			$title [] = $sh_LANG[$shLangIso]['_SH404SEF_FLEXICONTENT_ADD'];
 		}
-		
+
+		// Remove the vars from the url
+		if (!empty($id))
+			shRemoveFromGETVarsList ( 'id' );
+		if (!empty($cid))
+			shRemoveFromGETVarsList ( 'cid' );
+		if (!empty($layout))
+			shRemoveFromGETVarsList ( 'layout' );
+
 	break;
 	
 	case 'category' :
@@ -126,17 +122,24 @@ if ($typeid || $task == 'download' || $task == 'weblink') {
 			if ($globalcats[$cid]->ancestorsarray) {
 				$ancestors = $globalcats[$cid]->ancestorsarray;
 				foreach ($ancestors as $ancestor) {
-					$title[] = $globalcats[$ancestor]->title . '/';
+					if (shTranslateURL ( $option, $shLangName )) {
+						$query	= 'SELECT id, title, alias FROM #__categories WHERE id = ' . $ancestor;
+						$database->setQuery ( $query );
+						$row = $database->loadObject ();
+						$title[] = $row->title . '/';
+					} else {
+						$title[] = $globalcats[$ancestor]->title . '/';
+					}
 				}		
 			} else {
 				$title [] = '/';
 			}
 		}
 
-		shRemoveFromGETVarsList ( 'option' );
-		shRemoveFromGETVarsList ( 'lang' );
-		shRemoveFromGETVarsList ( 'view' );
-		shRemoveFromGETVarsList ( 'cid' );
+		// Remove the vars from the url
+		if (!empty($cid))
+			shRemoveFromGETVarsList ( 'cid' );
+
 	break;
 	
 	case 'tags' :
@@ -155,44 +158,62 @@ if ($typeid || $task == 'download' || $task == 'weblink') {
 				die ( $database->stderr () );
 			} elseif ($row) {
 				if ($row->name) {
+					$title [] = $sh_LANG[$shLangIso]['_SH404SEF_FLEXICONTENT_TAGS'] . '/';
 					$title [] = $row->name;
 				}
 			}
 		} else {
 			$title [] = '/';
 		}
-		shRemoveFromGETVarsList ( 'id' );
+
+		// Remove the vars from the url
+		if (!empty($id))
+			shRemoveFromGETVarsList ( 'id' );
+
 	break;
 	
 	case 'favourites' :
 		$title [] = $sh_LANG[$shLangIso]['_SH404SEF_FLEXICONTENT_FAVOURITES'] .  '/';
-shRemoveFromGETVarsList ( 'option' );
-shRemoveFromGETVarsList ( 'lang' );
-shRemoveFromGETVarsList ( 'view' );
 	break;
 
-	
 	case 'flexicontent' :
 		$title [] = $sh_LANG [$shLangIso] ['_SH404SEF_FLEXICONTENT'] . '/';
-shRemoveFromGETVarsList ( 'option' );
-shRemoveFromGETVarsList ( 'lang' );
-shRemoveFromGETVarsList ( 'view' );
 	break;
 
 	
 	default :
-//		$title [] = '/';
+		$title [] = '/';
 	break;
 }
 
+if ($task == 'download') {
+	$title [] = $sh_LANG[$shLangIso]['_SH404SEF_FLEXICONTENT_DOWNLOAD'];
+}
 
-if (! empty ( $Itemid ))
+if ($task == 'weblink') {
+	$title [] = $sh_LANG[$shLangIso]['_SH404SEF_FLEXICONTENT_WEBLINK'];
+}
+
+// Remove the remaining common vars from the url
+if (!empty($option))
+	shRemoveFromGETVarsList ( 'option' );
+
+if (!empty($task))
+	shRemoveFromGETVarsList ( 'task' );
+
+if (!empty($lang))
+	shRemoveFromGETVarsList ( 'lang' );
+
+if (!empty($view))
+	shRemoveFromGETVarsList ( 'view' );
+
+if (!empty($Itemid))
 	shRemoveFromGETVarsList ( 'Itemid' );
 
-if (! empty ( $limit ))
+if (!empty($limit))
 	shRemoveFromGETVarsList ( 'limit' );
 
-if (isset ( $limitstart ))
+if (isset($limitstart))
 	shRemoveFromGETVarsList ( 'limitstart' ); // limitstart can be zero
 	
 // ------------------  standard plugin finalize function - don't change ---------------------------  
