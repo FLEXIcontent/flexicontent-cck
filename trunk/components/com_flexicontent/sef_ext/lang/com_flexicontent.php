fadede2d7f49046ec405258e2a297b9661f09b7c
emmanuel.danan <emmanuel.danan@82e9b126-fb80-11de-b07f-13ea4a3b38a3>
2010-06-27 07:06:46 +0000

    # Bug: Last bugs on the sh404sef plugin ;) Now it seems to be perfect

--- trunk/components/com_flexicontent/sef_ext/lang/com_flexicontent.php
+++ trunk/components/com_flexicontent/sef_ext/lang/com_flexicontent.php
@@ -21,14 +21,23 @@ defined ( '_JEXEC' ) or die ( 'Restricted access' );
 
 // english
 $sh_LANG ['en'] ['_SH404SEF_FLEXICONTENT_FAVOURITES'] = 'My favourites';
+$sh_LANG ['en'] ['_SH404SEF_FLEXICONTENT_TAGS'] = 'Tags';
 $sh_LANG ['en'] ['_SH404SEF_FLEXICONTENT'] = 'Directory';
 $sh_LANG ['en'] ['_SH404SEF_FLEXICONTENT_EDIT'] = 'Edit';
 $sh_LANG ['en'] ['_SH404SEF_FLEXICONTENT_ADD'] = 'Add';
+$sh_LANG ['en'] ['_SH404SEF_FLEXICONTENT_DOWNLOAD'] = 'Download';
+$sh_LANG ['en'] ['_SH404SEF_FLEXICONTENT_WEBLINK'] = 'Weblink';
 
 // french
 $sh_LANG ['fr'] ['_SH404SEF_FLEXICONTENT_FAVOURITES'] = 'Mes favoris';
+$sh_LANG ['fr'] ['_SH404SEF_FLEXICONTENT_TAGS'] = 'Tags';
 $sh_LANG ['fr'] ['_SH404SEF_FLEXICONTENT'] = 'Annuaire';
 $sh_LANG ['fr'] ['_SH404SEF_FLEXICONTENT_EDIT'] = 'Editer';
 $sh_LANG ['fr'] ['_SH404SEF_FLEXICONTENT_ADD'] = 'Ajouter';
+$sh_LANG ['fr'] ['_SH404SEF_FLEXICONTENT_DOWNLOAD'] = 'Télécharger';
+$sh_LANG ['fr'] ['_SH404SEF_FLEXICONTENT_WEBLINK'] = 'Lien web';
+
+
+
 
 ?>
