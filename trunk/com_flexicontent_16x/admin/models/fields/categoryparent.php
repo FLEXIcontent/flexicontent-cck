15d70fb955c39b430443b3040796bdd1d159a9d2
enjoyman <enjoyman@82e9b126-fb80-11de-b07f-13ea4a3b38a3>
2010-10-27 09:37:37 +0000

    fixed a bug when display category tree.

--- trunk/com_flexicontent_16x/admin/models/fields/categoryparent.php
+++ trunk/com_flexicontent_16x/admin/models/fields/categoryparent.php
@@ -30,6 +30,71 @@ class JFormFieldCategoryParent extends JFormFieldList
 	protected $type = 'CategoryParent';
 
 	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return	string	The field input markup.
+	 * @since	1.6
+	 */
+	protected function getInput()
+	{
+		// Initialize variables.
+		$html = array();
+		$attr = '';
+
+		// Initialize some field attributes.
+		$attr .= $this->element['class'] ? ' class="'.(string) $this->element['class'].'"' : '';
+
+		// To avoid user's confusion, readonly="true" should imply disabled="true".
+		if ( (string) $this->element['readonly'] == 'true' || (string) $this->element['disabled'] == 'true') {
+			$attr .= ' disabled="disabled"';
+		}
+
+		$attr .= $this->element['size'] ? ' size="'.(int) $this->element['size'].'"' : '';
+		$attr .= $this->multiple ? ' multiple="multiple"' : '';
+
+		// Initialize JavaScript field attributes.
+		$attr .= $this->element['onchange'] ? ' onchange="'.(string) $this->element['onchange'].'"' : '';
+
+		// Get the field options.
+		$options = (array) $this->getOptions();
+
+		// Create a read-only list (no name) with a hidden input to store the value.
+		if ((string) $this->element['readonly'] == 'true') {
+			//$html[] = JHtml::_('select.genericlist', $options, '', trim($attr), 'value', 'text', $this->value, $this->id);
+			
+			$html[] = '<select name="" '.trim($attr).'>';
+			foreach($options as $opt) {
+				$disabled = '';
+				$selected = '';
+				if( @$opt->disable )
+					$disabled = ' disabled="disabled"';
+				if($opt->value==$this->value)
+					$selected = ' selected="selected"';
+				$html[] = '<option value="'.$opt->value.'"'.$disabled.$selected.'>'.$opt->text.'</option>';
+			}
+			$html[] = '</select>';
+			$html[] = '<input type="hidden" name="'.$this->name.'" value="'.$this->value.'"/>';
+		}
+		// Create a regular list.
+		else {
+			//$html[] = JHtml::_('select.genericlist', $options, $this->name, trim($attr), 'value', 'text', $this->value, $this->id);
+			$html[] = '<select name="'.$this->name.'" '.trim($attr).'>';
+			foreach($options as $opt) {
+				$disabled = '';
+				$selected = '';
+				if( @$opt->disable )
+					$disabled = ' disabled="disabled"';
+				if($opt->value==$this->value)
+					$selected = ' selected="selected"';
+				$html[] = '<option value="'.$opt->value.'"'.$disabled.$selected.'>'.$opt->text.'</option>';
+			}
+			$html[] = '</select>';
+		}
+
+		return implode("\n", $html);
+	}
+	
+	/**
 	 * Method to get the field options.
 	 *
 	 * @return	array	The field option objects.
@@ -103,8 +168,7 @@ class JFormFieldCategoryParent extends JFormFieldList
 		}
 
 		// Merge any additional options in the XML definition.
-		//$catlist = array_merge(parent::getOptions(), $catlist);
-
+		$catlist = array_merge(parent::getOptions(), $catlist);
 		return $catlist;
 	}
 }
