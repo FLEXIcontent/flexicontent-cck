158241d3bca22feccd094c4f1a53fc2a93539fb3
emmanuel.danan@gmail.com <emmanuel.danan@gmail.com>
2011-03-05 09:28:33 +0000

    + Add a canonical tag to avoid duplicate contents with multi-mapping

--- components/com_flexicontent/views/items/view.html.php
+++ components/com_flexicontent/views/items/view.html.php
@@ -173,6 +173,12 @@ class FlexicontentViewItems extends JView
 			}
 		}
 		
+		// @TODO chack that as it seems to be dirty :(
+		$uri  			=& JFactory::getURI();
+		$base 			= $uri->getScheme() . '://' . $uri->getHost();
+		$ucanonical 	= $base . JRoute::_(FlexicontentHelperRoute::getItemRoute($item->slug, $item->categoryslug));
+		$document->addHeadLink( $ucanonical, 'canonical', 'rel', '' );
+		
 		$limitstart	= JRequest::getVar('limitstart', 0, '', 'int');
 		
 		// increment the hit counter
