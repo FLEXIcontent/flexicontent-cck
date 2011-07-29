c435198aa4d1eef6508c445c2329effe7127abe9
ggppdk <ggppdk@82e9b126-fb80-11de-b07f-13ea4a3b38a3>
2011-07-29 04:54:03 +0000

    # Fix for filters not appearing always in category view ...

--- trunk/components/com_flexicontent/views/category/view.html.php
+++ trunk/components/com_flexicontent/views/category/view.html.php
@@ -266,7 +266,7 @@ class FlexicontentViewCategory extends JView
 			foreach ($filters as $filtre)
 			{
 				$value		= $mainframe->getUserStateFromRequest( $option.'.category'.$category->id.'.filter_'.$filtre->id, 'filter_'.$filtre->id, '', 'cmd' );
-				JPluginHelper::importPlugin('flexicontent_fields', $filtre->field_type);
+				JPluginHelper::importPlugin('flexicontent_fields', ($filtre->iscore ? 'core' : $filtre->field_type) );
 				$results 	= $dispatcher->trigger('onDisplayFilter', array( &$filtre, $value ));
 				$lists['filter_' . $filtre->id] = $value;
 			}
