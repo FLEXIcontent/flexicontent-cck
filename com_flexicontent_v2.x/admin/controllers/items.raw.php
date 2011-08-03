78718f211506304bead84c7565c7b81fabc0dbad
enjoyman@gmail.com <enjoyman@gmail.com>
2011-08-03 15:30:14 +0000

    #forgot add items raw file.

---
+++ com_flexicontent_v2.x/admin/controllers/items.raw.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @version 1.5 stable $Id: items.php 171 2010-03-20 00:44:02Z emmanuel.danan $
+ * @package Joomla
+ * @subpackage FLEXIcontent
+ * @copyright (C) 2009 Emmanuel Danan - www.vistamedia.fr
+ * @license GNU/GPL v2
+ * 
+ * FLEXIcontent is a derivative work of the excellent QuickFAQ component
+ * @copyright (C) 2008 Christoph Lukes
+ * see www.schlu.net for more information
+ *
+ * FLEXIcontent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport('joomla.application.component.controller');
+
+/**
+ * FLEXIcontent Component Item Controller
+ *
+ * @package Joomla
+ * @subpackage FLEXIcontent
+ * @since 1.0
+ */
+class FlexicontentControllerItems extends JController {
+	/**
+	 * Constructor
+	 *
+	 * @since 1.0
+	 */
+	function __construct() {
+		parent::__construct();
+	}
+
+	/**
+	 * Method to fetch the tags form
+	 * 
+	 * @since 1.5
+	 */
+	function getorphans()
+	{
+		$model 		=  $this->getModel('items');
+		$status 	=  $model->getExtdataStatus();
+
+		echo count($status['no']);
+	}
+}
