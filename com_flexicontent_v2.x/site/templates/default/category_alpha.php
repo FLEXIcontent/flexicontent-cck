7fe28afc5c146872b6a02485e4823acb7a7f33dd
Georgios Papadakis <ggppdk@gmail.com>
2011-08-16 08:18:53 +0000

    # Added more features to FC 2.0 Alpha Index 1. Added support for Default AlphaIndex per Language 2. Implemented character ranges using regular expression in the MySQL query 3. Added separator 4. Show alpha index now has 3 options , hide,   Show use language default,  Show use custom characters 5. Use of character # for  0-9 numbers 6. Option to Skip disabled characters / ranges 7. Some css fixes

--- com_flexicontent_v2.x/site/templates/default/category_alpha.php
+++ com_flexicontent_v2.x/site/templates/default/category_alpha.php
@@ -16,58 +16,125 @@
  * GNU General Public License for more details.
  */
 defined( '_JEXEC' ) or die( 'Restricted access' );
-$alphacharacters = $this->params->get('alphacharacters', "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,y,z|0,1,2,3,4,5,6,7,8,9");
-$groups = explode("|", $alphacharacters);
+
+function utf8ord($char) {
+	$i = 0;
+	$number = '';
+	while (isset($char{$i})) {
+		$number.= ord($char{$i});
+		++$i;
+	}
+	return $number;
+}
+
+$show_alpha = $this->params->get('show_alpha',1);
+if ($show_alpha == 1) {
+	// Language Default
+	$alphacharacters = JTEXT::_("FLEXI_ALPHA_INDEX_CHARACTERS");
+	$groups = explode("|", $alphacharacters);
+	$groupcssclasses = explode("|", JTEXT::_("FLEXI_ALPHA_INDEX_CSSCLASSES"));
+	$alphacharsep = JTEXT::_("FLEXI_ALPHA_INDEX_SEPARATOR");
+} else {  // $show_alpha == 2
+	// Custom setting
+	$alphacharacters = $this->params->get('alphacharacters', "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,y,z|0,1,2,3,4,5,6,7,8,9");
+	$groups = explode("|", $alphacharacters);
+	$groupcssclasses = explode("|", $this->params->get('alphagrpcssclasses'));
+	$alphacharsep = $this->params->get('alphacharseparator','');
+}
+$alphaskipempty = $this->params->get('alphaskipempty',0);
+
+// a. Trim classes names
+foreach ($groupcssclasses as $i => $grpcssclass) {
+	$groupcssclasses[$i] = trim($grpcssclass);
+}
+// b. Check for empty first value, means initial string was empty ... and set empty array
+if ($groupcssclasses[0]=='') $groupcssclasses = array();
+// c. Set missing classes to class 'letters'
+for($i=count($groupcssclasses); $i<count($groups); $i++) {
+	$groupcssclasses[$i] = 'letters';
+}
 ?>
 
 <div id="fc_alpha">
 	<?php
 	$flag = true;
+	$i=0;
 	foreach($groups as $group) {
 		$letters = explode(",", $group);
 	?>
-	<div class="letters">
+	<div class="<?php echo $groupcssclasses[$i]; ?>">
 	<?php if($flag) {?>
 	<a class="fc_alpha_index" href="#" onclick="document.getElementById('alpha_index').value='';document.getElementById('adminForm').submit();"><?php echo JText::_('FLEXI_ALL'); ?></a>
 	<?php $flag = false;}?>
 	<?php
 		foreach ($letters as $letter) :
+			// a. Skip on empty $letter (2 commas ,,)
 			$letter = trim($letter);
-			if (in_array($letter, $this->alpha)) :
-				echo "<a class=\"fc_alpha_index\" href=\"#\" onclick=\"document.getElementById('alpha_index').value='".$letter."';document.getElementById('adminForm').submit();\">".strtoupper($letter)."</a>";
-			else :
-				echo "<span class=\"fc_alpha_index\">".strtoupper($letter)."</span>";
+			if ($letter==='') continue;
+			
+			// b. Check for ALIASes
+			$letter_label = $letter;
+			if ($letter==='#' ) {
+				$letter = "0-9";
+			}
+			
+			// c. Try to get range of characters
+			$range = explode("-", $letter);
+			
+			// d. Check if character exists 
+			if(count($range)==1) {
+				// ERROR CHECK: String has single character
+				if (mb_strlen($letter) > 1) {
+					echo "Error in Alpha Index please correct letter: ".$letter." must have only one character<br>";
+					continue;
+				}
+				// Check Single character exists
+				$has_item = in_array($letter, $this->alpha);
+			} else {
+				// ERROR CHECK: Character range has only one minus(-)
+				if (count($range) != 2) {
+					echo "Error in Alpha Index please correct letter range: ".$letter."<br>";
+					continue;
+				}
+				
+				// Get range characters
+				$startletter = $range[0];  $endletter = $range[1];
+				
+				// ERROR CHECK: Range START and END are single character strings
+				if (mb_strlen($startletter) > 1 || mb_strlen($endletter) > 1) {
+					echo "Error in Alpha Index please correct letter range: ".$letter." start and end must be one character<br>";
+					continue;
+				}
+				
+				// Get rangle length
+				$range_length = utf8ord($endletter) - utf8ord($startletter);
+				
+				// ERROR CHECK: Character range has at least one character
+				if ($range_length > 200 || $range_length < 0) {
+					// A sanity check that the range is something logical and that 
+					echo "Error in Alpha Index, letter range: ".$letter.", is incorrect or contains more that 200 characters<br>";
+				}
+				// Check at that range has at least on character
+				for($ch=$startletter; $ch<=$endletter; $ch++) :
+					if (in_array($ch, $this->alpha)) {
+						$has_item = true;
+						break;
+					}
+				endfor;
+			}
+			
+			if ($alphacharsep) $aiclass = "fc_alpha_index_sep";
+			else $aiclass = "fc_alpha_index";
+			if ($has_item) :
+				if ($alphacharsep) echo "<span class=\"fc_alpha_index_sep\">$alphacharsep</span>";
+				echo "<a class=\"$aiclass\" href=\"#\" onclick=\"document.getElementById('alpha_index').value='".$letter."'; document.getElementById('adminForm').submit();\">".strtoupper($letter_label)."</a>";
+			elseif (!$alphaskipempty) :
+				if ($alphacharsep) echo "<span class=\"fc_alpha_index_sep\">$alphacharsep</span>";
+				echo "<span class=\"$aiclass\">".strtoupper($letter_label)."</span>";
 			endif;
 		endforeach;
 	?>
 	</div>
 	<?php
-	}
-	/*
-	?>
-	
-		<a class="fc_alpha_index" href="#" onclick="document.getElementById('alpha_index').value='';document.getElementById('adminForm').submit();"><?php echo JText::_('FLEXI_ALL'); ?></a>
-		<?php 
-		$letters = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-		foreach ($letters as $letter) :
-			if (in_array($letter, $this->alpha)) :
-				echo "<a class=\"fc_alpha_index\" href=\"#\" onclick=\"document.getElementById('alpha_index').value='".$letter."';document.getElementById('adminForm').submit();\">".strtoupper($letter)."</a>";
-			else :
-				echo "<span class=\"fc_alpha_index\">".strtoupper($letter)."</span>";
-			endif;
-		endforeach;
-		?>
-	</div>
-	<div class="numbers">
-		<?php 
-		$numbers = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
-		foreach ($numbers as $number) :
-			if (in_array($number, $this->alpha)) :
-				echo "<a class=\"fc_alpha_index\" href=\"".$this->action."&letter=".$number."\" onclick=\"document.getElementById('alpha_index').value='".($number ? $number : 0)."';document.getElementById('adminForm').submit();\">".$number."</a>";
-			else :
-				echo "<span class=\"fc_alpha_index\">".$number."</span>";
-			endif;
-		endforeach;
-		?>
-	</div><?php */ ?>
+	}?>
 </div>
