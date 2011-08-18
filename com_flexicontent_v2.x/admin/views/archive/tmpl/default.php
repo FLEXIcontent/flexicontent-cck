67f9e465c7acb025af6281c7d688338f3473408d
Georgios Papadakis <ggppdk@gmail.com>
2011-08-18 09:13:46 +0000

    # Fixed bug in archive manager, redirect to items manager on any user action

--- com_flexicontent_v2.x/admin/views/archive/tmpl/default.php
+++ com_flexicontent_v2.x/admin/views/archive/tmpl/default.php
@@ -123,7 +123,7 @@ defined('_JEXEC') or die('Restricted access'); ?>
 	<input type="hidden" name="boxchecked" value="0" />
 	<input type="hidden" name="option" value="com_flexicontent" />
 	<input type="hidden" name="controller" value="archive" />
-	<input type="hidden" name="view" value="items" />
+	<input type="hidden" name="view" value="archive" />
 	<input type="hidden" name="task" value="" />
 	<input type="hidden" name="filter_order" value="<?php echo $this->lists['order']; ?>" />
 	<input type="hidden" name="filter_order_Dir" value="<?php echo $this->lists['order_Dir']; ?>" />
