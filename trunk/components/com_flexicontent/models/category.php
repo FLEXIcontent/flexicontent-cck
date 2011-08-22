3d68f5c5828b9f2e6b841d3494debdfbf1cd03f5
enjoyman@gmail.com <enjoyman@gmail.com@82e9b126-fb80-11de-b07f-13ea4a3b38a3>
2011-08-22 10:44:15 +0000

    #change utf8_[function name] to be JString::[functio name]

--- trunk/components/com_flexicontent/models/category.php
+++ trunk/components/com_flexicontent/models/category.php
@@ -416,15 +416,15 @@ class FlexicontentModelCategory extends JModel{
 		$range = explode("-", $alpha);
 		
 		$regexp='';
-		if (utf8_strlen($alpha)==0) {
+		if (JString::strlen($alpha)==0) {
 			// nothing to do
 		} else if (count($range) > 2) {
 			echo "Error in Alpha Index please correct letter range: ".$alpha."<br>";
 		} else if (count($range) == 1) {
 			
-			$regexp = '"^('.utf8_substr($alpha,0,1);
-			for($i=1; $i<utf8_strlen($alpha); $i++) :
-				$regexp .= '|'.utf8_substr($alpha,$i,1);
+			$regexp = '"^('.JString::substr($alpha,0,1);
+			for($i=1; $i<JString::strlen($alpha); $i++) :
+				$regexp .= '|'.JString::substr($alpha,$i,1);
 			endfor;
 			$regexp .= ')"';
 			
@@ -434,7 +434,7 @@ class FlexicontentModelCategory extends JModel{
 			$startletter = $range[0];  $endletter = $range[1];
 			
 			// ERROR CHECK: Range START and END are single character strings
-			if (utf8_strlen($startletter) != 1 || utf8_strlen($endletter) != 1) {
+			if (JString::strlen($startletter) != 1 || JString::strlen($endletter) != 1) {
 				echo "Error in Alpha Index<br>letter range: ".$alpha." start and end must be one character<br>";
 				return $where;
 			}
