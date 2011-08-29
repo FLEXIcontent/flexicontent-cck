a98c95bea34a52118b991f9fe40e026a8733ae2c
Georgios Papadakis <ggppdk@gmail.com>
2011-08-29 08:09:47 +0000

    # Fixed SEF Tag links (Both FC ver), and fixed date field display in FC v2.0

--- com_flexicontent_v2.x/plugins/date/date.php
+++ com_flexicontent_v2.x/plugins/date/date.php
@@ -230,7 +230,8 @@ class plgFlexicontent_fieldsDate extends JPlugin
 			// We must use timezone offset ZERO, because the date(-time) value is stored in its final value
 			// AND NOT as GMT-0 which would need to be converted to localtime, if not specified the JHTML-date
 			// will convert to local time using a timezone offset, giving erroneous output
-			$field->{$prop}[]	= $values[$n] ? JHTML::_('date', $values[$n], JText::_($dateformat), $timezone_offset=0 ) : JText::_( 'FLEXI_NO_VALUE' );
+			// J1.6+  CANNOT USE 0 as $timezone_offset, removed it ...
+			$field->{$prop}[]	= $values[$n] ? JHTML::_('date', $values[$n], JText::_($dateformat)/*, $timezone_offset=0*/ ) : JText::_( 'FLEXI_NO_VALUE' );
 			$n++;
 		}
 		$field->{$prop} = implode($separatorf, $field->{$prop});	
