4de2be14ee422fab64ca70c214970a50e13fc692
ggppdk <ggppdk@82e9b126-fb80-11de-b07f-13ea4a3b38a3>
2011-09-02 15:21:59 +0000

    # Fixed Article Page Navigation (fcpagenav plugin) (both FC) to (a) display iterate through all items of current category (b) it will always stay inside current category (c) respect the Global configuration parameter of filtering language (display only current language items) # Fixed mod_flexicontent to respect "read more" parameter for non-featured items

--- trunk/plugins/flexicontent_fields/radioimage.php
+++ trunk/plugins/flexicontent_fields/radioimage.php
@@ -77,7 +77,7 @@ class plgFlexicontent_fieldsRadioimage extends JPlugin
 		if ($item->version < 2 && $default_value) {
 			$field->value = array();
 			$field->value[0] = $default_value;
-		} elseif (!$field->value[0]) {
+		} elseif ( !isset($field->value[0]) ) {
 			$field->value = array();
 			$field->value[0] = '';
 		}
