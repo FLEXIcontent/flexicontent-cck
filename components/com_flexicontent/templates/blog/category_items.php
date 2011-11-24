2cb09ad7910de284ff1db834a887f45f4dc6d2de
Georgios Papadakis <ggppdk@gmail.com>
2011-11-24 02:56:50 +0000

    # (a) Fixed flexicontent multilanguage postinstall task (J1.5), (b) Fixed blog template to display images for leading items when using custom field as image (they were not created) (J1.5)

--- components/com_flexicontent/templates/blog/category_items.php
+++ components/com_flexicontent/templates/blog/category_items.php
@@ -103,6 +103,7 @@ if ($this->limitstart == 0) :
     			<?php 
     			if ($this->params->get('lead_use_image', 1)) :
     				if ($this->params->get('lead_image')) :
+    					FlexicontentFields::getFieldDisplay($items[$i], $this->params->get('lead_image'), $values=null, $method='display');
 							if (isset($items[$i]->fields[$this->params->get('lead_image')]->value[0])) :
 								$dir{$i}	= $items[$i]->fields[$this->params->get('lead_image')]->parameters->get('dir');
 								$value{$i} 	= unserialize($items[$i]->fields[$this->params->get('lead_image')]->value[0]);
