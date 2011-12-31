da465e1bfd3b977bf176d052ee44ea23088879b4
enjoyman@gmail.com <enjoyman@gmail.com>
2011-12-31 14:02:09 +0000

    +add a feature, import items by csv file.

---
+++ administrator/components/com_flexicontent/views/import/tmpl/import.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * @version 1.5 stable $Id: copy.php 171 2010-03-20 00:44:02Z emmanuel.danan $
+ * @package Joomla
+ * @subpackage FLEXIcontent
+ * @copyright (C) 2009 Emmanuel Danan - www.vistamedia.fr
+ * @license GNU/GPL v2
+ * 
+ * FLEXIcontent is a derivative work of the excellent QuickFAQ component
+ * @copyright (C) 2008 Christoph Lukes
+ * see www.schlu.net for more information
+ *
+ * FLEXIcontent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+defined('_JEXEC') or die('Restricted access');
+?>
+<script>
+function submitbutton(task) {
+	if(task=='cancel') {
+		submitform(task);
+		return;
+	}
+	if(!$("type_id").value) {
+		alert("Please select item type.");
+		$("type_id").focus();
+		return;
+	}
+	var isselected = false;
+	for(i=0;i<$('seccats').length;i++) {
+		if($('seccats')[i].selected) {
+			isselected = true;
+			break;
+		}
+	};
+	if(($("maincat").value.length<=0) && !isselected) {
+		alert("Please select primary category, or secondary at lease one category.");
+		$("maincat").focus();
+		return;
+	}
+	if($("csvfile").value=="") {
+		alert("Please select your csv file that you want to import.");
+		$("csvfile").focus();
+		return;
+	}
+	submitform(task);
+}
+</script>
+<div class="flexicontent">
+<form action="index.php" method="post" enctype="multipart/form-data" name="adminForm" id="adminForm">
+	<table cellspacing="10" cellpadding="0" border="0" width="100%">
+		<tr>
+			<td value="top" colspan="2">
+				<label for="type_id">
+					<?php echo JText::_("FLEXI_ITEM_TYPE");?><span style="color:red;">*</span>
+				</label>
+				<?php echo $this->lists['type_id'];?>
+			</td>
+		</tr>
+		<tr>
+			<td valign="top" width="50%">
+			<fieldset>
+			<legend><?php echo JText::_( 'FLEXI_CATEGORY' ); ?></legend>
+				<table>
+					<tr valign="top">
+						<td class="key"><?php echo JText::_( 'FLEXI_PRIMARY_CATEGORY' ); ?><!--span style="color:red;">*</span!---></td>
+						<td>
+							<?php echo $this->lists['maincat']; ?>
+						</td>
+						<td class="key"><?php echo JText::_( 'FLEXI_SECONDARY_CATEGORIES' ); ?></td>
+						<td><?php echo $this->lists['seccats']; ?></td>
+					</tr>
+				</table>
+			</fieldset>
+			</td>
+		</tr>
+		<tr>
+			<td valign="top">
+			<fieldset>
+			<legend><?php echo JText::_( 'FLEXI_IMPORT_FILE' ); ?></legend>
+				<table>
+					<tr>
+						<td class="key">
+							<?php echo JText::_( 'FLEXI_CSVFILE' ); ?>
+							<span style="color:red;">*</span>
+						</td>
+						<td>
+							<input type="file" name="csvfile" id="csvfile" value="" />
+						</td>
+					</tr>
+				</table>
+			</fieldset>
+			</td>
+		</tr>
+	</table>
+	<input type="hidden" name="option" value="com_flexicontent" />
+	<input type="hidden" name="controller" value="items" />
+	<input type="hidden" name="view" value="items" />
+	<input type="hidden" name="task" value="" />
+	<?php echo JHTML::_( 'form.token' ); ?>
+</form>
+</div>
--- administrator/components/com_flexicontent/views/items/view.html.php
+++ administrator/components/com_flexicontent/views/items/view.html.php
@@ -325,7 +325,7 @@ class FlexicontentViewItems extends JView {
 		
 		JHTML::_('behavior.tooltip');
 
-		//add css and submenu to document
+		//add css to document
 		$document->addStyleSheet('components/com_flexicontent/assets/css/flexicontentbackend.css');
 
 		//add js functions
@@ -337,45 +337,14 @@ class FlexicontentViewItems extends JView {
 
 		if (FLEXI_ACCESS) {
 			$user =& JFactory::getUser();
-			$CanAdd 		= ($user->gid < 25) ? (FAccess::checkComponentAccess('com_content', 'submit', 'users', $user->gmid) || FAccess::checkAllContentAccess('com_content','add','users',$user->gmid,'content','all')) : 1;
-			$CanEdit 		= ($user->gid < 25) ? (FAccess::checkComponentAccess('com_content', 'edit', 'users', $user->gmid) || FAccess::checkComponentAccess('com_content', 'editown', 'users', $user->gmid)) : 1;
-			$CanPublish 	= ($user->gid < 25) ? (FAccess::checkComponentAccess('com_content', 'publish', 'users', $user->gmid) || FAccess::checkComponentAccess('com_content', 'publishown', 'users', $user->gmid)) : 1;
-			$CanDelete 		= ($user->gid < 25) ? (FAccess::checkComponentAccess('com_content', 'delete', 'users', $user->gmid) || FAccess::checkComponentAccess('com_content', 'deleteown', 'users', $user->gmid)) : 1;
 			$CanCats 		= ($user->gid < 25) ? FAccess::checkComponentAccess('com_flexicontent', 'categories', 'users', $user->gmid) : 1;
-			$CanTypes 		= ($user->gid < 25) ? FAccess::checkComponentAccess('com_flexicontent', 'types', 'users', $user->gmid) : 1;
-			$CanFields 		= ($user->gid < 25) ? FAccess::checkComponentAccess('com_flexicontent', 'fields', 'users', $user->gmid) : 1;
-			$CanTags 		= ($user->gid < 25) ? FAccess::checkComponentAccess('com_flexicontent', 'tags', 'users', $user->gmid) : 1;
-			$CanArchives 	= ($user->gid < 25) ? FAccess::checkComponentAccess('com_flexicontent', 'archives', 'users', $user->gmid) : 1;
-			$CanFiles	 	= ($user->gid < 25) ? FAccess::checkComponentAccess('com_flexicontent', 'files', 'users', $user->gmid) : 1;
-			$CanStats	 	= ($user->gid < 25) ? FAccess::checkComponentAccess('com_flexicontent', 'stats', 'users', $user->gmid) : 1;
 			$CanRights	 	= ($user->gid < 25) ? FAccess::checkComponentAccess('com_flexiaccess', 'manage', 'users', $user->gmid) : 1;
 			$CanOrder	 	= ($user->gid < 25) ? FAccess::checkComponentAccess('com_flexicontent', 'order', 'users', $user->gmid) : 1;
 		} else {
-			$CanAdd 		= 1;
-			$CanEdit		= 1;
-			$CanPublish		= 1;
-			$CanDelete		= 1;
 			$CanCats 		= 1;
-			$CanTypes 		= 1;
-			$CanFields		= 1;
-			$CanTags 		= 1;
-			$CanArchives	= 1;
-			$CanFiles		= 1;
-			$CanStats		= 1;
 			$CanRights		= 1;
 			$CanOrder		= 1;
 		}
-
-		//Create Submenu
-		JSubMenuHelper::addEntry( JText::_( 'FLEXI_HOME' ), 'index.php?option=com_flexicontent');
-		JSubMenuHelper::addEntry( JText::_( 'FLEXI_ITEMS' ), 'index.php?option=com_flexicontent&view=items', true);
-		if ($CanTypes)		JSubMenuHelper::addEntry( JText::_( 'FLEXI_TYPES' ), 'index.php?option=com_flexicontent&view=types');
-		if ($CanCats) 		JSubMenuHelper::addEntry( JText::_( 'FLEXI_CATEGORIES' ), 'index.php?option=com_flexicontent&view=categories');
-		if ($CanFields) 	JSubMenuHelper::addEntry( JText::_( 'FLEXI_FIELDS' ), 'index.php?option=com_flexicontent&view=fields');
-		if ($CanTags) 		JSubMenuHelper::addEntry( JText::_( 'FLEXI_TAGS' ), 'index.php?option=com_flexicontent&view=tags');
-		if ($CanArchives) 	JSubMenuHelper::addEntry( JText::_( 'FLEXI_ARCHIVE' ), 'index.php?option=com_flexicontent&view=archive');
-		if ($CanFiles) 		JSubMenuHelper::addEntry( JText::_( 'FLEXI_FILEMANAGER' ), 'index.php?option=com_flexicontent&view=filemanager');
-		if ($CanStats)		JSubMenuHelper::addEntry( JText::_( 'FLEXI_STATISTICS' ), 'index.php?option=com_flexicontent&view=stats');
 
 		//create the toolbar
 		JToolBarHelper::title( JText::_( 'FLEXI_COPYMOVE_ITEM' ), 'itemadd' );
