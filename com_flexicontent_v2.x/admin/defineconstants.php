014dc6e127235acf5549f4bc229025d3b23c6c71
Georgios Papadakis <ggppdk@gmail.com>
2012-02-24 03:52:20 +0000

    # version number changed to RC3 r1151

--- com_flexicontent_v2.x/admin/defineconstants.php
+++ com_flexicontent_v2.x/admin/defineconstants.php
@@ -49,5 +49,5 @@ if (!defined('FLEXI_ICONPATH'))		define('FLEXI_ICONPATH', 'media/system/images/'
 
 // Version constants
 define('FLEXI_VERSION',	'2.0');
-define('FLEXI_RELEASE',	'preRC3 (r1110)');
+define('FLEXI_RELEASE',	'RC3 (r1151)');
 ?>
