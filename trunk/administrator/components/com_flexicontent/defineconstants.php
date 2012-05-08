cb7c3e947c5db6abd420eb071ab6aa111652753a
ggppdk <ggppdk@82e9b126-fb80-11de-b07f-13ea4a3b38a3>
2012-05-08 01:51:53 +0000

    + (a) Added popup graphical selection of master language item for J1.5, and fixed this for J2.5, (b) some form field corrections and styling improvements

--- trunk/administrator/components/com_flexicontent/defineconstants.php
+++ trunk/administrator/components/com_flexicontent/defineconstants.php
@@ -71,5 +71,5 @@ if (!defined('FLEXI_ICONPATH'))		define('FLEXI_ICONPATH', 'images/M_images/');
 
 // Version constants
 define('FLEXI_VERSION',	'1.5.6');
-define('FLEXI_RELEASE',	'RC5 (r1264)');
+define('FLEXI_RELEASE',	'RC5 (r1269)');
 ?>
