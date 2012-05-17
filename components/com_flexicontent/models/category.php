18c9b7791238b19c9a66aca54acfabbd83141321
Georgios Papadakis <ggppdk@gmail.com>
2012-05-17 17:36:46 +0000

    # Some minor fixes (a) missing language file for related items backlinks field (b) a fix for category view while counting assigned items, (c) some untranslated strings fixed

--- components/com_flexicontent/models/category.php
+++ components/com_flexicontent/models/category.php
@@ -808,7 +808,8 @@ class FlexicontentModelCategory extends JModel {
 		$where .= ' AND ( i.state IN ('.$states.') OR i.created_by = '.$user->id.' OR ( i.modified_by = '.$user->id.' AND i.modified_by != 0 ) )';
 		
 		// Count items according to full depth level !!!
-		$where .= ' AND rel.catid IN ('.$globalcats[$id]->descendants.')';
+		$catlist = !empty($globalcats[$id]->descendants) ? $globalcats[$id]->descendants : $id;
+		$where .= ' AND rel.catid IN ('.$catlist.')';
 		
 		// Select only items user has access to if he is not allowed to show unauthorized items
 		if (!$show_noauth) {
