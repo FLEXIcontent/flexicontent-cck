8e9c904d263f400ac0089d845c982fd204a6105f
Georgios Papadakis <ggppdk@gmail.com>
2012-05-27 06:00:17 +0000

    - Removed phpthumb 1.7.9

--- administrator/components/com_flexicontent/views/items/tmpl/copy.php
+++ administrator/components/com_flexicontent/views/items/tmpl/copy.php
@@ -90,12 +90,12 @@ $copy_behaviour = JRequest::getVar('copy_behaviour','copy/move');
 								<input id="method-usejoomfish" type="radio" name="translate_method" value="2" onclick="copyonly();" />
 								<?php echo JText::_( 'FLEXI_USE_JF_DATA' ); ?>
 							</label><br />
+						<?php if ( JFile::exists(JPATH_COMPONENT.DS.'helpers'.DS.'translator.php') ) : ?>
 							<label for="method-autotranslation">
-							<?php if ( JFile::exists(JPATH_COMPONENT.DS.'helpers'.DS.'translator.php') ) : ?>
 								<input id="method-autotranslation" type="radio" name="translate_method" value="3" onclick="copyonly();" />
-							<?php endif; ?>
 								<?php echo JText::_( 'FLEXI_AUTO_TRANSLATION' ); ?>
 							</label><br />
+						<?php endif; ?>
 							<label for="method-firstjf-thenauto">
 								<!--input id="method-firstjf-thenauto" type="radio" name="translate_method" value="4" onclick="copyonly();" /-->
 								<?php echo " &nbsp;--&nbsp; <span style='color:gray;'>".JText::_( 'FLEXI_FIRST_JF_THEN_AUTO' )."</span>"; ?>
