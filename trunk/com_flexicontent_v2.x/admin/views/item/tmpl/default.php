858d318b6a2437755f02640d664ae9307d0d48fd
ggppdk <ggppdk@82e9b126-fb80-11de-b07f-13ea4a3b38a3>
2012-07-01 07:04:59 +0000

    # Readded fieldset as container to the fields in item edit form, it was commented out in previous revision

--- trunk/com_flexicontent_v2.x/admin/views/item/tmpl/default.php
+++ trunk/com_flexicontent_v2.x/admin/views/item/tmpl/default.php
@@ -463,7 +463,7 @@ if (isset($this->row->item_translations)) foreach ($this->row->item_translations
 
 				<div id="fc-change-error" class="fc-error" style="display:none;"><?php echo JText::_( 'FLEXI_TAKE_CARE_CHANGING_FIELD_TYPE' ); ?></div>
 				
-				<!--fieldset-->
+				<fieldset>
 					<legend>
 						<?php echo $this->row->type_id ? JText::_( 'FLEXI_ITEM_TYPE' ) . ' : ' . $this->typesselected->name : JText::_( 'FLEXI_TYPE_NOT_DEFINED' ); ?>
 					</legend>
@@ -570,11 +570,12 @@ if (isset($this->row->item_translations)) foreach ($this->row->item_translations
 										
 								<!--/td-->
 							<!--/tr-->
+								
 						<?php
 						}
 						?>
 					<!--/table-->
-				<!--/fieldset-->
+				</fieldset>
 				<?php
 				} else if ($this->row->id == 0) {
 				?>
