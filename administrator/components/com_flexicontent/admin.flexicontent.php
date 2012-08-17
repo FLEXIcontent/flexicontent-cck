0eaedc198e2c5eac25b4fee70977289a38cad30d
Georgios Papadakis <ggppdk@gmail.com>
2012-08-17 00:25:53 +0000

    ^ Change flexicontent module template 'news' to have a toplevel CSS class used in all css rules

--- administrator/components/com_flexicontent/admin.flexicontent.php
+++ administrator/components/com_flexicontent/admin.flexicontent.php
@@ -19,6 +19,12 @@
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
+$lhlist = array('localhost', '127.0.0.1');
+if( in_array($_SERVER['HTTP_HOST'], $lhlist) ) {
+	error_reporting(E_ALL & ~E_STRICT);
+	ini_set('display_errors',1);
+}
+
 //include constants file
 require_once (JPATH_COMPONENT.DS.'defineconstants.php');
 //include the needed classes and helpers
