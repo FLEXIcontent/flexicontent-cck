9929926dcd2dc4a0f9403d7f6e7a84d9824254a6
Georgios Papadakis <ggppdk@gmail.com>
2012-10-08 16:49:37 +0000

    + Added lightbox as a popup type for image field, light v2.0.3 js library is included in the libraries folder and loaded automatically

--- administrator/components/com_flexicontent/defineconstants.php
+++ administrator/components/com_flexicontent/defineconstants.php
@@ -86,5 +86,5 @@ if (!defined('FLEXI_ICONPATH'))		define('FLEXI_ICONPATH', 'images/M_images/');
 
 // Version constants
 define('FLEXI_VERSION',	'1.5.6');
-define('FLEXI_RELEASE',	'RC9 (r1514)');
+define('FLEXI_RELEASE',	'RC9 (r1515)');
 ?>
