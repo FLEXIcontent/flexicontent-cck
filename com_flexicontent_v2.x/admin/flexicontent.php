0b140dcf18552fb93321e9218e597bce6acfafd4
Georgios Papadakis <ggppdk@gmail.com>
2012-12-02 16:48:23 +0000

    # Some more changes regarding J3.0

---
+++ com_flexicontent_v2.x/admin/flexicontent.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @version 1.5 stable $Id: admin.flexicontent.php 1264 2012-05-04 15:55:52Z ggppdk $ 
+ * @package Joomla
+ * @subpackage FLEXIcontent
+ * @copyright (C) 2009 Emmanuel Danan - www.vistamedia.fr
+ * @license GNU/GPL v2
+ * 
+ * FLEXIcontent is a derivative work of the excellent QuickFAQ component
+ * @copyright (C) 2008 Christoph Lukes
+ * see www.schlu.net for more information
+ *
+ * FLEXIcontent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+require_once (JPATH_COMPONENT.DS.'admin.flexicontent.php');
+?>
