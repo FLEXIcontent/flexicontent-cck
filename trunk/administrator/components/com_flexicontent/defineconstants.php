457cb6d09ab665b8de3c82062fb550bec3acf12c
ggppdk <ggppdk@82e9b126-fb80-11de-b07f-13ea4a3b38a3>
2013-10-14 23:46:36 +0000

    # (a) Fixed universal module being empty in J3.1, (b) Fixed SQL error while creating filters in J3.1 after upgrading from J2.5

--- trunk/administrator/components/com_flexicontent/defineconstants.php
+++ trunk/administrator/components/com_flexicontent/defineconstants.php
@@ -186,6 +186,6 @@ if (!defined('FLEXI_ITEMVIEW'))		define('FLEXI_ITEMVIEW'	, FLEXI_J16GE ? 'item' 
 if (!defined('FLEXI_ICONPATH'))		define('FLEXI_ICONPATH'	, FLEXI_J16GE ? 'media/system/images/' : 'images/M_images/' );
 
 // Version constants
-define('FLEXI_VERSION',	FLEXI_J16GE ? '2.1.0' : '2.1.0');
-define('FLEXI_RELEASE',	'r1785');
+define('FLEXI_VERSION',	FLEXI_J16GE ? '2.1.1' : '2.1.1');
+define('FLEXI_RELEASE',	'r1788');
 ?>
