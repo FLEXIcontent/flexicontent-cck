e7a627771cac045b299640d52a30447edf49704a
ggppdk <ggppdk@gmail.com>
2014-10-13 02:20:52 +0300

    ^ Changed blog template CSS to make the number of columns responsive to the browser window width

--- site/templates/blog/category_items.php
+++ site/templates/blog/category_items.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 1.5 stable $Id: category_items.php 1904 2014-05-20 12:21:09Z ggppdk $
+ * @version 1.5 stable $Id$
  * @package Joomla
  * @subpackage FLEXIcontent
  * @copyright (C) 2009 Emmanuel Danan - www.vistamedia.fr
