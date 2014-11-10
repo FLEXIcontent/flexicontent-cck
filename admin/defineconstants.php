5da9f1df5d61e927c55a78ba0891b94d1e0f6ad8
ggppdk <ggppdk@gmail.com>
2014-11-11 00:42:07 +0200

    ^ Updated select2 JS Framework to v3.5.2

--- admin/defineconstants.php
+++ admin/defineconstants.php
@@ -182,5 +182,5 @@ if (!defined('FLEXI_ICONPATH'))		define('FLEXI_ICONPATH'	, FLEXI_J16GE ? 'media/
 
 // Version constants
 define('FLEXI_VERSION',	FLEXI_J16GE ? '2.2.1' : '2.2.1');
-define('FLEXI_RELEASE',	'rc');
+define('FLEXI_RELEASE',	'rc2');
 ?>
