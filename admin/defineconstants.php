03ee533be9ef02b7766d5e4c031d08efb95dc05d
ggppdk <ggppdk@gmail.com>
2015-03-12 20:42:45 +0200

    ^ Change version number 3.0 dev to 3.0 BETA5

--- admin/defineconstants.php
+++ admin/defineconstants.php
@@ -135,5 +135,5 @@ if (!defined('FLEXI_ICONPATH'))		define('FLEXI_ICONPATH'	, FLEXI_J16GE ? 'media/
 
 // Version constants
 define('FLEXI_VERSION',	FLEXI_J16GE ? '3.0' : '3.0');
-define('FLEXI_RELEASE',	'dev');
+define('FLEXI_RELEASE',	'BETA5');
 ?>
