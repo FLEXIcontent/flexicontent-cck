6876c3da2688009f6f4c189d412fa22eb1e93b21
ggppdk <ggppdk@gmail.com>
2015-04-28 04:23:01 +0300

    # a fix for displaying grouped fields selectmultiple, checkbox, checkboximage as ungrouped, when such display is used

--- plugins/flexicontent_fields/selectmultiple/selectmultiple.php
+++ plugins/flexicontent_fields/selectmultiple/selectmultiple.php
@@ -572,8 +572,9 @@ class plgFlexicontent_fieldsSelectmultiple extends JPlugin
 		$field->label = JText::_($field->label);
 		
 		// Some variables
-		$use_ingroup = !empty($field->ingroup);  //$field->parameters->get('use_ingroup', 0);
-		$multiple   = $use_ingroup || (int) $field->parameters->get( 'allow_multiple', 0 ) ;
+		$is_ingroup  = !empty($field->ingroup);  //$field->parameters->get('use_ingroup', 0);
+		$use_ingroup = $field->parameters->get('use_ingroup', 0);
+		$multiple    = $use_ingroup || (int) $field->parameters->get( 'allow_multiple', 0 ) ;
 		$view = JRequest::getVar('flexi_callview', JRequest::getVar('view', FLEXI_ITEMVIEW));
 		
 		// Get field values
@@ -736,7 +737,7 @@ class plgFlexicontent_fieldsSelectmultiple extends JPlugin
 		}
 		
 		
-		if (!$use_ingroup)  // do not convert the array to string if field is in a group
+		if (!$is_ingroup)  // do not convert the array to string if field is in a group
 		{
 			if ($multiple && self::$valueIsArr) {
 				// Values separator, field 's opening / closing texts, were already applied for every array of values
