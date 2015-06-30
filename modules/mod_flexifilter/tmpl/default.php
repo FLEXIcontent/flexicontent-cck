f789b028761e4987ed15a358741070758f0223de
Berges yannick <micker@free.fr>
2015-06-30 13:19:42 +0200

    Fix for title slider

--- modules/mod_flexifilter/tmpl/default.php
+++ modules/mod_flexifilter/tmpl/default.php
@@ -30,7 +30,7 @@ if ($ff_placement){
 	$ff_slider_id = 
 		($module->id     ? '_'.$module->id : '')
 		;
-	$ff_slider_title = JText::_($params->get('ff_slider_title', 'FLEXI_SEARCH_FORM_TOGGLE'));
+	$ff_slider_title = JText::_($params->get('ff_toggle_search_title', 'FLEXI_SEARCH_FORM_TOGGLE'));
 	echo JHtml::_('sliders.start', 'fcfilter_form_slider'.$ff_slider_id, array('useCookie'=>1, 'show'=>-1, 'display'=>-1, 'startOffset'=>-1, 'startTransition'=>1));
 	echo JHtml::_('sliders.panel', $ff_slider_title, 'fcfilter_form_togglebtn'.$ff_slider_id);
 }
