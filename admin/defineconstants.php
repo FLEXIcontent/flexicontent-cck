43e0a99bf079aea242d346cd15e7040b406f3d40
ggppdk <ggppdk@gmail.com>
2015-07-23 12:00:34 +0300

    Updated version number to v3.0.2

--- admin/defineconstants.php
+++ admin/defineconstants.php
@@ -136,6 +136,6 @@ if (!defined('FLEXI_ICONPATH'))		define('FLEXI_ICONPATH'	, FLEXI_J16GE ? 'media/
 // Version constants
 define('FLEXI_PHP_NEEDED',	'5.3.0');
 define('FLEXI_PHP_RECOMMENDED',	'5.4.0');
-define('FLEXI_VERSION', '3.0.1');
+define('FLEXI_VERSION', '3.0.2');
 define('FLEXI_RELEASE',	'');
 ?>
