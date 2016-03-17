47ab8e294852a00851b4b014c3e982fe7abdd292
ggppdk <ggppdk@gmail.com>
2016-03-17 07:36:50 +0200

    Fixed item next/previous navigation field (fcpagenav) to work for item links that are non-category origin e.g. from module

--- plugins/flexicontent_fields/fcpagenav/tmpl/value_default.php
+++ plugins/flexicontent_fields/fcpagenav/tmpl/value_default.php
@@ -1,7 +1,9 @@
 <?php
-$html = '<span class="fcpagenav btn-group">';
-$tooltip_class = FLEXI_J30GE ? ' hasTooltip' : ' hasTip';
-		
+$html = $img_err_msg.'<span class="fcpagenav btn-group">';
+
+$item_use_image = $field->parameters->get('item_use_image', 0);
+$img_size = ' width: ' .$field->parameters->get('nav_width', 200). "px; height: " .$field->parameters->get('nav_height', 200). 'px;';
+
 // CATEGORY back link
 if ($use_category_link)
 {
@@ -14,55 +16,69 @@ if ($use_category_link)
 		$html .= '
 			<a class="fcpagenav-return btn' . ($use_tooltip ? $tooltip_class : '') . '" ' . ($use_tooltip ? $tooltip : '') . ' href="'. JRoute::_(FlexicontentHelperRoute::getCategoryRoute($rows[$item->id]->categoryslug)).'?start='.$start .'">
 				<i class="icon-undo"></i>
-				' . htmlspecialchars($category->title, ENT_NOQUOTES, 'UTF-8') .'
-				' . ( $cat_image ? '
-					<br/><img src="'.$cat_image.'" alt="Return"/>
+				' . htmlspecialchars($category->title, ENT_NOQUOTES, 'UTF-8') . '
+				' . ($cat_image ? '
+				<br/><img src="'.$cat_image.'" alt="Return"/>
 				' : '') .'
-			</a>';
+			</a>
+		';
 	}
 }
-		
+
+
 // Next item linking
 if ($field->prev)
 {
 	$tooltip = $use_tooltip ? ' title="'. flexicontent_html::getToolTip($tooltip_title_prev, $field->prevtitle, 0) .'"' : '';
 	$html .= '
-		<a class="fcpagenav-prev btn' . ($use_tooltip ? $tooltip_class : '') . '" ' . ($use_tooltip ? $tooltip : '') . ' href="'. $field->prevurl .'">
-			<i class="icon-backward-circle"></i>
+		<a class="fcpagenav-prev btn' . ($use_tooltip ? ' '.$tooltip_class : '') . '" ' . ($use_tooltip ? $tooltip : '') . ' href="'. $field->prevurl .'">
+			<i class="icon-previous"></i>
 			' . ( $use_title ? $field->prevtitle : htmlspecialchars($prev_label, ENT_NOQUOTES, 'UTF-8') ).'
-			' . ($field->prevThumb ? '
-				<br/><img src="'.$field->prevThumb.'" alt="Previous"/>
-			' : '').'
-		</a>';
+			' . ( $item_use_image ? ($field->prevThumb ? '
+				<br/><img src="'.$field->prevThumb.'" alt="Previous" style="'.$img_size.'" />' : '
+				<br/><span class="fc-pagenav-noimg" style="display:inline-block; '.$img_size.'"></span>
+			') : '').'
+		</a>
+	';
 } else {
 	$html .= '
 		<span class="fcpagenav-prev btn disabled">
 			<i class="icon-previous"></i>
 			'.htmlspecialchars($prev_label, ENT_NOQUOTES, 'UTF-8').'
-		</span>';
+			'.( $item_use_image ? '
+				<br/><span class="fc-pagenav-noimg" style="display:inline-block; '.$img_size.'"></span>
+			' : '').'
+		</span>
+	';
 }
 		
 // Item location and total count
 $html .= $show_prevnext_count ? '<span class="fcpagenav-items-cnt btn disabled">'.($location+1).' / '.$item_count.'</span>' : '';
-		
+
 // Previous item linking
 if ($field->next)
 {
 	$tooltip = $use_tooltip ? ' title="'. flexicontent_html::getToolTip($tooltip_title_next, $field->nexttitle, 0) .'"' : '';
 	$html .= '
-		<a class="fcpagenav-next btn' . ($use_tooltip ? $tooltip_class : '') . '" ' . ($use_tooltip ? $tooltip : '') . ' href="'. $field->nexturl .'">
-			<i class="icon-forward-circle"></i>
+		<a class="fcpagenav-next btn' . ($use_tooltip ? ' '.$tooltip_class : '') . '" ' . ($use_tooltip ? $tooltip : '') . ' href="'. $field->nexturl .'">
+			<i class="icon-next"></i>
 			' . ( $use_title ? $field->nexttitle : htmlspecialchars($next_label, ENT_NOQUOTES, 'UTF-8') ).'
-			' . ($field->nextThumb ? '
-				<br/><img src="'.$field->nextThumb.'" alt="Next"/>
-			' : '').'
-		</a>';
+			' . ( $item_use_image ? ($field->nextThumb ? '
+				<br/><img src="'.$field->nextThumb.'" alt="Next" style="'.$img_size.'" />' : '
+				<br/><span class="fc-pagenav-noimg" style="display:inline-block; '.$img_size.'"></span>
+			') : '').'
+		</a>
+	';
 } else {
 	$html .= '
 		<span class="fcpagenav-next btn disabled">
 			<i class="icon-next"></i>
 			'.htmlspecialchars($next_label, ENT_NOQUOTES, 'UTF-8').'
-		</span>';
+			'.( $item_use_image ? '
+				<br/><span class="fc-pagenav-noimg" style="display:inline-block; '.$img_size.'"></span>
+			' : '').'
+		</span>
+	';
 }
 		
 $html .= '</span>';
