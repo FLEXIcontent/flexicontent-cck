916620adf2c75db3e343cc0da9a8308ca7cdc38e
ggppdk <ggppdk@gmail.com>
2016-03-30 05:11:15 +0300

    Added rel next / prev to Next / Previous links of flexi break (mult-paged items) plugin

--- plugins/content/flexibreak/flexibreak/tmpl/pagination_js.php
+++ plugins/content/flexibreak/flexibreak/tmpl/pagination_js.php
@@ -7,7 +7,7 @@ $display_method = $this->params->get('display_method', 1);
 <div class="tocNav">
 	<?php
 	if ($this->limitstart > 0 || $display_method==1) echo '
-		<a href="'.($display_method==2 ? $this->prev_link : 'javascript:;').'" class="tocPrev" onclick="'.($display_method==1 ? 'flexibreak.previous();' : '').'">'. JText::_( 'FLEXIBREAK_PREVIOUS_PAGE' ) .'</a>';
+		<a href="'.($display_method==2 ? $this->prev_link : 'javascript:;').'" class="tocPrev" rel="prev" onclick="'.($display_method==1 ? 'flexibreak.previous();' : '').'">'. JText::_( 'FLEXIBREAK_PREVIOUS_PAGE' ) .'</a>';
 	else if ($display_method==2) echo '
 		<span class="tocNoPrevNext tocPrev" >'. JText::_( 'FLEXIBREAK_PREVIOUS_PAGE' ) .'</span>';
 	
@@ -15,7 +15,7 @@ $display_method = $this->params->get('display_method', 1);
 		<span class="tocPrevNextCnt">'.($this->limitstart+1).' / '.$this->textscount.'</span>';
 	
 	if ($this->limitstart < $this->textscount - 1 || $display_method==1) echo '
-		<a href="'.($display_method==2 ? $this->next_link : 'javascript:;').'" class="tocNext" onclick="'.($display_method==1 ? 'flexibreak.next();' : '').'">'. JText::_( 'FLEXIBREAK_NEXT_PAGE' ) .'</a>';
+		<a href="'.($display_method==2 ? $this->next_link : 'javascript:;').'" class="tocNext" rel="next" onclick="'.($display_method==1 ? 'flexibreak.next();' : '').'">'. JText::_( 'FLEXIBREAK_NEXT_PAGE' ) .'</a>';
 	else if ($display_method==2) echo '
 		<span class="tocNoPrevNext tocNext" >'. JText::_( 'FLEXIBREAK_NEXT_PAGE' ) .'</span>';
 	?>
