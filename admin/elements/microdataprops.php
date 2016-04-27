62aa7e99f6dee8708c703b1aff8294696bed9b1a
ggppdk <ggppdk@gmail.com>
2016-04-27 16:10:28 +0300

    Modified microdata form element parameter to list all known microdata properties

--- admin/elements/microdataprops.php
+++ admin/elements/microdataprops.php
@@ -24,90 +24,36 @@ jimport('joomla.form.field');  // JFormField
 //JFormHelper::loadFieldClass('...');   // JFormField...
 
 
-class JFormFieldMicrodataprops extends JFormField {
-
+class JFormFieldMicrodataprops extends JFormField
+{
 	protected $type = 'microdataprops';
+	
+	public function getInput()
+	{
+		// Get microdata types
+		static $jm_types = null;
+		
+		if ($jm_types === null)
+		{
+			jimport('joomla.microdata.microdata');
+			$jm = new JMicrodata();
+			$jm_types = $jm->getTypes();
+		}
 
-	// getLabel() left out
-
-	public function getInput() {
-		$values = array(
-			'url' => 'Url',
-			'name' => 'Name',
-			'job-title' => 'Jobtitle',
-			'description'=>'description',
-			'address'=>'Address',
-			'streetAddress'=>'streetAddress',
-			'postOfficeBoxNumber'=>'postOfficeBoxNumber',
-			'addressLocality'=>'addressLocality',
-			'addressRegion'=>'addressRegion',
-			'addressCountry'=>'addressCountry',
-			'email'=>'email',
-			'telephone'=>'telephone',
-			'birthDate'=>'birthDate',
-			'image'=>'image',
-			'brand'=>'brand',
-			'review'=>'aggregateRating',
-			'offers'=>'offers',
-			'sku'=>'sku',
-			'price'=>'price',
-			'priceCurrency'=>'priceCurrency',
-			'priceValidUntil'=>'priceValidUntil',
-			'availability'=>'availability',
-			'itemOffered'=>'itemOffered',
-			'lowPrice'=>'lowPrice',
-			'highPrice'=>'highPrice',
-			'priceCurrency'=>'priceCurrency',
-			'offerCount'=>'offerCount',
-			'manufacturer'=>'manufacturer',
-			'model'=>'model',
-			'productId'=>'productId',
-			'recipeCategory'=>'recipeCategory',
-			'datePublished'=>'datePublished',
-			'prepTime'=>'prepTime',
-			'cookTime'=>'cookTime',
-			'totalTime'=>'totalTime',
-			'nutrition'=>'nutrition',
-			'recipeInstructions'=>'recipeInstructions',
-			'recipeYield'=>'recipeYield',
-			'ingredients'=>'ingredients',
-			'author'=>'author',
-			'location'=>'location',
-			'startDate'=>'startDate',
-			'offers.price'=>'offers.price',
-			'duration'=>'duration',
-			'postalCode'=>'postalCode',
-			'reviewBody'=>'reviewBody',
-			'reviewRating'=>'reviewRating',
-			'reviewRating.ratingValue'=>'reviewRating.ratingValue',
-			'reviewRating.bestRating'=>'reviewRating.bestRating',
-			'reviewRating.worstRating'=>'reviewRating.worstRating',
-			'thumbnailUrl'=>'thumbnailUrl',
-			'uploadDate'=>'uploadDate',
-			'contentUrl'=>'contentUrl',
-			'embedUrl'=>'embedUrl',
-			'director'=>'director',
-			'actors'=>'actors',
-			'publisher'=>'publisher',
-			'bookEdition'=>'bookEdition',
-			'isbn'=>'isbn',
-			'bookFormat'=>'bookFormat',
-			'color'=>'color'
-		);
-
-		## Initialize array to store dropdown options ##
+		// Initialize the options array
 		$options = array();
 		$options[] = JHTML::_('select.option','', '-- '.JText::_('FLEXI_DISABLE').' --');
+		
+		foreach($jm_types as $type => $tdata)
+		{
+			$options[] = JHTML::_('select.optgroup', JText::_( $type ) );
+			foreach($tdata['properties'] as $propname => $props) {
+				$options[] = JHTML::_('select.option', $propname, $propname);
+			}
+			$options[] = JHTML::_('select.optgroup', '' );
+		}
 
-		foreach($values as $key=>$value) :
-		## Create $value ##
-		$options[] = JHTML::_('select.option', $key, $value);
-		endforeach;
-
-		## Create <select name="icons" class="inputbox"></select> ##
-		$dropdown = JHTML::_('select.genericlist', $options, $this->name, 'class="use_select2_lib inputbox"', 'value', 'text', $this->value, $this->id);
-
-		## Output created <select> list ##
-		return $dropdown;
+		// Render and return the drop down select
+		return JHTML::_('select.genericlist', $options, $this->name, 'class="use_select2_lib inputbox"', 'value', 'text', $this->value, $this->id);
 	}
 }
