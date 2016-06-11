6406111ecb274fa86b88fe148f0e78bd6d7f6113
ggppdk <ggppdk@gmail.com>
2016-06-11 17:59:44 +0300

    Add font-based icon support to radioimage and checkbox image fields, also with per case font size and color support

--- plugins/flexicontent_fields/radioimage/tmpl/value_default.php
+++ plugins/flexicontent_fields/radioimage/tmpl/value_default.php
@@ -11,6 +11,16 @@ if ( $display_all ) {
   $ns_posttext = $remove_space ? $ns_posttext : ' ' . $ns_posttext;
 }
 
+
+// Create CSS class for image / icon HTML tag
+if ($text_or_value > 1)
+{
+	$_class = $element->image .' fc_ifield_val_icoclass'
+		.($icon_size ? ' fc-icon-'.$icon_size : '')
+		.($text_or_value == 2 || $text_or_value == 4  ?  ' '.$tooltip_class  :  '');
+}
+
+
 foreach ($values as $value)
 {
 	// Compatibility for serialized values
@@ -44,11 +54,16 @@ foreach ($values as $value)
 			
 			/* only for (*IMAGE) fields */
 			else if ($text_or_value == 2)
-				$disp = '<img src="'.$imgpath . $element->image .'" class="fc_ifield_val_img '.$tooltip_class.'" title="'.flexicontent_html::getToolTip(null, $element->text, 0).'" alt="'.$element->text.'" />';
+				$disp = !$image_type ?
+					'<img src="'. $imgpath . $element->image .'" class="fc_ifield_val_img '.$tooltip_class.'" title="'.flexicontent_html::getToolTip(null, $element->text, 0).'" alt="'.$element->text.'" />' :
+					'<span class="'. $_class .' '. $element->image .'" style="'.($icon_color ? 'color: '.$icon_color.';' : '').'" title="'.flexicontent_html::getToolTip(null, $element->text, 0).'"></span>' ;
 			else
 				$disp = '
 				<div class="fc_ifield_val_box">
-					<img src="'.$imgpath . $element->image .'" class="fc_ifield_val_img '.($text_or_value == 4 ? $tooltip_class : '').'" '.($text_or_value == 4 ? 'title="'.flexicontent_html::getToolTip(null, $element->text, 0).'"' : '').' alt="'.$element->text.'" />
+					'.(!$image_type ?
+						'<img src="'.$imgpath . $element->image .'" class="fc_ifield_val_img '.($text_or_value == 4 ? $tooltip_class : '').'" '.($text_or_value == 4 ? 'title="'.flexicontent_html::getToolTip(null, $element->text, 0).'"' : '').' alt="'.$element->text.'" />' :
+						'<span class="'. $_class .' '. $element->image .'" style="'.($icon_color ? 'color: '.$icon_color.';' : '').'" '.($text_or_value == 4 ? ' title="'.flexicontent_html::getToolTip(null, $element->text, 0).'"' : '').'"></span>'
+					).'
 					<span class="alert alert-info fc_ifield_val_txt">'.($text_or_value == 3 ? $element->text : $element->value).'</span>
 				</div>
 				';
@@ -77,11 +92,16 @@ foreach ($values as $value)
 			
 			/* only for (*IMAGE) fields */
 			else if ($text_or_value == 2)
-				$disp = '<img src="'.$imgpath . $element->image .'" class="fc_ifield_val_img '.$tooltip_class.'" title="'.flexicontent_html::getToolTip(null, $element->text, 0).'" alt="'.$element->text.'" />';
+				$disp = !$image_type ?
+					'<img src="'.$imgpath . $element->image .'" class="fc_ifield_val_img '.$tooltip_class.'" title="'.flexicontent_html::getToolTip(null, $element->text, 0).'" alt="'.$element->text.'" />' :
+					'<span class="'. $_class .' '. $element->image .'" style="'.($icon_color ? 'color: '.$icon_color.';' : '').'" title="'.flexicontent_html::getToolTip(null, $element->text, 0).'"></span>' ;
 			else
 				$disp = '
 				<div class="fc_ifield_val_box">
-					<img src="'.$imgpath . $element->image .'" class="fc_ifield_val_img '.($text_or_value == 4 ? $tooltip_class : '').'" '.($text_or_value == 4 ? 'title="'.flexicontent_html::getToolTip(null, $element->text, 0).'"' : '').' alt="'.$element->text.'" />
+					'.(!$image_type ?
+						'<img src="'. $imgpath . $element->image .'" class="fc_ifield_val_img '.($text_or_value == 4 ? $tooltip_class : '').'" '.($text_or_value == 4 ? 'title="'.flexicontent_html::getToolTip(null, $element->text, 0).'"' : '').' alt="'.$element->text.'" />' :
+						'<span class="'. $_class .' '. $element->image .'" style="'.($icon_color ? 'color: '.$icon_color.';' : '').'" '.($text_or_value == 4 ? ' title="'.flexicontent_html::getToolTip(null, $element->text, 0).'"' : '').'"></span>'
+					).'
 					<span class="alert alert-info fc_ifield_val_txt">'.($text_or_value == 3 ? $element->text : $element->value).'</span>
 				</div>
 				';
