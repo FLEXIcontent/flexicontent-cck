5e492a1addfa4465578f7e145f38b2775b11835d
ggppdk <ggppdk@gmail.com>
2016-07-26 15:21:22 +0300

    v3.1.0-rc2

--- admin/defineconstants.php
+++ admin/defineconstants.php
@@ -130,7 +130,7 @@ if (!defined('FLEXI_ICONPATH'))		define('FLEXI_ICONPATH'	, FLEXI_J16GE ? 'media/
 // Version constants
 define('FLEXI_PHP_NEEDED',	'5.3.0');
 define('FLEXI_PHP_RECOMMENDED',	'5.4.0');
-define('FLEXI_VERSION', '3.1.0-rc');
+define('FLEXI_VERSION', '3.1.0-rc2');
 define('FLEXI_RELEASE',	'');
 define('FLEXI_VHASH',	md5(filemtime(__FILE__) . filectime(__FILE__) . FLEXI_VERSION));
 ?>
