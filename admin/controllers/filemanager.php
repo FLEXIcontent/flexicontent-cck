07ccc10610c7558c613c948df1a78ed75cc0eefc
ggppdk <ggppdk@gmail.com>
2016-07-29 19:44:34 +0300

    Fixed filemanager add_url task filtering out slashes in new file URLs

--- admin/controllers/filemanager.php
+++ admin/controllers/filemanager.php
@@ -402,7 +402,8 @@ class FlexicontentControllerFilemanager extends FlexicontentController
 		$jinput = $app->input;
 		
 		$return		= $jinput->get( 'return-url', null, 'base64' );
-		$filename	= $jinput->get( 'file-url-data', null);  // Default filtering
+		$filename	= $jinput->get( 'file-url-data', null, 'string');
+		$filename = flexicontent_html::dataFilter($filename, 4000, 'URL', 0);  // Clean bad text/html
 		$altname	= $jinput->get( 'file-url-title', null, 'string' );
 		$ext			= $jinput->get( 'file-url-ext', null, 'alnum' );
 		$filedesc	= $jinput->get( 'file-url-desc', '');  // Default filtering
@@ -793,19 +794,24 @@ class FlexicontentControllerFilemanager extends FlexicontentController
 		$path = $data['secure'] ? COM_FLEXICONTENT_FILEPATH.DS : COM_FLEXICONTENT_MEDIAPATH.DS;  // JPATH_ROOT . DS . <media_path | file_path> . DS
 		$file_path = JPath::clean($path . $data['filename']);
 
-		// Get file size if local file
 		if (!$data['url'])
 		{
+			// Get file size from filesystem (local file)
 			$data['size'] = file_exists($file_path) ? filesize($file_path) : 0;
 		}
 		else
 		{
+			
+			// Get file size from submitted field (file URL)
 			$arr_sizes = array('KBs'=>1024, 'MBs'=>(1024*1024), 'GBs'=>(1024*1024*1024));
 			$size_unit = (int) @ $arr_sizes[$data['size_unit']];
 			if ( $size_unit )
 				$data['size'] = ((int)$data['size']) * $size_unit;
 			else
 				$data['size'] = 0;
+
+			// Validate file link
+			$data['filename_original'] = flexicontent_html::dataFilter($data['filename_original'], 4000, 'URL', 0);  // Clean bad text/html
 		}
 
 		if ($model->store($data))
