40522419db25dc49b008d3a17fff0bcd12bb7bbc
ggppdk <ggppdk@gmail.com>
2016-08-04 18:06:30 +0300

    Added new filters styling to category manager too

--- admin/views/tags/tmpl/default.php
+++ admin/views/tags/tmpl/default.php
@@ -73,7 +73,7 @@ function delAllFilters() {
 <?php endif;?>
 
 	<div id="fc-filters-header">
-		<span class="btn-group input-append filter-search fc-filter">
+		<span class="btn-group input-append fc-filter filter-search">
 			<input type="text" name="search" id="search" placeholder="<?php echo JText::_( 'FLEXI_SEARCH' ); ?>" value="<?php echo htmlspecialchars($this->lists['search'], ENT_QUOTES, 'UTF-8'); ?>" class="inputbox" />
 			<button title="" data-original-title="<?php echo JText::_('FLEXI_SEARCH'); ?>" class="<?php echo $btn_class.' '.$tip_class; ?>" onclick="document.adminForm.limitstart.value=0; Joomla.submitform();"><?php echo FLEXI_J30GE ? '<i class="icon-search"></i>' : JText::_('FLEXI_GO'); ?></button>
 			<button title="" data-original-title="<?php echo JText::_('FLEXI_RESET_FILTERS'); ?>" class="<?php echo $btn_class.' '.$tip_class; ?>" onclick="document.adminForm.limitstart.value=0; delAllFilters(); Joomla.submitform();"><?php echo FLEXI_J30GE ? '<i class="icon-remove"></i>' : JText::_('FLEXI_CLEAR'); ?></button>
