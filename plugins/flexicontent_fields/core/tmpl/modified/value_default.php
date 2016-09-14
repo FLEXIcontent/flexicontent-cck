85e5dc22ccab915a16e481e59a59deeae3366d4d
ggppdk <ggppdk@gmail.com>
2016-09-14 09:49:32 +0300

    Added layout file support for 3 more CORE fields: title, created (date), modified (date)

---
+++ plugins/flexicontent_fields/core/tmpl/modified/value_default.php
@@ -0,0 +1,7 @@
+<?php
+// Add prefix / suffix
+$field->{$prop} = $pretext.JHTML::_( 'date', $item->modified, $dateformat ).$posttext;
+
+// Add microdata property
+$itemprop = $field->parameters->get('microdata_itemprop', 'dateModified');
+if ($itemprop) $field->{$prop} = '<div style="display:inline" itemprop="'.$itemprop.'" >' .$field->{$prop}. '</div>';
