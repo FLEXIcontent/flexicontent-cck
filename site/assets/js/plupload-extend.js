6a6f60a3072359e2f3740a68140720ec8a2e6182
ggppdk <ggppdk@gmail.com>
2016-09-28 01:31:58 +0300

    Cleanups for file property editing feature, fixed a file name display in uploader after property editing

--- site/assets/js/plupload-extend.js
+++ site/assets/js/plupload-extend.js
@@ -14,7 +14,7 @@
 
 		if (uploader_container.find('.fc_plupload_toggleThumbs_btn').length==0)
 		{
-			uploader_container.find(".plupload_header_content").prepend('\
+			uploader_container.find('.plupload_header_content').prepend('\
 				<span class="btn fc_plupload_toggleThumbs_btn" style="float:right; margin: 12px;" onclick="jQuery(this).closest(\'.plupload_container\').toggleClass(\'fc_uploader_hide_preview\');">\
 					<span class="icon-image"></span> Thumbnails\
 				</span>\
@@ -42,10 +42,10 @@
 			fc_plupload_extend_row( uploader, i );
 
 			// Mark edit button with SUCCESS color to show that it has already assigned file properties
-			var row_id = uploader.files[i].id;
-			!!form_data[row_id] ?
-				jQuery("#"+row_id).find(".fc_props_edit_btn").addClass("btn-success") :
-				jQuery("#"+row_id).find(".fc_props_edit_btn").removeClass("btn-success") ;
+			var file_row_id = uploader.files[i].id;
+			!!form_data[file_row_id] ?
+				jQuery("#"+file_row_id).find('.fc_props_edit_btn').addClass("btn-success") :
+				jQuery("#"+file_row_id).find('.fc_props_edit_btn').removeClass("btn-success") ;
 		}
 	}
 
@@ -53,7 +53,7 @@
 	// Create client side image preview. This is given a File object (as presented by Plupload),
 	// and show the client-side-only preview of the selected image object.
 
-	function fc_plupload_extend_row( uploader, i, item, params )
+	function fc_plupload_extend_row( uploader, i )
 	{
 		var IEversion = isIE();
 		var is_IE8_IE9 = IEversion && IEversion < 10;
@@ -62,16 +62,16 @@
 		params.edit_properties = typeof params.edit_properties == "undefined" ? 1 : params.edit_properties;
 		
 		var file = uploader.files[i];
-		var row_id = file.id;
-		var row = jQuery("#"+row_id);
-		var item = row.find(".plupload_file_name");
+		var file_row_id = file.id;
+		var file_row = jQuery('#'+file_row_id);
+		var file_name_box = file_row.find('.plupload_file_name');
 		var is_img = is_IE8_IE9 && !fc_has_flash_addon() ? 0 : file.name.match(/\.(jpg|jpeg|png|gif)$/i);
 
 
 		/*
 		 * Add properties editing button
 		 */
-		var btn_box = jQuery("<span class=\"btn-group fc_uploader_row_btns\"></span>").insertAfter( item );
+		var btn_box = jQuery("<span class=\"btn-group fc_uploader_row_btns\"></span>").insertAfter( file_name_box );
 
 		if (params.edit_properties)
 		{
@@ -83,8 +83,8 @@
 			{
 				var form_data = jQuery(uploader.settings.container).data("form_data");
 				if (!form_data) form_data = {};
-				var file_id = jQuery(this).closest("li").attr("id");
-				var file_data = typeof form_data[file_id] == "undefined" ? false : form_data[file_id];
+				var file_row_id = jQuery(this).closest("li").attr("id");
+				var file_data = typeof form_data[file_row_id] == "undefined" ? false : form_data[file_row_id];
 
 				// Set the EDIT btn that open current file properties (this can used to find the file row being edited)
 				var btn = jQuery(this);
@@ -94,7 +94,7 @@
 
 				// Restore the form field data and set ... and then also set the file ID in it
 				fc_restore_form_field_values(form, file_data);
-				form.find('[name="uploader_file_id"]').val(file_id);
+				form.find('[name="file_row_id"]').val(file_row_id);
 
 				// Get current filename and extension from the file row
 				var file_name = btn.closest("li").find('.plupload_file_name').text();
@@ -122,36 +122,37 @@
 			var box = jQuery("<span class=\"plupload_img_preview\"></span>").insertAfter( btn_box );
 
 			// Try to use already loaded image, otherwise we will load it later
-			var image_already_loaded = !! fc_plupload_loaded_imgs[row_id];
+			var image_already_loaded = !! fc_plupload_loaded_imgs[file_row_id];
 			if (!image_already_loaded)
 			{
-				fc_plupload_loaded_imgs[row_id] = jQuery( "<img src=\"components/com_flexicontent/assets/images/ajax-loader.gif\" />" );
+				fc_plupload_loaded_imgs[file_row_id] = jQuery( "<img src=\"components/com_flexicontent/assets/images/ajax-loader.gif\" />" );
 			}
 
 
-			row.addClass("fc_uploader_is_image");     // Add class to indicate that file ROW is an image
-			fc_plupload_loaded_imgs[row_id].appendTo( box );   // Add existing image or loading animation icon to the DOM
+			file_row.addClass("fc_uploader_is_image");     // Add class to indicate that file ROW is an image
+			fc_plupload_loaded_imgs[file_row_id].appendTo( box );   // Add existing image or loading animation icon to the DOM
 
 
 			// Add image zoom-in-out on click of image preview button
-			imgpreview_handle.add(fc_plupload_loaded_imgs[row_id]).on( "click", function()
+			imgpreview_handle.add(fc_plupload_loaded_imgs[file_row_id]).on( "click", function()
 			{
 				// Close any open previews
-				var row, btn, img_box;
-				row = jQuery(this).closest("li");
-				btn = row.find(".fc_img_preview_btn");
-				img_box = row.find(".plupload_img_preview");
+				var file_row, btn, img_box;
+				file_row = jQuery(this).closest("li");
+				btn = file_row.find('.fc_img_preview_btn');
+				img_box = file_row.find('.plupload_img_preview');
 
-				row.closest("ul").find("li:not('#" + btn.closest("li").attr("id") + "') .btn.fc_img_preview_btn.active").trigger("click");
-				if (row.hasClass("fc_uploader_zoomed"))
+				file_row.closest("ul").find("li:not('#" + btn.closest('li').attr('id') + "') .btn.fc_img_preview_btn.active").trigger("click");
+				if (file_row.hasClass("fc_uploader_zoomed"))
 				{
 					btn.removeClass("active btn-info");
-					row.removeClass("fc_uploader_zoomed");
-					setTimeout(function(){ row.removeClass("fc_uploader_zooming"); }, 400);
+					file_row.removeClass("fc_uploader_zoomed");
+					setTimeout(function(){ file_row.removeClass("fc_uploader_zooming"); }, 400);
 				}
-				else {
-					if (row.hasClass("fc_uploader_zooming")) return;
-					row.addClass("fc_uploader_zooming fc_uploader_zoomed");
+				else
+				{
+					if (file_row.hasClass("fc_uploader_zooming")) return;
+					file_row.addClass("fc_uploader_zooming fc_uploader_zoomed");
 					btn.addClass("active btn-info");
 				}
 			});
@@ -173,7 +174,7 @@
 				this.downsize( 800, 600 );
 
 				// Now that the image is preloaded, grab the Base64 encoded data URL. This will show the image without making an Network request using the client-side file binary.
-				fc_plupload_loaded_imgs[row_id].prop( "src", this.getAsDataURL() );
+				fc_plupload_loaded_imgs[file_row_id].prop( "src", this.getAsDataURL() );
 			};
 
 			// Calling the .getSource() on the file will return an instance of mOxie.File, which is a unified file wrapper that can be used across the various runtimes.
@@ -192,15 +193,17 @@
 		var form = (!IEversion || IEversion > 8) ? jQuery(obj.form) : jQuery(obj).closest('form');
 		var data = form.serialize();	
 
-		// Mark EDIT button of the FILE row, as having file properties
+		// Mark EDIT button of the file row, as having file properties
 		var btn = form.data("edit_btn");	
 		if (btn) btn.addClass('btn-success');
 
-		// Store file properties of the current FILE row, so that they can be reloaded and re-edited, without contacting WEB server
-		var file_id = form.find('[name="uploader_file_id"]').val();	
+		// Store file properties of the current file row, so that they can be reloaded and re-edited, without contacting WEB server
+		var file_row_id = form.find('[name="file_row_id"]').val();
+		var file_row = jQuery("#"+file_row_id);
+
 		var form_data = jQuery(uploader.settings.container).data("form_data");
 		if (!form_data) form_data = {};
-		form_data[file_id] = form.serializeObject();
+		form_data[file_row_id] = form.serializeObject();
 		jQuery(uploader.settings.container).data("form_data", form_data);
 
 		// Update file row so that new filename is displayed
@@ -208,22 +211,23 @@
 		new_filename = fc_plupload_sanitize_filename( new_filename ) + '.' +  form.find('[name="file-props-name-ext"]').val();
 		if (new_filename != '')
 		{
-			var old_filename = jQuery(uploader.settings.container).find('li .plupload_file_name').text();
-			jQuery(uploader.settings.container).find('li .plupload_file_name').html('<span>'+new_filename+'</span>');
-	
+			var file_name_box = file_row.find('.plupload_file_name');
+			var old_filename = file_name_box.text();
+			file_name_box.html('<span>'+new_filename+'</span>');
+
 			// Set new filename into the files data of the uploader
 			for ( var i = 0 ; i < uploader.files.length ; i++ )
 			{
 				var file = uploader.files[i];
-				if (uploader.files[i].name == old_filename)
+				if (file.name == old_filename)
 				{
-					uploader.files[i].name = new_filename;
+					file.name = new_filename;
 				}
 			}
 		}
 
 		// Set data
-		var props_msg_box = jQuery("li#"+file_id).find(".fileprops_message");
+		var props_msg_box = jQuery("li#"+file_row_id).find('.fileprops_message');
 		props_msg_box.html("<div class=\"fc_loading_msg\">"+Joomla.JText._('FLEXI_APPLYING_DOT')+"</div>");
 		props_msg_box.css({display: '', opacity: ''});   // show message
 		props_msg_box.parent().find('.plupload_img_preview').css('display', 'none');  // Hide preview image
@@ -240,7 +244,7 @@
 			data: data,
 			success: function(data) {
 				uploader_footer.show();  // Show uploader buttons previously hidden
-				props_msg_box.html('');  // Start with empting the row's message box
+				props_msg_box.html('');  // Start with empting the file row's message box
 				try {
 					var response = typeof data !== "object" ? jQuery.parseJSON( data ) : data;
 					jQuery('#system-message-container').html(!!response.sys_messages ? response.sys_messages : '');
