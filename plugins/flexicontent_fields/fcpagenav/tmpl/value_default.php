75c91d4b63a1626dc2187e462ffd78e632b001f7
ggppdk <ggppdk@gmail.com>
2017-04-22 00:17:40 +0300

    Removed unneded start=0 of back-to-category link in article pagination field plugin

--- plugins/flexicontent_fields/fcpagenav/tmpl/value_default.php
+++ plugins/flexicontent_fields/fcpagenav/tmpl/value_default.php
@@ -14,7 +14,7 @@ if ($use_category_link)
 	if (!empty($rows[$item->id]->categoryslug)) {
 		$tooltip = $use_tooltip ? ' title="'. flexicontent_html::getToolTip($category_label, $category->title, 0) .'"' : '';
 		$html .= '
-			<a class="fcpagenav-return btn' . ($use_tooltip ? ' '.$tooltip_class : '') . '" ' . ($use_tooltip ? $tooltip : '') . ' href="'. JRoute::_(FlexicontentHelperRoute::getCategoryRoute($rows[$item->id]->categoryslug)).'?start='.$start .'">
+			<a class="fcpagenav-return btn' . ($use_tooltip ? ' '.$tooltip_class : '') . '" ' . ($use_tooltip ? $tooltip : '') . ' href="'. JRoute::_(FlexicontentHelperRoute::getCategoryRoute($rows[$item->id]->categoryslug)) . ($start ? '?start='.$start : '') . '">
 				<i class="icon-undo"></i>
 				' . htmlspecialchars($category->title, ENT_NOQUOTES, 'UTF-8') . '
 				' . ($cat_image ? '
