01161a89470584980afd1d998493eaab68518a0a
ggppdk <ggppdk@gmail.com>
2017-04-28 09:39:56 +0300

    Corrected 'fcdate' form element to work with J3.7 (currently used in universal module configuration, at the date scope)

--- admin/elements/fcdate.php
+++ admin/elements/fcdate.php
@@ -23,7 +23,7 @@ jimport('cms.html.html');      // JHtml
 jimport('joomla.form.field');  // JFormField
 
 //jimport('joomla.form.helper'); // JFormHelper
-//JFormHelper::loadFieldClass('...');   // JFormField...
+//JFormHelper::loadFieldClass('calendar');   // JFormField...
 
 /**
  * Renders a date element
@@ -44,58 +44,66 @@ class JFormFieldFcdate extends JFormField
 
 	public function getInput()
 	{
-		$document = JFactory::getDocument();
-		
 		$node = & $this->element;
 		$attributes = get_object_vars($node->attributes());
 		$attributes = $attributes['@attributes'];
-		
-		$css  = '.calendar { vertical-align:middle; }';
-		$document->addStyleDeclaration($css);
-		
-		$value = FLEXI_J16GE ? $this->value : $value;
-		$fieldname	= FLEXI_J16GE ? $this->name : $control_name.'['.$name.']';
-		$element_id = FLEXI_J16GE ? $this->id : $control_name.$name;
-		$format = '%Y-%m-%d';
-		
-		$attribs = (@$attributes['size']) ? ' size="'.@$attributes['size'].'" ' : ' size="18" ';
-		
-		if ($class = @$attributes['class']) {
-			$attribs .= 'class="'.$class.'"';
+
+		$value = $this->value;
+		$fieldname	= $this->name;
+		$element_id = $this->id;
+		$date_format = @$attributes['date_format'] ? $attributes['date_format'] : '%Y-%m-%d';
+
+		$attribs_arr = array();
+
+		if ($class = @$attributes['class'])
+		{
+			$attribs_arr['class'] = $class;
 		}
-		
-		if ($placeholder = @$attributes['placeholder']) {
-			$attribs .= 'placeholder="'.JText::_($placeholder).'"';
+
+		if ($placeholder = @$attributes['placeholder'])
+		{
+			$attribs_arr['placeholder'] = $placeholder;
 		}
-		
-		$calendar_class = '';
-		if ($calendar_class = @$attributes['calendar_class']) {
-			$attribs .= 'placeholder="'.$calendar_class.'"';
-		}
-		
-		//return JHTML::_('calendar', $value, $fieldname, $element_id, $format, $attribs);
- 		return $this->calendar($value, $fieldname, $element_id, $format, $attribs, $calendar_class);
+
+		$attribs_arr['size']     = isset($attributes['size']) ? $attributes['size'] : 18;
+		$attribs_arr['showTime'] = isset($attributes['showTime']) ? $attributes['showTime'] : 0;
+		$calendar_class = isset($attributes['calendar_class']) ? $attributes['calendar_class'] : null;
+
+		//return JHTML::_('calendar', $value, $fieldname, $element_id, $format, $attribs_arr);
+		return $this->calendar($value, $attribs_arr['showTime'], $fieldname, $element_id, $attribs_arr, $skip_on_invalid=true, $timezone=false, $date_format);
 	}
-	
-	
-	function calendar($value, $name, $id, $format = '%Y-%m-%d', $attribs = null, $calendar_class)
+
+
+	// Method to create a calendar form field according to a given configuation
+	function calendar($value, $date_allowtime, $fieldname, $elementid, $attribs='', $skip_on_invalid=false, $timezone=false, $date_format='%Y-%m-%d')
 	{
-		JHTML::_('behavior.calendar'); //load the calendar behavior
-
-		if (is_array($attribs)) {
-			$attribs = JArrayHelper::toString( $attribs );
+		// 'false' timezone means ==> use server setting (=joomla site configured TIMEZONE),
+		// in J1.5 this must be null for using server setting (=joomla site configured OFFSET)
+		$timezone = ($timezone === false && !FLEXI_J16GE) ? null : $timezone;
+		
+		@list($date, $time) = preg_split('#\s+#', $value, $limit=2);
+		$time = ($date_allowtime==2 && !$time) ? '00:00' : $time;
+		
+		try {
+			// we check if date has no SYNTAX error (=being invalid) so use $gregorian = true,
+			// to avoid it being change according to CALENDAR of current user
+			// because user already entered the date in his/her calendar
+			if ( !$value ) {
+				$date = '';
+			} else if (!$date_allowtime || !$time) {
+				$date = JHTML::_('date',  $date, 'Y-m-d', $timezone, $gregorian = true);
+			} else {
+				$date = JHTML::_('date',  $value, 'Y-m-d H:i', $timezone, $gregorian = true);
+			}
+		} catch ( Exception $e ) {
+			if (!$skip_on_invalid) return '';
+			else $date = '';
 		}
-		$document = JFactory::getDocument();
-		$document->addScriptDeclaration('window.addEvent(\'domready\', function() {Calendar.setup({
-        inputField     :    "'.$id.'",     // id of the input field
-        ifFormat       :    "'.$format.'",      // format of the input field
-        button         :    "'.$id.'_img",  // trigger for the calendar (button ID)
-        align          :    "Tl",           // alignment (defaults to "Bl")
-        singleClick    :    true
-    });});');
-
-		return '<input type="text" name="'.$name.'" id="'.$id.'" value="'.htmlspecialchars($value, ENT_COMPAT, 'UTF-8').'" '.$attribs.' />'.
-				 '<img class="calendar '.$calendar_class.'" src="'.JURI::root(true).'/templates/system/images/calendar.png" alt="calendar" id="'.$id.'_img" />';
+		
+		// Create JS calendar
+		$time_formats_map = array('0'=>'', '1'=>' %H:%M', '2'=>' 00:00');
+		$date_time_format = $date_format . $time_formats_map[$date_allowtime];
+		$calendar = JHTML::_('calendar', $date, $fieldname, $elementid, $date_time_format, $attribs);
+		return $calendar;
 	}
-
 }
