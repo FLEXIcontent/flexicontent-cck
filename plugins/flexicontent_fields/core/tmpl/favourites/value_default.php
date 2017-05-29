c339e32321751e1a55466f4b6c181ae7760bb1a9
ggppdk <ggppdk@gmail.com>
2017-05-29 22:10:35 +0300

    Moved favourites layouts to more proper place

--- plugins/flexicontent_fields/core/tmpl/favourites/value_default.php
+++ plugins/flexicontent_fields/core/tmpl/favourites/value_default.php
@@ -1,9 +1,9 @@
 <?php
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
-$field_layouts_path = realpath(JPATH_ROOT.DS.'plugins'.DS.'flexicontent_fields'.DS.'core');
+$layouts_path = null; //realpath(JPATH_ROOT.DS.'plugins'.DS.'flexicontent_fields'.DS.'core');
 
-$layoutData = array(
+$displayData = array(
 	'field' => $field,
 	'item' => $item,
 	'favoured' => $favoured,
@@ -11,18 +11,18 @@ $layoutData = array(
 	'type' => 'item'
 );
 
-//ob_start();  include('layouts'.DS.'favicon.php');   $favicon  = ob_get_contents();  ob_end_clean();
-//ob_start();  include('layouts'.DS.'userlist.php');  $userlist = ob_get_contents();  ob_end_clean();
+//ob_start();  include(JPATH_SITE.DS.'components'.DS.'com_flexicontent'.DS.'layouts'.DS.'flexicontent_fields'.DS.'favourites'.DS.'favicon.php');   $favicon  = ob_get_contents();  ob_end_clean();
+//ob_start();  include(JPATH_SITE.DS.'components'.DS.'com_flexicontent'.DS.'layouts'.DS.'flexicontent_fields'.DS.'favourites'.DS.'userlist.php');  $userlist = ob_get_contents();  ob_end_clean();
 
 $field->{$prop} =
 	$pretext . '
 		<div class="fav-block">
-			' . JLayoutHelper::render('tmpl.favourites.layouts.favicon', $layoutData, $field_layouts_path) .'
+			' . JLayoutHelper::render('flexicontent_fields.favourites.favicon', $displayData, $layouts_path) .'
 			<div id="fcfav-reponse_item_'.$item->id.'" class="fcfav-reponse-tip">
 				<div class="fc-mssg fc-info fc-iblock fc-nobgimage '.($favoured ? 'fcfavs-is-subscriber' : 'fcfavs-isnot-subscriber').'">
 					' . JText::_($favoured ? 'FLEXI_FAVS_YOU_HAVE_SUBSCRIBED' : 'FLEXI_FAVS_CLICK_TO_SUBSCRIBE') . '
 				</div>
-				'. JLayoutHelper::render('tmpl.favourites.layouts.userlist', $layoutData, $field_layouts_path) .'
+				'. JLayoutHelper::render('flexicontent_fields.favourites.userlist', $displayData, $layouts_path) .'
 			</div>
 		</div>
 	' . $posttext;
