48133b78317c1b0b972813608763dcaa5989c54a
Georgios Papadakis <ggppdk@users.noreply.github.com>
2017-05-29 10:20:25 +0300

    Load favourites icon and userlist directly from the new layout files

--- plugins/flexicontent_fields/core/tmpl/favourites/value_default.php
+++ plugins/flexicontent_fields/core/tmpl/favourites/value_default.php
@@ -1,17 +1,25 @@
 <?php
+defined( '_JEXEC' ) or die( 'Restricted access' );
 
-// DO NOT OVERRIDE THIS FILE YET !!
-// API Methods will change !! and their HTML / JS will be moved here
+ob_start();
+include ('layouts'.DS.'$favicon.php');
+$favicon = ob_get_contents();
+ob_end_clean();
+
+ob_start();
+include ('layouts'.DS.'userlist.php');
+$userlist = ob_get_contents();
+ob_end_clean();
 
 $field->{$prop} =
 	$pretext . '
 		<div class="fav-block">
-			'.flexicontent_html::favicon( $field, $favoured, $item ).'
+			' . $favicon .'
 			<div id="fcfav-reponse_item_'.$item->id.'" class="fcfav-reponse-tip">
 				<div class="fc-mssg fc-info fc-iblock fc-nobgimage '.($favoured ? 'fcfavs-is-subscriber' : 'fcfavs-isnot-subscriber').'">
 					'.JText::_($favoured ? 'FLEXI_FAVS_YOU_HAVE_SUBSCRIBED' : 'FLEXI_FAVS_CLICK_TO_SUBSCRIBE').'
 				</div>
-				'. flexicontent_html::favoured_userlist( $field, $item, $favourites) .'
+				'. $userlist .'
 			</div>
 		</div>
 	' . $posttext;
