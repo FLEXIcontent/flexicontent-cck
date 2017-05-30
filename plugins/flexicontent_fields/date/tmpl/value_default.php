dd0f9b11622cbbba4bee887ad9b8083ec307227b
ggppdk <ggppdk@gmail.com>
2017-05-31 00:20:13 +0300

    Minor code cleanups for fields layouts

--- plugins/flexicontent_fields/date/tmpl/value_default.php
+++ plugins/flexicontent_fields/date/tmpl/value_default.php
@@ -3,30 +3,42 @@
 $n = 0;
 foreach ($values as $value)
 {
-	if ( !strlen($value) && !$is_ingroup ) continue; // Skip empty if not in field group
-	if ( !strlen($value) ) {
-		$field->{$prop}[$n++]	= $no_value_msg;
+	// If time is not allowed then disgard it from the value
+	if ( strlen($value) && !$date_allowtime )
+	{
+		@ list($date, $time) = preg_split('#\s+#', $value, $limit=2);
+	}
+	else
+	{
+		$date = $value;
+	}
+
+	// Try to parse the date string
+	if ( !empty($date) )
+	{
+		try {
+			$date = JHTML::_('date', $date, $dateformat, $timezone ).$tz_info;
+		} catch ( Exception $e ) {
+			$date = '';
+		}
+	}
+
+	// Skip empty value, adding an empty placeholder if field inside in field group
+	if ( empty($date) )
+	{
+		if ( $is_ingroup )
+		{
+			$field->{$prop}[$n++]	= $no_value_msg;
+		}
 		continue;
 	}
-	
-	// Check if dates are allowed to have time part
-	if ($date_allowtime) $date = $value;
-	else @list($date, $time) = preg_split('#\s+#', $value, $limit=2);
-	
-	if ( empty($date) ) continue;
-	
-	try {
-		$date = JHTML::_('date', $date, $dateformat, $timezone ).$tz_info;
-	} catch ( Exception $e ) {
-		$date = '';
-	}
-	
+
 	// Add prefix / suffix
-	$field->{$prop}[$n]	= $pretext.$date.$posttext;
-	
+	$field->{$prop}[$n]	= $pretext . $date . $posttext;
+
 	// Add microdata to every value if field -- is -- in a field group
 	if ($is_ingroup && $itemprop) $field->{$prop}[$n] = '<div style="display:inline" itemprop="'.$itemprop.'" >' .$field->{$prop}[$n]. '</div>';
-	
+
 	$n++;
 	if (!$multiple) break;  // multiple values disabled, break out of the loop, not adding further values even if the exist
 }
