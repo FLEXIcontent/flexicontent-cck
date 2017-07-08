148a3056f296f999b9bb75dd4e306c4056e8c155
ggppdk <ggppdk@gmail.com>
2017-07-08 07:46:48 +0300

    Allowed filtered menuitem parameter elements to deselect currently selected option

--- admin/elements/fcmenuitem.php
+++ admin/elements/fcmenuitem.php
@@ -16,31 +16,165 @@
  * GNU General Public License for more details.
  */
 
-// Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die('Restricted access');
+defined('JPATH_PLATFORM') or die;
 
-jimport('cms.html.html');      // JHtml
-jimport('cms.html.select');    // JHtmlSelect
+JFormHelper::loadFieldClass('groupedlist');
 
-jimport('joomla.form.helper'); // JFormHelper
-JFormHelper::loadFieldClass('menuitem');   // JFormFieldMenuitem
+// Import the com_menus helper.
+require_once realpath(JPATH_ADMINISTRATOR . '/components/com_menus/helpers/menus.php');
 
 /**
- * Renders a types element
+ * Supports an HTML grouped select list of menu item grouped by menu
  *
  * @package 	Joomla
  * @subpackage	FLEXIcontent
  * @since		1.5
  */
-class JFormFieldFcMenuitem extends JFormFieldMenuitem
+class JFormFieldFcMenuitem extends JFormFieldGroupedList
 {
 	/**
-	 * Element name
-	 * @access	protected
-	 * @var		string
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  1.6
 	 */
-	var	$type = 'FcMenuitem';
+	public $type = 'FcMenuitem';
 
+	/**
+	 * The menu type.
+	 *
+	 * @var    string
+	 * @since  3.2
+	 */
+	protected $menuType;
+
+	/**
+	 * The client id.
+	 *
+	 * @var    string
+	 * @since  3.2
+	 */
+	protected $clientId;
+
+	/**
+	 * The language.
+	 *
+	 * @var    array
+	 * @since  3.2
+	 */
+	protected $language;
+
+	/**
+	 * The published status.
+	 *
+	 * @var    array
+	 * @since  3.2
+	 */
+	protected $published;
+
+	/**
+	 * The disabled status.
+	 *
+	 * @var    array
+	 * @since  3.2
+	 */
+	protected $disable;
+
+	/**
+	 * Method to get certain otherwise inaccessible properties from the form field object.
+	 *
+	 * @param   string  $name  The property name for which to the the value.
+	 *
+	 * @return  mixed  The property value or null.
+	 *
+	 * @since   3.2
+	 */
+	public function __get($name)
+	{
+		switch ($name)
+		{
+			case 'menuType':
+			case 'clientId':
+			case 'language':
+			case 'published':
+			case 'disable':
+				return $this->$name;
+		}
+
+		return parent::__get($name);
+	}
+
+	/**
+	 * Method to set certain otherwise inaccessible properties of the form field object.
+	 *
+	 * @param   string  $name   The property name for which to the the value.
+	 * @param   mixed   $value  The value of the property.
+	 *
+	 * @return  void
+	 *
+	 * @since   3.2
+	 */
+	public function __set($name, $value)
+	{
+		switch ($name)
+		{
+			case 'menuType':
+				$this->menuType = (string) $value;
+				break;
+
+			case 'clientId':
+				$this->clientId = (int) $value;
+				break;
+
+			case 'language':
+			case 'published':
+			case 'disable':
+				$value = (string) $value;
+				$this->$name = $value ? explode(',', $value) : array();
+				break;
+
+			default:
+				parent::__set($name, $value);
+		}
+	}
+
+	/**
+	 * Method to attach a JForm object to the field.
+	 *
+	 * @param   SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
+	 * @param   mixed             $value    The form field value to validate.
+	 * @param   string            $group    The field name group control value. This acts as an array container for the field.
+	 *                                      For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                      full field name would end up being "bar[foo]".
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @see     JFormField::setup()
+	 * @since   3.2
+	 */
+	public function setup(SimpleXMLElement $element, $value, $group = null)
+	{
+		$result = parent::setup($element, $value, $group);
+
+		if ($result === true)
+		{
+			$this->menuType  = (string) $this->element['menu_type'];
+			$this->clientId  = (int) $this->element['client_id'];
+			$this->published = $this->element['published'] ? explode(',', (string) $this->element['published']) : array();
+			$this->disable   = $this->element['disable'] ? explode(',', (string) $this->element['disable']) : array();
+			$this->language  = $this->element['language'] ? explode(',', (string) $this->element['language']) : array();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Method to get the field option groups.
+	 *
+	 * @return  array  The field option objects as a nested array in groups.
+	 *
+	 * @since   1.6
+	 */
 	protected function getGroups()
 	{
 		static $comp_items = array();
@@ -50,9 +184,12 @@ class JFormFieldFcMenuitem extends JFormFieldMenuitem
 		$link_filters = empty($this->element['link_filters']) ? false : explode('%%', (string)$this->element['link_filters']);
 		
 		// Get the menu items.
-		if ( !$menuType && $component && isset($comp_items[$component]) ) {
+		if ( !$menuType && $component && isset($comp_items[$component]) )
+		{
 			$items = & $comp_items[$component];
-		} else {
+		}
+		else
+		{
 			//$items = MenusHelper::getMenuLinks($menuType, 0, 0, $this->published, $this->language);
 			$items = $this->getMenuLinks($menuType, 0, 0, $this->published, $this->language);
 			
@@ -90,7 +227,8 @@ class JFormFieldFcMenuitem extends JFormFieldMenuitem
 			foreach ($items as $link)
 			{
 				$skip = 0;
-				if ($link_filters && $this->value != $link->value) foreach($link_filters as $filter_text) {
+				if ($link_filters && $this->value != $link->value) foreach($link_filters as $filter_text)
+				{
 					if ($filter_text[0]=='!') {
 						if (strstr($link->url, substr($filter_text, 1)) !== false) { $skip=1; break; }
 					} else {
@@ -111,7 +249,6 @@ class JFormFieldFcMenuitem extends JFormFieldMenuitem
 		// Build groups for all menu types.
 		else
 		{
-			
 			// Build the groups arrays.
 			foreach ($items as $menu)
 			{
@@ -142,7 +279,7 @@ class JFormFieldFcMenuitem extends JFormFieldMenuitem
 		}
 
 		// Merge any additional groups in the XML definition.
-		//$groups = array_merge(parent::getGroups(), $groups);
+		$groups = array_merge(parent::getGroups(), $groups);
 
 		return $groups;
 	}
