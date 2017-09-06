3c73e5c29d0487703189a07ca4142be80103bd38
ggppdk <ggppdk@gmail.com>
2017-09-06 12:22:26 +0300

    Moved all dashboard tasks out of main controller into a new dashboard specific controller named 'flexicontent'

--- admin/views/flexicontent/view.html.php
+++ admin/views/flexicontent/view.html.php
@@ -45,15 +45,7 @@ class FlexicontentViewFlexicontent extends JViewLegacy
 		$user     = JFactory::getUser();		
 		$db       = JFactory::getDbo();
 		$print_logging_info = $params->get('print_logging_info');
-		
-		// Special displaying when getting flexicontent version
-		$layout = $app->input->get('layout', '', 'CMD');
-		if($layout=='fversion')
-		{
-			$this->fversion($tpl, $params);
-			return;
-		}
-		
+
 		// Load the file system librairies
 		jimport('joomla.filesystem.folder');
 		jimport('joomla.filesystem.file');
@@ -119,7 +111,7 @@ class FlexicontentViewFlexicontent extends JViewLegacy
 			$deprecatedfiles	= $model->getDeprecatedFiles();
 			$nooldfieldsdata	= $model->getNoOldFieldsData();
 			$missingversion		= true; //!$use_versioning || !$model->checkCurrentVersionData();
-			$cachethumb				= $model->getCacheThumbChmod();
+			$cachethumb				= $model->getCacheThumbPerms();
 			
 			$existdbindexes    = ! (boolean) ($missingindexes = $model->getExistDBindexes($check_only=false));
 			$itemcountingdok   = $model->getItemCountingDataOK();
@@ -183,7 +175,7 @@ class FlexicontentViewFlexicontent extends JViewLegacy
 			if (0) // FLEXI_J37GE
 			{
 				$btn_task = '';
-				$popup_load_url = JUri::base().'index.php?option=com_flexicontent&layout=import&tmpl=component';
+				$popup_load_url = JUri::base().'index.php?option=com_flexicontent&layout=import&format=raw';
 				//$toolbar->appendButton('Popup', 'download', JText::_('FLEXI_IMPORT_JOOMLA'), str_replace('&', '&amp;', $popup_load_url), 780, 500);
 				$js .= "
 					jQuery('#toolbar-download a.toolbar, #toolbar-download button').attr('href', '".$popup_load_url."')
@@ -230,7 +222,7 @@ class FlexicontentViewFlexicontent extends JViewLegacy
 		$lists['languages'] = JHtml::_('select.genericlist', $options, 'lang', '', 'value', 'text', $activelang);
 
 		// Missing files
-		$lists['missing_lang'] = $model->processlanguagefiles();
+		$lists['missing_lang'] = $model->createLanguagePack();
 
 		// Get the default copyright values to populate the form automatically
 		$mailfrom = $app->getCfg('mailfrom');
@@ -287,9 +279,9 @@ class FlexicontentViewFlexicontent extends JViewLegacy
 
 		$this->sidebar = FLEXI_J30GE ? JHtmlSidebar::render() : null;
 		parent::display($tpl);
-
 	}
-	
+
+
 	/**
 	 * Creates the buttons view
 	 *
@@ -318,126 +310,4 @@ class FlexicontentViewFlexicontent extends JViewLegacy
 		</span>
 		<?php
 	}
-	
-	
-	/**
-	 * Fetch the version from the flexicontent.org server
-	 */
-	static function getUpdateComponent()
-	{
-		// Read installation file
-		$manifest_path = JPATH_ADMINISTRATOR .DS. 'components' .DS. 'com_flexicontent' .DS. 'manifest.xml';
-		$com_xml = JInstaller::parseXMLInstallFile( $manifest_path );
-		
-		// Version checking URL
-		$url = 'http://www.flexicontent.org/flexicontent_update.xml';
-		$data = '';
-		$check = array();
-		$check['connect'] = 0;
-		$check['current_version'] = $com_xml['version'];
-		$check['current_creationDate'] = $com_xml['creationDate'];
-
-		//try to connect via cURL
-		if (function_exists('curl_init') && function_exists('curl_exec')) {
-			$ch = @curl_init();
-			
-			@curl_setopt($ch, CURLOPT_URL, $url);
-			@curl_setopt($ch, CURLOPT_HEADER, 0);
-			//http code is greater than or equal to 300 ->fail
-			@curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-			@curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			//timeout of 5s just in case
-			@curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-						
-			$data = @curl_exec($ch);
-						
-			@curl_close($ch);
-		}
-		
-		//try to connect via fsockopen
-		if(function_exists('fsockopen') && $data == '') {
-
-			$errno = 0;
-			$errstr = '';
-
-			//timeout handling: 5s for the socket and 5s for the stream = 10s
-			$fsock = @fsockopen("www.flexicontent.org", 80, $errno, $errstr, 5);
-			//$fsock = @fsockopen("flexicontent.googlecode.com", 80, $errno, $errstr, 5);
-		
-			if ($fsock) {
-				@fputs($fsock, "GET /flexicontent_update.xml HTTP/1.1\r\n");
-				@fputs($fsock, "HOST: www.flexicontent.org\r\n");
-				@fputs($fsock, "Connection: close\r\n\r\n");
-				
-				//force stream timeout...
-				@stream_set_blocking($fsock, 1);
-				@stream_set_timeout($fsock, 5);
-				 
-				$get_info = false;
-				while (!@feof($fsock))
-				{
-					if ($get_info)
-					{
-						$data .= @fread($fsock, 1024);
-					}
-					else
-					{
-						if (@fgets($fsock, 1024) == "\r\n")
-						{
-							$get_info = true;
-						}
-					}
-				}
-				@fclose($fsock);
-				
-				//need to check data cause http error codes aren't supported here
-				if(!strstr($data, '<?xml version="1.0" encoding="utf-8"?><update>')) {
-					$data = '';
-				}
-			}
-		}
-
-		//try to connect via fopen
-		if (function_exists('fopen') && ini_get('allow_url_fopen') && $data == '') {
-			
-			//set socket timeout
-			ini_set('default_socket_timeout', 5);
-			
-			$handle = @fopen ($url, 'r');
-			
-			//set stream timeout
-			@stream_set_blocking($handle, 1);
-			@stream_set_timeout($handle, 5);
-			
-			$data	= @fread($handle, 1000);
-			
-			@fclose($handle);
-		}
-		
-		if( $data && strstr($data, '<?xml') )
-		{
-			$xml = simplexml_load_string($data);
-			$check['version']  = (string)$xml->version;
-			$check['released'] = (string)$xml->released;
-			$check['connect']  = 1;
-			$check['enabled']  = 1;
-			$check['current']  = version_compare( str_replace(' ', '', $check['current_version']), str_replace(' ', '', $check['version']) );
-		}
-		
-		return $check;
-	}
-	
-	
-	function fversion(&$tpl, &$params)
-	{
-		// Cache update check of FLEXIcontent version
-		$cache = JFactory::getCache('com_flexicontent');
-		$cache->setCaching( 1 );
-		$cache->setLifeTime( 3600 );  // Set expire time (hard-code this to 1 hour), to avoid server load
-		$check = $cache->get(array( 'FlexicontentViewFlexicontent', 'getUpdateComponent'), array('component'));
-		$this->check = $check;
-		
-		parent::display($tpl);
-	}
 }
-?>
---
+++ admin/views/flexicontent/view.raw.php
@@ -0,0 +1,179 @@
+<?php
+/**
+ * @version 1.5 stable $Id: view.html.php 1900 2014-05-03 07:25:51Z ggppdk $
+ * @package Joomla
+ * @subpackage FLEXIcontent
+ * @copyright (C) 2009 Emmanuel Danan - www.vistamedia.fr
+ * @license GNU/GPL v2
+ * 
+ * FLEXIcontent is a derivative work of the excellent QuickFAQ component
+ * @copyright (C) 2008 Christoph Lukes
+ * see www.schlu.net for more information
+ *
+ * FLEXIcontent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport('legacy.view.legacy');
+
+/**
+ * HTML View class for the FLEXIcontent View
+ *
+ * @package Joomla
+ * @subpackage FLEXIcontent
+ * @since 1.0
+ */
+class FlexicontentViewFlexicontent extends JViewLegacy
+{
+	/**
+	 * Creates the Entrypage
+	 *
+	 * @since 1.0
+	 */
+	function display( $tpl = null )
+	{
+		$app      = JFactory::getApplication();
+		$config   = JFactory::getConfig();
+		$params   = JComponentHelper::getParams('com_flexicontent');
+		$document	= JFactory::getDocument();
+		$session  = JFactory::getSession();
+		$user     = JFactory::getUser();		
+		$db       = JFactory::getDbo();
+		$print_logging_info = $params->get('print_logging_info');
+		
+		// Special displaying when getting flexicontent version
+		$layout = $app->input->get('layout', '', 'CMD');
+		if ($layout=='fversion')
+		{
+			$this->fversion($tpl, $params);
+		}
+
+		// Raw output
+		parent::display($tpl);
+	}
+
+
+	/**
+	 * Fetch the version from the flexicontent.org server
+	 */
+	static function getUpdateComponent()
+	{
+		// Read installation file
+		$manifest_path = JPATH_ADMINISTRATOR .DS. 'components' .DS. 'com_flexicontent' .DS. 'manifest.xml';
+		$com_xml = JInstaller::parseXMLInstallFile( $manifest_path );
+		
+		// Version checking URL
+		$url = 'http://www.flexicontent.org/flexicontent_update.xml';
+		$data = '';
+		$check = array();
+		$check['connect'] = 0;
+		$check['current_version'] = $com_xml['version'];
+		$check['current_creationDate'] = $com_xml['creationDate'];
+
+		//try to connect via cURL
+		if (function_exists('curl_init') && function_exists('curl_exec')) {
+			$ch = @curl_init();
+			
+			@curl_setopt($ch, CURLOPT_URL, $url);
+			@curl_setopt($ch, CURLOPT_HEADER, 0);
+			//http code is greater than or equal to 300 ->fail
+			@curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+			@curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			//timeout of 5s just in case
+			@curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+						
+			$data = @curl_exec($ch);
+						
+			@curl_close($ch);
+		}
+		
+		//try to connect via fsockopen
+		if(function_exists('fsockopen') && $data == '') {
+
+			$errno = 0;
+			$errstr = '';
+
+			//timeout handling: 5s for the socket and 5s for the stream = 10s
+			$fsock = @fsockopen("www.flexicontent.org", 80, $errno, $errstr, 5);
+			//$fsock = @fsockopen("flexicontent.googlecode.com", 80, $errno, $errstr, 5);
+		
+			if ($fsock) {
+				@fputs($fsock, "GET /flexicontent_update.xml HTTP/1.1\r\n");
+				@fputs($fsock, "HOST: www.flexicontent.org\r\n");
+				@fputs($fsock, "Connection: close\r\n\r\n");
+				
+				//force stream timeout...
+				@stream_set_blocking($fsock, 1);
+				@stream_set_timeout($fsock, 5);
+				 
+				$get_info = false;
+				while (!@feof($fsock))
+				{
+					if ($get_info)
+					{
+						$data .= @fread($fsock, 1024);
+					}
+					else
+					{
+						if (@fgets($fsock, 1024) == "\r\n")
+						{
+							$get_info = true;
+						}
+					}
+				}
+				@fclose($fsock);
+				
+				//need to check data cause http error codes aren't supported here
+				if(!strstr($data, '<?xml version="1.0" encoding="utf-8"?><update>')) {
+					$data = '';
+				}
+			}
+		}
+
+		//try to connect via fopen
+		if (function_exists('fopen') && ini_get('allow_url_fopen') && $data == '') {
+			
+			//set socket timeout
+			ini_set('default_socket_timeout', 5);
+			
+			$handle = @fopen ($url, 'r');
+			
+			//set stream timeout
+			@stream_set_blocking($handle, 1);
+			@stream_set_timeout($handle, 5);
+			
+			$data	= @fread($handle, 1000);
+			
+			@fclose($handle);
+		}
+		
+		if( $data && strstr($data, '<?xml') )
+		{
+			$xml = simplexml_load_string($data);
+			$check['version']  = (string)$xml->version;
+			$check['released'] = (string)$xml->released;
+			$check['connect']  = 1;
+			$check['enabled']  = 1;
+			$check['current']  = version_compare( str_replace(' ', '', $check['current_version']), str_replace(' ', '', $check['version']) );
+		}
+		
+		return $check;
+	}
+
+
+	function fversion(&$params)
+	{
+		// Cache update check of FLEXIcontent version
+		$cache = JFactory::getCache('com_flexicontent');
+		$cache->setCaching( 1 );
+		$cache->setLifeTime( 3600 );  // Set expire time (hard-code this to 1 hour), to avoid server load
+		$check = $cache->get(array( 'FlexicontentViewFlexicontent', 'getUpdateComponent'), array('component'));
+		$this->check = $check;
+	}
+}
+?>
