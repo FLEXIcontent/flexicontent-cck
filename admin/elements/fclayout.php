e2592269fe062b01f2be85f09e65c650185fb876
ggppdk <ggppdk@gmail.com>
2017-09-20 06:32:18 +0300

    Added per gallery layout unique configuration (XML file, AJAX loaded) to image-gallery field (currently these are empty)

--- admin/elements/fclayout.php
+++ admin/elements/fclayout.php
@@ -83,8 +83,19 @@ class JFormFieldFclayout extends JFormFieldList
 		$filter   = (string) @ $attributes['filter'];
 		$exclude  = (string) @ $attributes['exclude'];
 		$stripExt = (string) @ $attributes['stripext'];
+		$stripPrefix = (string) @ $attributes['stripprefix'];
 		$hideNone    = (string) @ $attributes['hide_none'];
 		$hideDefault = (string) @ $attributes['hide_default'];
+
+		$icon_class = (string) @ $attributes['icon_class'];
+		$icon_class = $icon_class ?: 'icon-palette';
+		
+		$custom_layouts_label  = (string) @ $attributes['custom_layouts_label'];
+		$use_default_label  = (string) @ $attributes['use_default_label'];
+		$use_default_label  = $use_default_label ?: 'JOPTION_USE_DEFAULT';
+		
+		$layout_label = (string) @ $attributes['layout_label'];
+		$layout_label = JText::_($layout_label ?: 'FLEXI_LAYOUT');
 		
 		// Get the path which contains layouts
 		$directory = (string) @ $attributes['directory'];
@@ -98,38 +109,142 @@ class JFormFieldFclayout extends JFormFieldList
 		$groups = array();
 		$groups['_'] = array();
 		$groups['_']['id'] = $this->id . '__';
-		$groups['_']['text'] = $view=='module' ? JText::sprintf('JOPTION_FROM_MODULE') : 'Layouts';
+		$groups['_']['text'] = $view=='module'
+			? JText::sprintf('JOPTION_FROM_MODULE')
+			: '';
 		$groups['_']['items'] = array();
 
 		// Prepend some default options based on field attributes.
 		if (!$hideNone)   $groups['_']['items'][] = JHtml::_('select.option', '-1', JText::alt('JOPTION_DO_NOT_USE', preg_replace('/[^a-zA-Z0-9_\-]/', '_', $this->fieldname)));
-		if (!$hideDefault) $groups['_']['items'][] = JHtml::_('select.option', '', JText::alt('JOPTION_USE_DEFAULT', preg_replace('/[^a-zA-Z0-9_\-]/', '_', $this->fieldname)));
-		
-		// Get a list of files in the search path with the given filter.
-		$files = JFolder::files($path, $filter);
-		
-		$module_layouts = array();
-		// Build the options list from the list of files.
-		if ( is_array($files) )  foreach ($files as $file)
-		{
-			// Check to see if the file is in the exclude mask.
-			if ( $exclude && preg_match(chr(1) . $exclude . chr(1), $file) )  continue;
-			
-			// If the extension is to be stripped, do it.
-			if ($stripExt)  $file = JFile::stripExt($file);
-			
-			$groups['_']['items'][] = JHtml::_('select.option', $file, $file);
-			$module_layouts[] = $file;
-		}
-		// Merge any additional options in the XML definition.
-		//if (FLEXI_J16GE) $options = array_merge(parent::getOptions(), $options);
-		// Merge any additional options in the XML definition.
+		if (!$hideDefault) $groups['_']['items'][] = JHtml::_('select.option', '', JText::alt($use_default_label, preg_replace('/[^a-zA-Z0-9_\-]/', '_', $this->fieldname)));
+
+		// ***
+		// *** Get any additional options in the XML definition.
+		// ***
+
+		/*
 		$options = parent::getOptions();
-		if(count($options)>0) {
+		if (count($options)>0)
+		{
 			$groups['extended'] = array();
 			$groups['extended']['id'] = $this->id . '_extended';
-			$groups['extended']['text'] = JText::sprintf('From xml options elements');
+			$groups['extended']['text'] =  JText::_('Built-in');
 			$groups['extended']['items'] = $options;
+		}
+		*/
+
+		$core_layout_names = array();
+
+		$num_index = 0;
+		$last_was_grp = false;
+		foreach ($this->element->children() as $option)
+		{
+			$name = $option->getName();   //echo 'Name: ' . $name . '<pre>' . print_r($option, true) .'</pre>'; exit;
+
+			// If current option is group then iterrate through its children, otherwise create single value array
+			$children = $name=="group"
+				? $option->children()
+				: array( & $option );
+
+			$_options = array();
+			foreach ($children as $sub_option)
+			{
+				$val  = (string) $sub_option->attributes()->value;
+				$text = JText::_( (string) $sub_option );
+				$attr_arr = array();
+
+				if (isset($sub_option->attributes()->filename))
+				{
+					$filename = (string) $sub_option->attributes()->filename;
+					$attr_arr['data-filename']  = $filename;
+					$attr_arr['data-filename']  = $filename;
+					$core_layout_names[$filename] = $val;
+				}
+
+				$disable = $sub_option->attributes()->disable ? true : false;
+				$hidden = $sub_option->attributes()->hidden ? true : false;
+				if ($hidden)
+				{
+					continue;
+				}
+
+				$_options[] = (object) array(
+					'value' => $val,
+					'text'  => $text,
+					'disable' => $disable,
+					'attr'  => $attr_arr
+				);
+				$V2L[$val] = $text;
+				//print_r($attr_arr);
+			}
+
+			// Check for current option is a GROUP
+			if ($name=="group")
+			{
+				$grp = $option->attributes()->name ?: $option->attributes()->label;
+				$grp = (string) $grp;
+				$groups[$grp] = array();
+				$groups[$grp]['id'] = null;
+				$groups[$grp]['text'] = JText::_($option->attributes()->label);
+				$groups[$grp]['items'] = $_options;
+				$last_was_grp = true;
+			}
+			else
+			{
+				$num_index = !$last_was_grp ? $num_index : ($num_index + 1);
+				$groups[$num_index]['items'][] = reset($_options);
+				$last_was_grp = false;
+			}
+		}
+
+
+		// *** 
+		// *** Get a list of files in the search path with the given filter.
+		// ***
+
+		$files = JFolder::files($path, $filter);
+		$files = is_array($files) ? $files : array();
+		$files = array_flip($files);
+
+		// Build the options list from the list of files.
+		$groups['custom'] = array();
+		$groups['custom']['id'] = $this->id . '_custom';
+		$groups['custom']['text'] = $view=='module'
+			? JText::sprintf('JOPTION_FROM_MODULE')
+			: JText::_($custom_layouts_label ?: 'FLEXI_LAYOUTS');
+		$groups['custom']['items'] = array();
+
+		$layout_files = array();
+		foreach ($files as $file => $index)
+		{
+			// Check to see if the file is in the exclude mask.
+			if ( $exclude && preg_match(chr(1) . $exclude . chr(1), $file) )
+			{
+				continue;
+			}
+
+			// If the extension is to be stripped, do it.
+			if ($stripExt)
+			{
+				$file = JFile::stripExt($file);
+			}
+
+			if (isset($core_layout_names[$file]))
+			{
+				continue;
+			}
+
+			$text = $stripPrefix ? str_replace($stripPrefix, '', $value) : $file;
+
+			$groups['custom']['items'][] = (object) array('text' => $text, 'value' => $file);
+			$layout_files[] = $file;
+
+		}
+
+		// If none non core files were found then remove the group
+		if (!count($layout_files))
+		{
+			unset($groups['custom']);
 		}
 		
 		
@@ -215,7 +330,7 @@ class JFormFieldFclayout extends JFormFieldList
 						foreach ($files as $i => $file)
 						{
 							// Remove layout that already exist in component ones
-							if (in_array(basename($file, '.php'), $module_layouts))
+							if (in_array(basename($file, '.php'), $layout_files))
 							{
 								unset($files[$i]);
 							}
@@ -258,15 +373,15 @@ class JFormFieldFclayout extends JFormFieldList
 		} else {
 			$attribs .= 'class="inputbox"';
 		}
-		$attribs .= ' onchange="fc_getLayout(this);"';
+		$attribs .= ' onchange="fc_getLayout_'.$_name.'(this);"';
 		
 		
 		// Container of parameters
 		$tmpl_container = (string) @ $attributes['tmpl_container'];
+
 		// Add JS code to display parameters, either via 'file' or 'inline'
 		// For modules we can not use method 'file' (external xml file), because J2.5+ does form validation on the XML file ...
 		$params_source = (string) @ $attributes['params_source'];
-		$container_sx = FLEXI_J16GE ? '-options' : '-page';
 
 flexicontent_html::loadJQuery();
 if ( ! @$attributes['skipparams'] ) {
@@ -275,39 +390,54 @@ if ( ! @$attributes['skipparams'] ) {
 
 ".($params_source=="file" ? "
 
-function fc_getLayout(el)
+function fc_getLayout_".$_name."(el)
 {
-	var container = jQuery('#".$tmpl_container.$container_sx."');
- 	var container2 = jQuery('a[href=\"#attrib-".$tmpl_container."\"]');
- 	
-  // *** Hide layout container
-	//if (container) container.parent().css('display', 'none');
- 	//if (container2) container2.parent().css('display', 'none');
+ 	var bs_tab_handle = jQuery('a[href=\"#attrib-".$tmpl_container."\"]');
+ 	var fc_tab_handle = jQuery('#".$tmpl_container."');
+
+	// First try Joomla bootstrap TAB handle with 'attrib-' prefix for its ID
+	var panel_header = bs_tab_handle;
+	var panel_id = 'attrib-".$tmpl_container."';
+	var panel = jQuery('#'+panel_id);
 	
-	var panel;
-	var panel_id;
-	var panel_header = container;
-	if (panel_header) {
-		panel_id = '".$tmpl_container.$container_sx."';
-		panel = panel_header.next();
+	// Second try FC TAB handle
+	if (!panel_header.length && fc_tab_handle.length)
+	{
+		panel_header = fc_tab_handle;
+		panel = panel_header.closest('.tabberlive').children().eq( panel_header.parent().index() + 1 );
+		panel_id = panel.attr('id');
 	}
 	
-	if (panel_header.length==0 && container2.length>0) {
-		panel_header = container2;
-		panel_id = 'attrib-".$tmpl_container."';
-		panel = jQuery('#'+panel_id);
+	if (!panel_header.length)
+	{
+		alert('Layout container: ".$tmpl_container." not found');
+		return;
 	}
 	
-	var layout_name = el.value;
+	var filename = jQuery(el).find(':selected').data('filename');
+	var layout_name = filename ? filename : el.value;
+
 	var _loading_img = '<img src=\"components/com_flexicontent/assets/images/ajax-loader.gif\" align=\"center\">';
-	panel_header.html('<a href=\"javascript:void(0);\"><span>Layout: '+_loading_img+'</span></a>');
+	bs_tab_handle.length
+		? panel_header.html('<a href=\"javascript:void(0);\"><span> " . $layout_label . ": '+_loading_img+'</span></a>')
+		: panel_header.html('<i class=\"".$icon_class."\"></i> " . $layout_label . ": ' + _loading_img);
 	panel.html('');
+
 	jQuery.ajax({
 		type: 'GET',
 		url: 'index.php?option=com_flexicontent&task=templates.getlayoutparams&ext_option=".$option."&ext_view=".$view."&ext_name=".$ext_name."&ext_id=".$pk."&directory=".$directory."&layout_name='+layout_name+'&format=raw',
 		success: function(str) {
-			panel_header.html('<a href=\"javascript:void(0);\"><span>Layout: '+layout_name+'</span></a>');
-		 	panel_header.parent().css('display', '');
+			if (bs_tab_handle.length)
+			{
+				panel_header.html('<a href=\"javascript:void(0);\"><span> " . $layout_label . ": '+layout_name+'</span></a>');
+				panel_header.parent().css('display', '');
+			}
+			else
+			{
+				var display_name = layout_name.replace('value_', '').replace('gallery_', '');
+				display_name = display_name.replace('_', ' ').replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+				panel_header.html('<i class=\"".$icon_class."\"></i> " . $layout_label . ": ' + display_name);
+		 	}
 			panel.html(str);
 			panel.find('.hasTooltip').tooltip({html: true, container: panel});
 			panel.find('.hasPopover').popover({html: true, container: panel, trigger : 'hover focus'});
@@ -323,46 +453,34 @@ function fc_getLayout(el)
 
 ":"
 
-function fc_getLayout(el)
+function fc_getLayout_".$_name."(el)
 {
   // *** Hide default container
-	var container = $('".$tmpl_container.$container_sx."');
-	if (container) container.getParent().setStyle('display', 'none');
-	
-	".( FLEXI_J30GE ? "
  	var container = jQuery('a[href=\"#attrib-".$tmpl_container."\"]');
  	if (container) container.parent().css('display', 'none');
- 	" : "")."
 	
+	var filename = jQuery(el).find(':selected').data('filename');
+	var layout_name = filename ? filename : el.value;
+
   // *** Hide ALL containers
-  var layout_name = el.value;
-	var layouts = new Array('".implode("','", $module_layouts)."');
-	for (i=0; i<layouts.length; i++) {
+	var layouts = new Array('".implode("','", $layout_files)."');
+	for (i=0; i<layouts.length; i++)
+	{
 		if (layouts[i] == layout_name) continue;
-		
-		var container = $('".$tmpl_container."_' + layouts[i] + '".$container_sx."');
-		if (container) container.getParent().setStyle('display', 'none');
-		
-		".( FLEXI_J30GE ? "
+
   	var container = jQuery('a[href=\"#attrib-".$tmpl_container."_' + layouts[i] + '\"]');
   	if (container) {container.parent().css('display', 'none');}
-  	" : "")."
   }
-	
+
 	// *** Show current container
-  var container = $('".$tmpl_container."_' + layout_name + '".$container_sx."');
-  if (container) container.getParent().setStyle('display', '');
-  
-	".( FLEXI_J30GE ? "
 	var container = jQuery('a[href=\"#attrib-".$tmpl_container."_' + layout_name + '\"]');
 	if (container) container.parent().css('display', '');
- 	" : "")."
 }
 
 ")."
 
 window.addEvent('domready', function(){
-	fc_getLayout($('jform_".($view=='field' ? "attribs_" : "params_").$_name."'));
+	fc_getLayout_".$_name."($('jform_".($view=='field' ? "attribs_" : "params_").$_name."'));
 });
 
 ";
@@ -373,7 +491,13 @@ window.addEvent('domready', function(){
 
 		// Create form element
 		return JHtml::_('select.groupedlist', $groups, $fieldname,
-			array('id' =>  $element_id, 'group.id' => 'id', 'list.attr' => $attribs, 'list.select' => $selected)
+			array(
+				'id' =>  $element_id,
+				'group.id' => 'id',
+				'list.attr' => $attribs,
+				'list.select' => $selected,
+				'option.attr' => 'attr'  // need to set the name we use for options attributes
+			)
 		);
 	}
 }
