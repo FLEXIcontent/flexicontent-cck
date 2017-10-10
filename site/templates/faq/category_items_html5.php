d9a008d939ebcfa7859da18d0a87c7e53a15236b
ggppdk <ggppdk@gmail.com>
2017-10-10 07:17:23 +0300

    [WIP] started some preperation work for infinite scroll at category layouts

--- site/templates/faq/category_items_html5.php
+++ site/templates/faq/category_items_html5.php
@@ -1,60 +1,64 @@
 <?php
 /**
- * HTML5 Template
- * @version 1.5 stable $Id: category_items_html5.php 0001 2012-09-23 14:00:28Z Rehne $
  * @package Joomla
  * @subpackage FLEXIcontent
  * @copyright (C) 2009 Emmanuel Danan - www.vistamedia.fr
  * @license GNU/GPL v2
  * 
- * FLEXIcontent is a derivative work of the excellent QuickFAQ component
- * @copyright (C) 2008 Christoph Lukes
- * see www.schlu.net for more information
+ * FLEXIcontent is a derivative work of QuickFAQ component @copyright (C) 2008 Christoph Lukes
  *
- * FLEXIcontent is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * FLEXIcontent is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  */
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
+
 // first define the template name
 $tmpl = $this->tmpl;
 $user = JFactory::getUser();
 
+$btn_class = 'btn';
+$tooltip_class = 'hasTooltip';
+
 // MICRODATA 'itemtype' for ALL items in the listing (this is the fallback if the 'itemtype' in content type / item configuration are not set)
 $microdata_itemtype_cat = $this->params->get( 'microdata_itemtype_cat', 'Article' );
-?>
 
-<?php
-	ob_start();
-	
-	// Form for (a) Text search, Field Filters, Alpha-Index, Items Total Statistics, Selectors(e.g. per page, orderby)
-	// If customizing via CSS rules or JS scripts is not enough, then please copy the following file here to customize the HTML too
-	include(JPATH_SITE.DS.'components'.DS.'com_flexicontent'.DS.'tmpl_common'.DS.'listings_filter_form_html5.php');
-	
-	$filter_form_html = trim(ob_get_contents());
-	ob_end_clean();
-	if ( $filter_form_html ) {
-		echo '<aside class="group">'."\n".$filter_form_html."\n".'</aside>';
-	}
-?>
 
-<div class="fcclear"></div>
+// Form for (a) Text search, Field Filters, Alpha-Index, Items Total Statistics, Selectors(e.g. per page, orderby)
+// If customizing via CSS rules or JS scripts is not enough, then please copy the following file here to customize the HTML too
 
-<?php
-$items = & $this->items;
+ob_start();
+include(JPATH_SITE.DS.'components'.DS.'com_flexicontent'.DS.'tmpl_common'.DS.'listings_filter_form_html5.php');
+$filter_form_html = trim(ob_get_contents());
+ob_end_clean();
+
+if ( $filter_form_html )
+{
+	echo '
+	<div class="fcclear"></div>
+	<aside class="group">
+		' . $filter_form_html . '
+	</aside>';
+}
 
 // -- Check matching items found
-if (!$items) {
+if (!$this->items)
+{
 	// No items exist
-	if ($this->getModel()->getState('limit')) {
+	if ($this->getModel()->getState('limit'))
+	{
 		// Not creating a category view without items
-		echo '<div class="noitems group">' . JText::_( 'FLEXI_NO_ITEMS_FOUND' ) . '</div>';
+		echo '
+		<div class="fcclear"></div>
+		<div class="noitems group">
+			' . JText::_( 'FLEXI_NO_ITEMS_FOUND' ) . '
+		</div>';
 	}
 	return;
 }
 
+$items = & $this->items;
+$count = count($items);
 
 // -- Decide whether to show the item edit options
 if ( $user->id ) :
