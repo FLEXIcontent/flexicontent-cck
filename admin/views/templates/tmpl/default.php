aae39338ce7aa808ed6fccef9fbfbff8f70cfbc2
Robert Stark <hello@iamrobert.com>
2017-10-13 13:40:14 +0800

    Update default.php
    Let me know if this works better:
    a) Made it responsive
    b) Added a help button accordion
    c) added colour for copy and delete icons
    
    Need to add the following .less
    ````
    .icon-delete {
    	color: @btnDangerBackgroundHighlight;
    }
    
    .icon-copy:before {
    	color: @btnSuccessBackgroundHighlight;
    }
    
    #howto_box {
    	display:inline-block;
    	width: auto;
    }
    
    
    @media (min-width: 768px) {
    	
    
    .adminlist td.col_tmpl_edit {
    	width:80px;
    }
    	}
    
    @media (max-width: 767px) {
    .view-templates {
    	.tmpl-icon-box  {
    		[class^="icon-"], [class*=" icon-"] {
    			margin-right: 0;
    		}
    	}
    		}
    }
    ````

--- admin/views/templates/tmpl/default.php
+++ admin/views/templates/tmpl/default.php
@@ -61,9 +61,9 @@ JFactory::getDocument()->addScriptDeclaration($js);
 
 $edit_layout = htmlspecialchars(JText::_('FLEXI_EDIT_LAYOUT_N_GLOBAL_PARAMETERS', true), ENT_QUOTES, 'UTF-8');
 $edit_icon         = '<span class="icon-edit"></span>';
-$editSingle_icon   = $edit_icon . ' <span class="icon-file"></span>';
+$editSingle_icon   = $edit_icon . ' <span class="icon-file hidden-phone"></span>';
 	//JHtml::image ( 'components/com_flexicontent/assets/images/page_single_edit.png', $edit_layout, ' style="min-width:22px;" class="'.$ico_class.' '.$tip_class.'" title="'.$edit_layout.'" ' );
-$editMultiple_icon = $edit_icon . ' <span class="icon-stack"></span>';
+$editMultiple_icon = $edit_icon . ' <span class="icon-stack hidden-phone"></span>';
 	//JHtml::image ( 'components/com_flexicontent/assets/images/page_multiple_edit.png', $edit_layout, ' style="min-width:22px;" class="'.$ico_class.' '.$tip_class.'" title="'.$edit_layout.'" '  );
 $editLayout_icon   = $editSingle_icon;
 $noEditLayout_icon = '<span class="icon-edit" title="' . JText::_( 'FLEXI_NOEDIT_LAYOUT', true ) .'"></span>';
@@ -76,65 +76,54 @@ $list_total_cols = 8;
 ?>
 
 <div id="flexicontent" class="flexicontent">
+<?php if (!empty( $this->sidebar)) : ?>
+	<div id="j-sidebar-container" class="span2">
+		<?php echo $this->sidebar; ?>
+	</div>
+	<div id="j-main-container" class="span10">
+<?php else : ?>
+	<div id="j-main-container">
+<?php endif;?>
 
 <form action="index.php" method="post" name="adminForm" id="adminForm">
 
-<?php if (!empty( $this->sidebar)) : ?>
-<div class="<?php echo FLEXI_J40GE ? 'row' : 'row-fluid'; ?>">
-	<div id="j-sidebar-container" class="span2 col-md-2">
-		<?php echo str_replace('type="button"', '', $this->sidebar); ?>
+<div id="outer_templates">
+
+<div class="row-fluid">
+<div class="span12 text-right">
+<button data-toggle="collapse" data-target="#howto_box" class="btn clearfix"><span class="icon-help"></span></button>
+</div></div>
+<div id="howto_box" class="collapse">
+	<div class="alert alert-info mt-20">
+		<h4 class="alert-heading">Configure display of your fields <span class="badge">item</span> view and <span class="badge">multi-item</span> views</h4>
+															<p class="alert-message"><span class="badge badge-warning">ITEM Layout</span> Select this in configuration of <span class="badge">types</span> and (optionally) in  <span class="badge">items</span></p>
+                                                            <p><span class="badge badge-warning">CATEGORY Layout</span> Select this in configuration of <span class="badge">categories / content lists</span> except for <strong>search view</strong></p>
 	</div>
-	<div class="span10 col-md-10">
-		<div id="j-main-container">
-<?php else : ?>
-<div class="<?php echo FLEXI_J40GE ? 'row' : 'row-fluid'; ?>">
-	<div class="span12 col-md-12">
-		<div id="j-main-container">
-<?php endif;?>
-
-
-
-<div id="outer_templates" style="float: left; min-width: 50%;">
-
-	<div id="howto_box" style="margin:10px 0% 24px 0%; width: 100%;">
-		<table class="fc-table-list" style="margin: 0; min-width: unset; width: 100%;">
-			<tr>
-				<th>Configure display of your fields <span class="badge">item</span> view and <span class="badge">multi-item</span> views</th>
-			</tr>
-			<tr>
-				<td><span class="label label-info">ITEM Layout</span> Select this in configuration of <span class="badge">types</span> and (optionally) in  <span class="badge">items</span></td>
-			</tr>
-			<tr>
-				<td><span class="label label-info">CATEGORY Layout</span> Select this in configuration of <span class="badge">categories / content lists</span> except for <b>search view</b></td>
-			</tr>
-		</table>
-	</div>
+</div>
 	
-	<!--
-	<?php $_class = FLEXI_J30GE ? ' btn' : ' fc_button fcsimple fcsmall'; ?>
-	<div class="btn-group" style="margin: 2px 32px 6px -3px; display:inline-block;">
-		<input type="button" id="fc_howto_box_btn" class="<?php echo $_class; ?> btn-warning" onclick="fc_toggle_box_via_btn('howto_box', this, 'btn-primary');" value="<?php echo JText::_( 'FLEXI_HOW_TO' ); ?>" />
-	</div>
-	-->
 	
-	<table id="adminListTableFCtemplates" class="adminlist fcmanlist" style="width: 100% !important;">
+	
+	
+
+<div class="block-flat no-padding-bottom">
+	<table id="adminListTableFCtemplates" class="adminlist fcmanlist">
 	
 	<thead>
 		<tr class="header">
-			<th><?php echo JText::_( 'FLEXI_NUM' ); ?></th>
-			<th class="left">
+			<th class="text-center hidden-tablet hidden-phone"><small><?php echo JText::_( 'FLEXI_NUM' ); ?></small></th>
+			<th class="text-center checker hidden-phone">
 				<input type="checkbox" name="checkall-toggle" id="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
 				<label for="checkall-toggle" class="green single"></label>
 			</th>
-			<th></th>
-			<th class="title" style="text-align:left;"><?php echo JText::_( 'FLEXI_TEMPLATE_NAME' ); ?></th>
-			<th colspan="2" style="text-align: left">
-				<?php echo JText::_( 'FLEXI_SINGLE_CONTENT' ); ?><br/>
-				<span class="label label-info">ITEM Layout</span>
+			<th class="col_tmpl_edit"></th>
+			<th class="text-left"><?php echo JText::_( 'FLEXI_TEMPLATE_NAME' ); ?></th>
+			<th colspan="2"  class="text-left">
+				<?php echo JText::_( 'FLEXI_SINGLE_CONTENT' ); ?><br class="hidden-phone">
+				<span class="label label-info hidden-phone">ITEM Layout</span>
 			</th>
-			<th colspan="2" style="text-align: left">
-				<?php echo JText::_( 'FLEXI_CONTENT_LISTS' ); ?><br/>
-				<span class="label label-info">CATEGORY Layout</span>
+			<th colspan="2"  class="text-left">
+				<?php echo JText::_( 'FLEXI_CONTENT_LISTS' ); ?><br class="hidden-phone">
+				<span class="label label-info hidden-phone">CATEGORY Layout</span>
 			</th>
 		</tr>
 	</thead>
@@ -161,22 +150,22 @@ $list_total_cols = 8;
 			$checked	= JHtml::_('grid.checkedout', $row, $i );
 			?>
 		<tr class="<?php echo "row$k"; ?>" id="<?php echo 'up-'.$row->name ?>">
-			<td>
+			<td class="text-center hidden-tablet hidden-phone">
 				<div class="adminlist-table-row"></div>
-				<?php echo $i+1; ?>
+				<small><?php echo $i+1; ?></small>
 			</td>
-			<td>
+			<td class="text-center checker hidden-phone">
 				<?php echo JHtml::_('grid.id', $i, $row->id); ?>
 				<label for="cb<?php echo $i; ?>" class="green single"></label>
 			</td>
-			<td class="right">
+			<td class="col_tmpl_edit text-right">
 				<?php if (!in_array($row->name, $basetemplates)) :?>
-					<span class="btn hasTooltip deletable-template" title="<?php echo JText::_('FLEXI_REMOVE_TEMPLATE', true); ?>" id="<?php echo 'del-' . $row->name ?>">
+					<span class="btn btn-small hasTooltip deletable-template" title="<?php echo JText::_('FLEXI_REMOVE_TEMPLATE', true); ?>" id="<?php echo 'del-' . $row->name ?>">
 						<?php echo $delTmpl_icon; ?>
 					</span>
 			 	<?php endif; ?>
 				<?php /*<a class="modal" onclick="javascript:;" rel="{handler: 'iframe', size: {x: 390, y: 210}}" href="<?php echo $copylink; ?>"><?php echo $copyTmpl_icon; ?></a>*/ ?>
-				<span class="btn hasTooltip" title="<?php echo JText::_('FLEXI_DUPLICATE_TEMPLATE', true); ?>" onclick="var url = jQuery(this).attr('data-href'); fc_tmpls_modal = fc_showDialog(url, 'fc_modal_popup_container', 0, 440, 300, fc_template_modal_close); return false;" data-href="<?php echo $copylink; ?>">
+				<span class="btn btn-small hasTooltip green" title="<?php echo JText::_('FLEXI_DUPLICATE_TEMPLATE', true); ?>" onclick="var url = jQuery(this).attr('data-href'); fc_tmpls_modal = fc_showDialog(url, 'fc_modal_popup_container', 0, 440, 300, fc_template_modal_close); return false;" data-href="<?php echo $copylink; ?>">
 					<?php echo $copyTmpl_icon; ?>
 				</span>
 			</td>
@@ -188,30 +177,30 @@ $list_total_cols = 8;
 					<span class="icon-user"></span><span class="badge"><?php echo JText::_('FLEXI_USER').' - '.JText::_('FLEXI_CREATED'); ?></span>
 				<?php endif; ?>
 			</td>
-			<td style="padding-right: 0;">
+			<td style="padding-right: 0;"  class="tmpl-icon-box">
 				<?php echo !empty($row->items)
-					? '<a class="btn hasTooltip" href="'.$itemlink.'" title="'.$edit_layout.'">'.$editSingle_icon.'</a>'
-					: '<span class="btn disabled">'.$noEditLayout_icon.'</span>'; ?>
+					? '<a class="btn btn-small hasTooltip" href="'.$itemlink.'" title="'.$edit_layout.'">'.$editSingle_icon.'</a>'
+					: '<span class="btn btn-small disabled">'.$noEditLayout_icon.'</span>'; ?>
 			</td>
 			<td style="text-align: left; padding-left: 0;">
 				<?php if ($defaulttitle_item): ?>
-					<span data-placement="top" class="<?php echo $tip_class; ?>" title="<?php echo flexicontent_html::getToolTip('', $description_item, 0, 1); ?>" >
-						<i class="icon-info"></i>
+					<span data-placement="top" class="<?php echo $tip_class; ?> hidden-phone" title="<?php echo flexicontent_html::getToolTip('', $description_item, 0, 1); ?>" >
+						<i class="small icon-info"></i>
 					</span>
-					<?php echo $defaulttitle_item; ?>
+				<span class="small hidden-phone"><?php echo $defaulttitle_item; ?></span>
 				<?php endif; ?>
 			</td>
-			<td style="padding-right: 0;">
+			<td style="padding-right: 0;" class="tmpl-icon-box">
 				<?php echo !empty($row->category)
-					? '<a class="btn hasTooltip" href="'.$catlink.'" title="'.$edit_layout.'">'.$editMultiple_icon.'</a>'
-					: '<span class="btn disabled">'.$noEditLayout_icon.'</span>'; ?>
+					? '<a class="btn btn-small hasTooltip" href="'.$catlink.'" title="'.$edit_layout.'">'.$editMultiple_icon.'</a>'
+					: '<span class="btn btn-small disabled">'.$noEditLayout_icon.'</span>'; ?>
 			</td>
-			<td style="text-align: left; padding-left: 0;">
+			<td style="text-align: left; padding-left: 0;" class="tmpl-icon-box">
 				<?php if ($defaulttitle_cat): ?>
-					<span data-placement="top" class="<?php echo $tip_class; ?>" title="<?php echo flexicontent_html::getToolTip('', $description_cat, 0, 1); ?>" >
-						<i class="icon-info"></i>
+					<span data-placement="top" class="<?php echo $tip_class; ?> hidden-phone" title="<?php echo flexicontent_html::getToolTip('', $description_cat, 0, 1); ?>" >
+						<i class="small icon-info"></i>
 					</span>
-					<?php echo $defaulttitle_cat; ?>
+					<span class="small hidden-phone"><?php echo $defaulttitle_cat; ?></span>
 				<?php endif; ?>
 			</td>
 		</tr>
@@ -221,59 +210,28 @@ $list_total_cols = 8;
 		endforeach;
 		?>
 	</tbody>
-
-	<tfoot>
-		<tr>
-			<td colspan="<?php echo $list_total_cols; ?>" style="text-align: left;">
-				<table class="admintable" style="margin: 0 auto !important;">
-					<tr>
-						<td>
-						<?php echo '<span class="btn">'.$copyTmpl_icon.'</span>'; ?>
-						</td>
-						<td>
-						<?php echo JText::_( 'FLEXI_DUPLICATE_TEMPLATE' ); ?>
-						</td>
-						<td>
-						<?php echo '<span class="btn">'.$edit_icon.'</span>'; ?>
-						</td>
-						<td>
-						<?php echo JText::_( 'FLEXI_EDIT_LAYOUT_N_GLOBAL_PARAMETERS' ); ?>
-						</td>
-					</tr>
-					<tr>
-						<td>
-						<?php echo '<span class="btn">'.$delTmpl_icon.'</span>'; ?>
-						</td>
-						<td>
-						<?php echo JText::_( 'FLEXI_REMOVE_TEMPLATE' ); ?>
-						</td>
-						<td>
-						<?php echo '<span class="btn disabled">'.$noEditLayout_icon.'</span>'; ?>
-						</td>
-						<td>
-						<?php echo JText::_( 'FLEXI_NOEDIT_LAYOUT' ); ?>
-						</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-	</tfoot>
-
 	</table>
-</div>
-
-
-	<div class="fcclear"></div>
+	</div>	
+	<div class="block-flat m20 hidden-phone">
+   <div class="row-fluid">
+              <div class="span6">
+              <p><?php echo $copyTmpl_icon; ?> <?php echo JText::_( 'FLEXI_DUPLICATE_TEMPLATE' ); ?></p>
+              <p><?php echo $edit_icon; ?> <?php echo JText::_( 'FLEXI_EDIT_LAYOUT' ); ?></p>
+           </div>   
+           <div class="span6">
+           <p><?php echo $delTmpl_icon; ?> <?php echo JText::_( 'FLEXI_REMOVE_TEMPLATE' ); ?></p>
+           <p><?php echo $noEditLayout_icon; ?>	<?php echo JText::_( 'FLEXI_NOEDIT_LAYOUT' ); ?></p>
+           </div>   
+           </div>
+ </div>
+	<div class="clear"></div>
 	
+</div><!-- #outer_templates -->
+
 	<input type="hidden" name="option" value="com_flexicontent" />
 	<input type="hidden" name="controller" value="templates" />
 	<input type="hidden" name="view" value="templates" />
 	<input type="hidden" name="task" value="" />
 	<?php echo JHtml::_( 'form.token' ); ?>
-
-		<!-- fc_perf -->
-		</div>  <!-- j-main-container -->
-	</div>  <!-- spanNN -->
-</div>  <!-- row -->
-</form>
+</form></div>
 </div><!-- #flexicontent end -->
