ce3a6e1ea4faa42e7a2613f8de743c81d2b61893
ggppdk <ggppdk@gmail.com>
2017-10-21 14:33:21 +0300

    Added options in show / hide configuration TAB of all modules for showing / hiding module based on clients: desktop, tablet, mobile

--- modules/mod_flexicategories/mod_flexicategories.php
+++ modules/mod_flexicategories/mod_flexicategories.php
@@ -1,18 +1,87 @@
 <?php
 /**
- * @package     Joomla.Site
- * @subpackage  mod_flexicategories
+ * @version 1.0 $Id: mod_flexicategories.php 1795 2013-10-23 00:08:42Z ggppdk $
+ * @package Joomla
+ * @subpackage FLEXIcategories Module
+ * @copyright (C) 2011 flexicontent.org
+ * @license GNU/GPL v3
+ * 
+ * Categories Module for flexicontent.
  *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * FLEXIcontent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 if (!defined('DS'))  define('DS',DIRECTORY_SEPARATOR);
 
-// Include the helper functions only once
-require_once __DIR__ . '/helper.php';
+
+// Decide whether to show module contents
+$app     = JFactory::getApplication();
+$jinput  = $app->input;
+$option  = $jinput->get('option', '', 'cmd');
+$view    = $jinput->get('view', '', 'cmd');
+
+
+// Show in view
+
+$_view   = $option=='com_flexicontent' ? $view : 'others';
+$show_in_views = $params->get('show_in_views', array());
+$show_in_views = !is_array($show_in_views) ? array($show_in_views) : $show_in_views;
+$views_show_mod = !count($show_in_views) || in_array($_view,$show_in_views);
+
+
+// Show in client
+
+$show_in_clients = $params->get('show_in_clients', array());
+$show_in_clients = !is_array($show_in_clients) ? array($show_in_clients) : $show_in_clients;
+
+if (count($show_in_clients) && count($show_in_clients) < 4)  // zero means not saved since we also have 1 extra value '__SAVED__'
+{
+	$mobileDetector = flexicontent_html::getMobileDetector();
+	$_client = $mobileDetector->isTablet()
+		? 'tablet'
+		: ($mobileDetector->isMobile() ? 'mobile' : 'desktop');
+
+	$clients_show_mod = in_array($_client, $show_in_clients);
+}
+else
+{
+	$clients_show_mod = true;
+}
+
+
+// Show via PHP rule
+
+$php_show_mod = $params->get('enable_php_rule', 0)
+	? eval($params->get('php_rule'))
+	: true;
+
+
+// Combine rules
+$show_mod = $params->get('combine_show_rules', 'AND') == 'AND'
+	? ($views_show_mod && $clients_show_mod && $php_show_mod)
+	: ($views_show_mod || $clients_show_mod || $php_show_mod);
+
+
+// ***
+// *** TERMINATE if not assigned to current view
+// ***
+if ( !$show_mod )  return;
+
+
+
+global $modfc_jprof;
+jimport('joomla.profiler.profiler');
+$modfc_jprof = new JProfiler();
+$modfc_jprof->mark('START: FLEXIcontent Categories Module');
+
+
+// include the helper only once
+require_once (dirname(__FILE__).DS.'helper.php');
 
 JLoader::register('JCategoryNode', JPATH_BASE . '/libraries/legacy/categories/categories.php');
 
--- modules/mod_flexifilter/mod_flexifilter.php
+++ modules/mod_flexifilter/mod_flexifilter.php
@@ -18,25 +18,53 @@
 defined('_JEXEC') or die('Restricted access');
 if (!defined('DS'))  define('DS',DIRECTORY_SEPARATOR);
 
+
 // Decide whether to show module contents
 $app     = JFactory::getApplication();
 $jinput  = $app->input;
 $option  = $jinput->get('option', '', 'cmd');
 $view    = $jinput->get('view', '', 'cmd');
 
-$_view   = $option=='com_flexicontent' ? $view : 'others';
 
+// Show in view
+
+$_view   = $option=='com_flexicontent' ? $view : 'others';
 $show_in_views = $params->get('show_in_views', array());
 $show_in_views = !is_array($show_in_views) ? array($show_in_views) : $show_in_views;
-$views_show_mod =!count($show_in_views) || in_array($_view,$show_in_views);
+$views_show_mod = !count($show_in_views) || in_array($_view,$show_in_views);
 
-if ($params->get('enable_php_rule', 0)) {
-	$php_show_mod = eval($params->get('php_rule'));
-	$show_mod = $params->get('combine_show_rules', 'AND')=='AND'
-		? ($views_show_mod && $php_show_mod)  :  ($views_show_mod || $php_show_mod);
-} else {
-	$show_mod = $views_show_mod;
+
+// Show in client
+
+$show_in_clients = $params->get('show_in_clients', array());
+$show_in_clients = !is_array($show_in_clients) ? array($show_in_clients) : $show_in_clients;
+
+if (count($show_in_clients) && count($show_in_clients) < 4)  // zero means not saved since we also have 1 extra value '__SAVED__'
+{
+	$mobileDetector = flexicontent_html::getMobileDetector();
+	$_client = $mobileDetector->isTablet()
+		? 'tablet'
+		: ($mobileDetector->isMobile() ? 'mobile' : 'desktop');
+
+	$clients_show_mod = in_array($_client, $show_in_clients);
 }
+else
+{
+	$clients_show_mod = true;
+}
+
+
+// Show via PHP rule
+
+$php_show_mod = $params->get('enable_php_rule', 0)
+	? eval($params->get('php_rule'))
+	: true;
+
+
+// Combine rules
+$show_mod = $params->get('combine_show_rules', 'AND') == 'AND'
+	? ($views_show_mod && $clients_show_mod && $php_show_mod)
+	: ($views_show_mod || $clients_show_mod || $php_show_mod);
 
 
 // ***
