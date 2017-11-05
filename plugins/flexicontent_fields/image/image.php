8d3e67d49fc1eb3bf40e550889aa317d8818fce4
ggppdk <ggppdk@gmail.com>
2017-11-05 07:03:43 +0200

    Better support for showing item description as a custom column in backend items manager, limit text displayed in column, and view full description in modal

--- plugins/flexicontent_fields/image/image.php
+++ plugins/flexicontent_fields/image/image.php
@@ -1139,6 +1139,7 @@ class plgFlexicontent_fieldsImage extends FCField
 		static $isMobile, $isTablet, $useMobile;
 		if ($initialized===null)
 		{
+			$initialized = 1;
 			$app       = JFactory::getApplication();
 			$document	 = JFactory::getDocument();
 			$option    = $app->input->get('option', '', 'cmd');
