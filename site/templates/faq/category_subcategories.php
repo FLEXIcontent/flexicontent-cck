229c5707f7fa2ef0f70ea5774604ea4fccfd9303
ggppdk <ggppdk@gmail.com>
2018-02-02 07:01:53 +0200

    Updated license in templates folder

--- site/templates/faq/category_subcategories.php
+++ site/templates/faq/category_subcategories.php
@@ -1,14 +1,9 @@
 <?php
 /**
- * @package Joomla
- * @subpackage FLEXIcontent
- * @copyright (C) 2009 Emmanuel Danan - www.vistamedia.fr
+ * @package FLEXIcontent
+ * @copyright (C) 2009-2018 Emmanuel Danan, Georgios Papadakis, Yannick Berges
+ * @author Emmanuel Danan, Georgios Papadakis, Yannick Berges, others, see contributor page
  * @license GNU/GPL v2
- * 
- * FLEXIcontent is a derivative work of QuickFAQ component @copyright (C) 2008 Christoph Lukes
- *
- * FLEXIcontent is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  */
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
