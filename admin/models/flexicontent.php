090af251fbbd35c6b91c95a8610d588df85c646e
ggppdk <ggppdk@gmail.com>
2018-03-03 08:35:43 +0200

    Remove no longer used file in minigallery CSS folder

--- admin/models/flexicontent.php
+++ admin/models/flexicontent.php
@@ -1196,7 +1196,8 @@ class FlexicontentModelFlexicontent extends JModelLegacy
 		$files = array(
 			JPATH_ADMINISTRATOR . '/components/com_flexicontent/helpers/html/users.php',
 			JPATH_ADMINISTRATOR . '/components/com_flexicontent/installation/install.mysql.nonutf8.sql',
-			JPATH_ADMINISTRATOR . '/components/com_flexicontent/installation/uninstall.mysql.nonutf8.sql'
+			JPATH_ADMINISTRATOR . '/components/com_flexicontent/installation/uninstall.mysql.nonutf8.sql',
+			JPATH_SITE . '/plugins/flexicontent_fields/minigallery/css/minigallery.php'
 		);
 		foreach ($files as $file)
 		{
