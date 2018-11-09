54f7c35826567cf8872462a1dcb9fc2e86c2b3c2
Berges Yannick <micker@free.fr>
2018-11-09 15:33:47 +0100

    Move select at first for textselect
    It more logical, at first user set if value already exist and after tip it

--- plugins/flexicontent_fields/text/text.php
+++ plugins/flexicontent_fields/text/text.php
@@ -5,7 +5,7 @@
  *
  * @author          Emmanuel Danan, Georgios Papadakis, Yannick Berges, others, see contributor page
  * @link            http://www.flexicontent.com
- * @copyright       Copyright © 2017, FLEXIcontent team, All Rights Reserved
+ * @copyright       Copyright Â© 2017, FLEXIcontent team, All Rights Reserved
  * @license         http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
@@ -376,8 +376,8 @@ class plgFlexicontent_fieldsText extends FCField
 				.$posttext;
 
 			$field->html[] = '
-				'.$text_field.'
 				'.($auto_value ? '' : $select_field).'
+				'.$text_field.'
 				'.($auto_value ? '<span class="fc-mssg-inline fc-info fc-nobgimage">' . JText::_('FLEXI_AUTO') . '</span>' : '').'
 				'.($use_ingroup || !$multiple || $auto_value ? '' : '
 				<div class="'.$input_grp_class.' fc-xpended-btns">
