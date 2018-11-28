a0113386940b043267f0a6d47e05e6e5afc6f41d
ggppdk <ggppdk@gmail.com>
2018-11-28 23:15:42 +0200

    Code revise for preprocessForm of field model, to make some fields readonly when needed, replacing similar code in view / template files

--- admin/tables/flexicontent_favourites.php
+++ admin/tables/flexicontent_favourites.php
@@ -5,7 +5,7 @@
  *
  * @author          Emmanuel Danan, Georgios Papadakis, Yannick Berges, others, see contributor page
  * @link            https://flexicontent.org
- * @copyright       Copyright © 2018, FLEXIcontent team, All Rights Reserved
+ * @copyright       Copyright Â© 2018, FLEXIcontent team, All Rights Reserved
  * @license         http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
