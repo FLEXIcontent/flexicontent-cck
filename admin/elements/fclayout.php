8041b3e8f9db3ec164c0cf8886d51fd0108c6eca
Georgios Papadakis <ggppdk@gmail.com>
2019-02-15 02:00:24 +0200

    Fix AJAX loaded layout parameters not always initializing properly when placed inside Tabs

--- admin/elements/fclayout.php
+++ admin/elements/fclayout.php
@@ -428,7 +428,7 @@ function fc_getLayout_".$_name."(el, initial)
 		panel_header = fc_tab_handle;
 
 		// Remove empty box, to allow index number to match correct Tab container
-		panel_header.closest('.tabberlive').children('.fc_empty_box').remove();
+		panel_header.closest('.tabberlive').children('.fc_empty_box,.field-spacer').remove();
 
 		panel = panel_header.closest('.tabberlive').children().eq( panel_header.parent().index() + 1 );
 		panel_id = panel.attr('id');
