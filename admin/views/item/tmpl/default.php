3fdfa74e8072bcc859244482ea2ddc88a3307a18
Berges Yannick <micker@free.fr>
2019-06-05 22:20:33 +0200

    hide comments field in form

--- admin/views/item/tmpl/default.php
+++ admin/views/item/tmpl/default.php
@@ -886,7 +886,7 @@ if ($this->item->type_id) {
 		<div class="fc_tabset_inner">
 
 			<?php
-			$hide_ifempty_fields = array('fcloadmodule', 'fcpagenav', 'toolbar');
+			$hide_ifempty_fields = array('fcloadmodule', 'fcpagenav', 'toolbar', 'comments');
 			$row_k = 0;
 
 			foreach ($this->fields as $field) :
