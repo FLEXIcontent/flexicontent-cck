991ff750d6eb85081fc2146bd8f51ce4bb204759
Georgios Papadakis <ggppdk@gmail.com>
2020-01-02 22:07:46 +0200

    Add the 'core-news' responsive template

---
+++ site/templates/core-news/category_category.php
@@ -0,0 +1,4 @@
+<?php
+	// If customizing via CSS rules or JS scripts is not enough, then please copy the following file here to customize the HTML too
+	include(JPATH_SITE.DS.'components'.DS.'com_flexicontent'.DS.'tmpl_common'.DS.'category_category.php');
+?>
