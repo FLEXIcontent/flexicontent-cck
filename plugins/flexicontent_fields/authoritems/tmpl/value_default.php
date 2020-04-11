48acccc1e278b67bff27a5993323d27364b01067
Georgios Papadakis <ggppdk@gmail.com>
2020-04-11 16:54:29 +0300

    Fix issue with SEF authored items URLs (without IDs) breaking if username includes capital letters

--- plugins/flexicontent_fields/authoritems/tmpl/value_default.php
+++ plugins/flexicontent_fields/authoritems/tmpl/value_default.php
@@ -1,6 +1,6 @@
 <?php
 
 $field->{$prop} =
-	'<a href="' . $author_links[$item->created_by] . '" itemprop="author">
+	'<a href="' . $author_links[$item->created_by] . '" itemprop="author" target="_blank">
 		' . JText::_('FLEXI_FIELD_AI_MORE_ITEMS_BY_THIS_AUTHOR') . '
 	</a>';
