51bb275e8dbd480fcd35e993706ce9ab1ab849a1
Georgios Papadakis <ggppdk@gmail.com>
2020-07-25 10:43:33 +0300

    Fixed Rokbox and widgetkit layouts of image gallery field not including title in the pop window

--- plugins/flexicontent_fields/image/tmpl/value_gallery_rokbox.php
+++ plugins/flexicontent_fields/image/tmpl/value_gallery_rokbox.php
@@ -19,7 +19,7 @@ foreach ($values as $n => $value)
 	if ($result === _FC_CONTINUE_) continue;
 	if ($result === _FC_BREAK_) break;
 
-	$title_attr = $desc_encoded ? $desc_encoded : $title_encoded;
+	$title_attr = $desc_encoded ? '<b>' . $title_encoded . '</b><br>' . $desc_encoded : $title_encoded;
 	$group_str = $group_name ? 'data-rokbox-album="'.$group_name.'"' : '';
 	$field->{$prop}[] = $pretext.
 		'<a style="'.$style.'" href="'.JUri::root(true).'/'.$srcl.'" rel="rokbox['.$wl.' '.$hl.']" '.$group_str.' title="' . $title_attr . '" class="fc_image_thumb" data-rokbox data-rokbox-caption="' . $title_attr . '">
