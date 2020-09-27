3624717cacea53adb3357c8bd5749ab4e8d0956c
Georgios Papadakis <ggppdk@gmail.com>
2020-09-27 08:51:34 +0300

    A fix for recently added code to use falang for displaying tags translations in item form

--- admin/defineconstants.php
+++ admin/defineconstants.php
@@ -118,7 +118,8 @@ if (FLEXI_J40GE)
 if (!defined('FLEXI_ACCESS'))  define('FLEXI_ACCESS'	, 0);  // TO BE REMOVED
 if (!defined('FLEXI_CACHE'))   define('FLEXI_CACHE'		, $params->get('advcache', 1));
 if (!defined('FLEXI_CACHE_TIME'))	define('FLEXI_CACHE_TIME'	, $params->get('advcache_time', 3600));
-if (!defined('FLEXI_FISH'))    define('FLEXI_FISH'		, ($params->get('flexi_fish', 0) && (JPluginHelper::isEnabled('system', 'falangdriver' ))) ? 1 : 0);
+if (!defined('FLEXI_FALANG'))     define('FLEXI_FALANG'		, (JPluginHelper::isEnabled('system', 'falangdriver') ? 1 : 0));
+if (!defined('FLEXI_FISH'))       define('FLEXI_FISH'		  , ($params->get('flexi_fish', 0) && FLEXI_FALANG ? 1 : 0));
 if (!defined('FLEXI_ONDEMAND'))		define('FLEXI_ONDEMAND'	, 1 );
 if (!defined('FLEXI_ITEMVIEW'))		define('FLEXI_ITEMVIEW'	, FLEXI_J16GE ? 'item' : 'items' );
 if (!defined('FLEXI_ICONPATH'))		define('FLEXI_ICONPATH'	, FLEXI_J16GE ? 'media/system/images/' : 'images/M_images/' );
