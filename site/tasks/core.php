f1e21d04d316813e7d8c8ee555844f9a803c69a6
Charley Wu <akunzai@gmail.com>
2021-07-17 08:18:57 +0800

    Make sure the `pageNum` start with 1 to avoid SQL errors

--- site/tasks/core.php
+++ site/tasks/core.php
@@ -110,8 +110,15 @@ class FlexicontentTasksCore
 		$lang    = substr($lang, 0,2);
 
 		$pageSize = $jinput->get('pageSize', 20, 'int');
-		$pageNum  = $jinput->get('pageNum', 1, 'int');		
-		$usesubs  = $jinput->get('usesubs', 1, 'int');		
+		$pageNum  = $jinput->get('pageNum', 1, 'int');
+		
+		// Make sure the pageNum start with 1 to avoid SQL errors
+		if ($pageNum < 1)
+		{
+			$pageNum = 1;
+		}
+		
+		$usesubs  = $jinput->get('usesubs', 1, 'int');
 
 		$min_word_len = $app->getUserState( $this->option.'.min_word_len', 0 );
 		$filtercat    = $cparams->get('filtercat', 0);      // Filter items using currently selected language
