e54dc2e397ede5683ff314353aa8e1a922e12248
Georgios Papadakis <ggppdk@gmail.com>
2021-08-25 12:43:15 +0300

    Add support for more image formats in fields

--- plugins/flexicontent_fields/file/file.php
+++ plugins/flexicontent_fields/file/file.php
@@ -101,7 +101,7 @@ class plgFlexicontent_fieldsFile extends FCField
 
 		$mediapath   = $cparams->get('media_path', 'components/com_flexicontent/medias');
 		$docspath    = $cparams->get('file_path', 'components/com_flexicontent/uploads');
-		$imageexts   = array('jpg','gif','png','bmp','jpeg');
+		$imageexts   = array('png', 'ico', 'gif', 'jpg', 'jpeg', 'webp', 'bmp');
 
 		$thumb_size_resizer = (int) $field->parameters->get('thumb_size_resizer', 0);
 		$thumb_size_default = (int) $field->parameters->get('thumb_size_default', 120);
@@ -1594,6 +1594,7 @@ class plgFlexicontent_fieldsFile extends FCField
 			case 'bmp':
 			case 'ico':
 			case 'jpeg':
+			case 'webp':
 				$file->icon = 'components/com_flexicontent/assets/images/mime-icon-16/image.png';
 			break;
 
