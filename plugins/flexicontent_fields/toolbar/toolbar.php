e468db1267f0eeda0a5ecd82afaa38a5cc65bcdd
Berges Yannick <yannick@com3elles.com>
2022-01-25 21:02:44 +0100

    Disable mail to function for j4

--- plugins/flexicontent_fields/toolbar/toolbar.php
+++ plugins/flexicontent_fields/toolbar/toolbar.php
@@ -5,7 +5,7 @@
  *
  * @author          Emmanuel Danan, Georgios Papadakis, Yannick Berges, others, see contributor page
  * @link            https://flexicontent.org
- * @copyright       Copyright © 2020, FLEXIcontent team, All Rights Reserved
+ * @copyright       Copyright ï¿½ 2020, FLEXIcontent team, All Rights Reserved
  * @license         http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
@@ -183,7 +183,7 @@ class plgFlexicontent_fieldsToolbar extends FCField
 		}
 
 		// email button
-		if ($display_email)
+		if ($display_email && !FLEXI_J40GE)
 		{
 			require_once(JPATH_SITE.DS.'components'.DS.'com_mailto'.DS.'helpers'.DS.'mailto.php');
 
