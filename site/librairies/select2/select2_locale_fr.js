feea1c899afa930fd2ab4a865c05e64464276d81
Alain Rihs <alainrihs@sunrise.ch>
2022-09-30 19:06:35 +0200

    Update select2_locale_fr.js
    Correction gramaticale

--- site/librairies/select2/select2_locale_fr.js
+++ site/librairies/select2/select2_locale_fr.js
@@ -7,7 +7,7 @@
     $.fn.select2.locales['fr'] = {
         formatMatches: function (matches) { return matches + " résultats sont disponibles, utilisez les flèches haut et bas pour naviguer."; },
         formatNoMatches: function () { return "Aucun résultat trouvé"; },
-        formatInputTooShort: function (input, min) { var n = min - input.length; return "Saisissez " + n + " caractère" + (n == 1? "" : "s") + " supplémentaire" + (n == 1? "" : "s") ; },
+        formatInputTooShort: function (input, min) { var n = min - input.length; return "Saisissez " + (n == 1? "au moins 1 caractère" : n) + (n == 1? "" : "caractères supplémentaires") ; },
         formatInputTooLong: function (input, max) { var n = input.length - max; return "Supprimez " + n + " caractère" + (n == 1? "" : "s"); },
         formatSelectionTooBig: function (limit) { return "Vous pouvez seulement sélectionner " + limit + " élément" + (limit == 1 ? "" : "s"); },
         formatLoadMore: function (pageNumber) { return "Chargement de résultats supplémentaires…"; },
