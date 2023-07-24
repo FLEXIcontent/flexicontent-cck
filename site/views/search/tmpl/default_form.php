41791f83012357be78c542c20bf78309e3e7b2e7
Berges Yannick <yannick@com3elles.com>
2023-07-24 14:08:22 +0200

    more cleaner space in result page

--- site/views/search/tmpl/default_form.php
+++ site/views/search/tmpl/default_form.php
@@ -19,7 +19,7 @@ $form_placement = (int) $this->params->get('form_placement', 0);
 
 if ($form_placement)
 {
-	$form_placement_class = $form_placement ? 'col-search span4 col-md-4' : 'top-search';
+	$form_placement_class = $form_placement ? 'col-search span3 col-md-3' : 'top-search';
 	$form_placement_style = $form_placement === 1 ? 'float: left; margin: 0 0 1rem 0;' : 'float: right; margin: 0 0 1rem 0;';
 }
 else
