511fa6a2ccda11fc9bf8173ed7b1b00511043471
Berges Yannick <yannick@com3elles.com>
2023-10-13 14:44:47 +0200

    Update mod_flexifilter.php

--- modules/mod_flexifilter/mod_flexifilter.php
+++ modules/mod_flexifilter/mod_flexifilter.php
@@ -111,7 +111,7 @@ $flexiparams = JComponentHelper::getParams('com_flexicontent');
 require_once (dirname(__FILE__).DS.'helper.php');
 
 // Get module's basic display parameters
-$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx') ?? '');/
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx') ?? '');
 $layout 				= $params->get('layout', 'default');
 
 // Workaround for legacy serialized form submit bug, posting empty radio as value 'on'
