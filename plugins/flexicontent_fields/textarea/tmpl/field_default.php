06c50fd0f1f2891e1df2fa5ee14f300751365eb6
Georgios Papadakis <ggppdk@gmail.com>
2024-05-01 14:49:51 +0300

    Check for null value in textarea form field layout

--- plugins/flexicontent_fields/textarea/tmpl/field_default.php
+++ plugins/flexicontent_fields/textarea/tmpl/field_default.php
@@ -19,7 +19,8 @@ foreach ($field->value as $i => $value)
       return;
     }
   }
-  if (!strlen($value ?? '') && !$use_ingroup && $n) continue;  // If at least one added, skip empty if not in field group
+	$value = $value ?? '';
+  if (!strlen($value) && !$use_ingroup && $n) continue;  // If at least one added, skip empty if not inside a field group
 
   $fieldname_n = $field->field_type == 'maintext' ? $fieldname : $fieldname.'['.$n.']';
   $elementid_n = $field->field_type == 'maintext' ? $elementid : $elementid.'_'.$n;
