9a522b73b5daff6bed3ceb3b2212bbaf2aa7f36f
Berges Yannick <yannick@com3elles.com>
2024-12-27 12:28:13 +0100

    rebuild joomla message in backend

--- site/classes/helpers/html.php
+++ site/classes/helpers/html.php
@@ -102,6 +102,9 @@ class flexicontent_html
 		<div id="system-message-container">
 <?php endif; ?>
 			<div id="fc_ajax_system_messages">
+		<?php if (version_compare(JVERSION, '4.0', 'ge')): ?>
+			<?php echo \Joomla\CMS\Factory::getApplication()->getRenderer('message')->render(null); ?>
+		<?php else: ?>
 			<?php if (is_array($msgsByType) && $msgsByType) : ?>
 				<?php foreach ($msgsByType as $type => $msgs) : ?>
 					<div class="alert <?php echo $alert_class[$type]; ?>">
@@ -115,6 +118,7 @@ class flexicontent_html
 					</div>
 				<?php endforeach; ?>
 			<?php endif; ?>
+		<?php endif; ?>
 			</div>
 <?php if ($add_containers) : ?>
 		</div>
