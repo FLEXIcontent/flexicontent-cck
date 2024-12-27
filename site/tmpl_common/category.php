39fa89eeec3f722f81b20e8124568f3694b322dd
Berges Yannick <yannick@com3elles.com>
2024-12-27 14:34:01 +0100

    Add tooltips in front

--- site/tmpl_common/category.php
+++ site/tmpl_common/category.php
@@ -1,5 +1,7 @@
 <?php
 defined( '_JEXEC' ) or die( 'Restricted access' );
+/** add tooltip */
+\Joomla\CMS\HTML\HTMLHelper::_('bootstrap.popover', '.hasTooltip', array('trigger' => 'click hover'));
 
 $page_classes  = '';
 $page_classes .= $this->pageclass_sfx ? ' page'.$this->pageclass_sfx : '';
