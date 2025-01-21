d8cb4efdce301b044ae7dc7d7ba34d01696bbc12
Berges Yannick <yannick@com3elles.com>
2025-01-21 16:17:37 +0100

    update defaut values

--- modules/mod_flexicontent/tmpl/news.php
+++ modules/mod_flexicontent/tmpl/news.php
@@ -38,10 +38,10 @@ $mod_do_hlight .= $hl_items_onnav == 2 || $hl_items_onnav == 3 ? ' mod_hl_hover'
 
 // Item Dimensions featured
 $inner_inline_css_feat = (int)$params->get($layout.'_inner_inline_css_feat', 0);
-$padding_top_bottom_feat = (int)$params->get($layout.'_padding_top_bottom_feat', 8);
-$padding_left_right_feat = (int)$params->get($layout.'_padding_left_right_feat', 12);
-$margin_top_bottom_feat = $params->get($layout.'_margin_left_right_feat', 4);
-$margin_left_right_feat = $params->get($layout.'_margin_left_right_feat', 4);
+$padding_top_bottom_feat = (int)$params->get($layout.'_padding_top_bottom_feat', '8px');
+$padding_left_right_feat = (int)$params->get($layout.'_padding_left_right_feat', '12px');
+$margin_top_bottom_feat = $params->get($layout.'_margin_left_right_feat', '2rem');
+$margin_left_right_feat = $params->get($layout.'_margin_left_right_feat', '2rem');
 $border_width_feat = (int)$params->get($layout.'_border_width_feat', 1);
 $item_column_mode_feat = (int)$params->get($layout.'_item_column_mode_feat', 1);// 0 column mode old, 1 grid minmax size
 $item_width_feat = $params->get($layout.'_item_width_feat', '200px');
@@ -50,10 +50,10 @@ $item_fit_feat = $params->get($layout.'_content_width_fit_feat', 'auto-fill');
 
 // Item Dimensions standard
 $inner_inline_css = (int)$params->get($layout.'_inner_inline_css', 0);
-$padding_top_bottom = (int)$params->get($layout.'_padding_top_bottom', 8);
-$padding_left_right = (int)$params->get($layout.'_padding_left_right', 12);
-$margin_top_bottom = $params->get($layout.'_margin_left_right', 4);
-$margin_left_right = $params->get($layout.'_margin_left_right', 4);
+$padding_top_bottom = (int)$params->get($layout.'_padding_top_bottom', '8px');
+$padding_left_right = (int)$params->get($layout.'_padding_left_right', '12px');
+$margin_top_bottom = $params->get($layout.'_margin_left_right', '2rem');
+$margin_left_right = $params->get($layout.'_margin_left_right', '2rem');
 $border_width = (int)$params->get($layout.'_border_width', 1);
 $item_column_mode_std = (int)$params->get($layout.'_item_column_mode_std', 1);// 0 column mode old, 1 grid minmax size
 $item_width_std = $params->get($layout.'_item_width_std', '200px');
