fe8d7daf8846746bbf9fdd9c30199a9a65b52c3b
Berges Yannick <yannick@com3elles.com>
2026-04-29 14:03:27 +0200

    allow webp and optimize display on flexicategory module

--- modules/mod_flexicategories/tmpl/default.php
+++ modules/mod_flexicategories/tmpl/default.php
@@ -8,7 +8,19 @@
  */
 
 defined('_JEXEC') or die;
+
+$cat_card_layout = $params->get('cat_image_float', 'left');
+
+// Conteneur selon le layout
+// top   → grille de cards (row / row-cols)
+// left  → liste verticale (ul standard)
+// right → liste verticale (ul standard)
+if ($cat_card_layout === 'top') {
+	$cols = $params->get('cat_card_cols', 3); // nombre de colonnes, paramètre optionnel
+	echo '<ul class="categories-module' . $moduleclass_sfx . ' list-unstyled row row-cols-1 row-cols-sm-2 row-cols-md-' . (int)$cols . ' g-3" style="list-style:none;padding-left:0">';
+} else {
+	echo '<ul class="categories-module' . $moduleclass_sfx . ' list-unstyled d-flex flex-column gap-2" style="list-style:none;padding-left:0">';
+}
 ?>
-<ul class="categories-module<?php echo $moduleclass_sfx; ?>">
 <?php require \Joomla\CMS\Helper\ModuleHelper::getLayoutPath('mod_flexicategories', $params->get('layout', 'default') . '_items'); ?>
 </ul>
--- modules/mod_flexicategories/tmpl/default_items.php
+++ modules/mod_flexicategories/tmpl/default_items.php
@@ -10,174 +10,289 @@
 defined('_JEXEC') or die;
 
 $app = \Joomla\CMS\Factory::getApplication();
-//$cparams = clone(\Joomla\CMS\Component\ComponentHelper::getComponent('com_flexicontent')->params);     // Get the COMPONENT only parameters
 global $globalcats;
 
-// category image params
-$show_cat_image = $params->get('show_description_image', 0);  // we use different name for variable
-$cat_image_source = $params->get('cat_image_source', 2); // 0: extract, 1: use param, 2: use both
-$cat_link_image = $params->get('cat_link_image', 1);
-$cat_image_method = $params->get('cat_image_method', 1);
-$cat_image_width = $params->get('cat_image_width', 24);
-$cat_image_height = $params->get('cat_image_height', 24);
-$cat_image_float = $params->get('cat_image_float', 'left');
-$cat_default_image = $params->get('cat_default_image','');
+// ---------------------------------------------------------------------------
+// Paramètres image catégorie
+// ---------------------------------------------------------------------------
+$show_cat_image    = $params->get('show_description_image', 0);
+$cat_image_source  = $params->get('cat_image_source', 2);
+$cat_link_image    = $params->get('cat_link_image', 1);
+$cat_image_method  = $params->get('cat_image_method', 1);
+$cat_image_width   = $params->get('cat_image_width', 24);
+$cat_image_height  = $params->get('cat_image_height', 24);
+$cat_default_image = $params->get('cat_default_image', '');
+$cat_image_webp    = (bool) $params->get('cat_image_webp', 0);
+
+// Layout card : top | left | right
+$cat_card_layout = $params->get('cat_image_float', 'left');
 
 if ($show_cat_image)
 {
-	$joomla_image_path = $app->getCfg('image_path',  '');
-	$joomla_image_url  = str_replace (DS, '/', $joomla_image_path);
-	$joomla_image_path = $joomla_image_path ? $joomla_image_path.DS : '';
-	$joomla_image_url  = $joomla_image_url  ? $joomla_image_url.'/' : '';
+	$joomla_image_path = $app->getCfg('image_path', '');
+	$joomla_image_url  = str_replace(DS, '/', $joomla_image_path);
+	$joomla_image_path = $joomla_image_path ? $joomla_image_path . DS : '';
+	$joomla_image_url  = $joomla_image_url  ? $joomla_image_url  . '/' : '';
 
-	$h		= '&amp;h=' . $cat_image_height;
-	$w		= '&amp;w=' . $cat_image_width;
-	$aoe	= '&amp;aoe=1';
-	$q		= '&amp;q=95';
-	$ar 	= '&amp;ar=x';
-	$zc		= $cat_image_method ? '&amp;zc=' . $cat_image_method : '';
-	$phpThumbURL = \Joomla\CMS\Uri\Uri::base(true).'/components/com_flexicontent/librairies/phpthumb/phpThumb.php?src=';
+	$h   = '&amp;h=' . $cat_image_height;
+	$w   = '&amp;w=' . $cat_image_width;
+	$aoe = '&amp;aoe=1';
+	$q   = '&amp;q=95';
+	$ar  = '&amp;ar=x';
+	$zc  = $cat_image_method ? '&amp;zc=' . $cat_image_method : '';
+
+	$phpThumbURL = \Joomla\CMS\Uri\Uri::base(true)
+		. '/components/com_flexicontent/librairies/phpthumb/phpThumb.php?src=';
 }
 
+// ---------------------------------------------------------------------------
+// Helper : encode les espaces segment par segment
+// ---------------------------------------------------------------------------
+$encodeSrc = static function ($src) {
+	return implode('/', array_map(static function ($seg) {
+		return str_replace(
+			['%2E', '%2D', '%5F', '%7E'],
+			['.',   '-',   '_',   '~'],
+			rawurlencode($seg)
+		);
+	}, explode('/', $src)));
+};
 
+// ---------------------------------------------------------------------------
+// Helper : construit une URL phpThumb
+// ---------------------------------------------------------------------------
+$buildThumbUrl = static function (
+	$src, $wParam, $hParam, $aoe, $q, $ar, $zc, $ext, $forceWebp = false
+) use ($phpThumbURL, $encodeSrc) {
+	$safeSrc = $encodeSrc($src);
+	if ($forceWebp) {
+		$f = '&amp;f=webp';
+	} elseif (in_array($ext, ['png','gif','jpeg','jpg','webp','wbmp','bmp','ico'])) {
+		$f = '&amp;f=' . $ext;
+	} else {
+		$f = '';
+	}
+	return $phpThumbURL . $safeSrc . $wParam . $hParam . $aoe . $q . $ar . $zc . $f;
+};
 
-// ***
-// *** Create thumbnail of default category image
-// ***
+// ---------------------------------------------------------------------------
+// Helper : construit le tag <picture> ou <img>
+// ---------------------------------------------------------------------------
+$buildImgTag = static function ($urlNative, $urlWebp, $alt, $layout, $imgWidth, $imgHeight) {
+	$safeAlt = htmlspecialchars($alt, ENT_QUOTES, 'UTF-8');
 
-if ($cat_default_image)
+	if ($layout === 'top') {
+		$imgClass = 'img-fluid w-100';
+		$style    = 'object-fit:cover;height:' . $imgHeight . 'px;';
+	} else {
+		$imgClass = 'img-fluid flex-shrink-0';
+		$style    = 'width:' . $imgWidth . 'px;height:' . $imgHeight . 'px;object-fit:cover;';
+	}
+
+	$imgTag = '<img src="' . $urlNative . '" class="' . $imgClass . '"'
+		. ' alt="' . $safeAlt . '" title="' . $safeAlt . '"'
+		. ' style="' . $style . '">';
+
+	if ($urlWebp) {
+		return '<picture>'
+			. '<source type="image/webp" srcset="' . $urlWebp . '">'
+			. $imgTag
+			. '</picture>';
+	}
+	return $imgTag;
+};
+
+// ---------------------------------------------------------------------------
+// Image par défaut
+// ---------------------------------------------------------------------------
+$default_img_native = '';
+$default_img_webp   = '';
+
+if ($cat_default_image && $show_cat_image)
 {
-	$src = \Joomla\CMS\Uri\Uri::base(true) ."/". $joomla_image_url . $cat_default_image;
-	
-	$ext = strtolower(pathinfo($src, PATHINFO_EXTENSION));
-	$f = in_array( $ext, array('png', 'gif', 'jpeg', 'jpg', 'webp', 'wbmp', 'bmp', 'ico') ) ? '&amp;f='.$ext : '';
-	$conf	= $w . $h . $aoe . $q . $ar . $zc . $f;
-	
-	$default_image = $phpThumbURL.$src.$conf;
-	$default_image = '<img src="'.$default_image.'" alt="%s" title="%s" style="float: ' . $cat_image_float . '" />';
-}
-else
-{
-	$default_image = '';
+	$srcDefault = \Joomla\CMS\Uri\Uri::base(true) . '/' . $joomla_image_url . $cat_default_image;
+	$extDefault = strtolower(pathinfo($srcDefault, PATHINFO_EXTENSION));
+	$default_img_native = $buildThumbUrl($srcDefault, $w, $h, $aoe, $q, $ar, $zc, $extDefault, false);
+	$default_img_webp   = $cat_image_webp
+		? $buildThumbUrl($srcDefault, $w, $h, $aoe, $q, $ar, $zc, $extDefault, true)
+		: '';
 }
 
-
-// ***
-// *** Other parameters
-// ***
-
+// ---------------------------------------------------------------------------
+// Autres paramètres
+// ---------------------------------------------------------------------------
 $show_empty_cats = (int) $params->get('show_empty_cats', 1);
-$numitems = (int) $params->get('numitems', 0);
-$item_heading = (int) $params->get('item_heading', 4);
+$numitems        = (int) $params->get('numitems', 0);
+$item_heading    = (int) $params->get('item_heading', 4);
 
-
-// ***
-// *** Loop through categories and display them according to their depth level
-// ***
-
+// ---------------------------------------------------------------------------
+// Boucle catégories
+// ---------------------------------------------------------------------------
 foreach ($list as $cat) :
 
 	if (isset($globalcats[$cat->id]) && isset($globalcats[$cat->id]->totalitems))
 	{
 		$totalitems = (int) $globalcats[$cat->id]->totalitems;
-		if (!$show_empty_cats && $totalitems === 0)
-		{
-			continue;
-		}
+		if (!$show_empty_cats && $totalitems === 0) { continue; }
 	}
 	else
 	{
 		$totalitems = 0;
 	}
-	
-	$cat->slug = $cat->id.':'.$cat->alias;
-	$cat->link = \Joomla\CMS\Router\Route::_( FlexicontentHelperRoute::getCategoryRoute($cat->slug) );
 
-	$image = '';
-	$src = '';
+	$cat->slug = $cat->id . ':' . $cat->alias;
+	$cat->link = \Joomla\CMS\Router\Route::_(FlexicontentHelperRoute::getCategoryRoute($cat->slug));
+
+	$imgNative = '';
+	$imgWebp   = '';
+	$src       = '';
 
 	if ($show_cat_image)
 	{
-		if (!is_object($cat->params))
-		{
+		if (!is_object($cat->params)) {
 			$cat->params = new \Joomla\Registry\Registry($cat->params);
 		}
-		
-		$cat->image = $cat->params->get('image');
-		$_parts  = explode('#', $cat->image ?? '');
-	    $cat->image         = $_parts[0];
-		$cat->introtext = & $cat->description;
-		$cat->fulltext = '';
-		
-		if ( $cat_image_source && $cat->image && file_exists( JPATH_SITE .DS. $joomla_image_path . $cat->image ) )
+		$cat->image     = $cat->params->get('image');
+		$_parts         = explode('#', $cat->image ?? '');
+		$cat->image     = $_parts[0];
+		$cat->introtext = &$cat->description;
+		$cat->fulltext  = '';
+		$ext            = '';
+
+		if ($cat_image_source && $cat->image && file_exists(JPATH_SITE . DS . $joomla_image_path . $cat->image))
 		{
-			$src = \Joomla\CMS\Uri\Uri::base(true) ."/". $joomla_image_url . $cat->image;
-			
+			$src = \Joomla\CMS\Uri\Uri::base(true) . '/' . $joomla_image_url . $cat->image;
 			$ext = strtolower(pathinfo($src, PATHINFO_EXTENSION));
-			$f = in_array( $ext, array('png', 'gif', 'jpeg', 'jpg', 'webp', 'wbmp', 'bmp', 'ico') ) ? '&amp;f='.$ext : '';
-			$conf	= $w . $h . $aoe . $q . $ar . $zc . $f;
 		}
-		else if ( $cat_image_source!=1 && $src = flexicontent_html::extractimagesrc($cat) )
+		elseif ($cat_image_source != 1 && $extracted = flexicontent_html::extractimagesrc($cat))
 		{
-			$ext = strtolower(pathinfo($src, PATHINFO_EXTENSION));
-			$f = in_array( $ext, array('png', 'gif', 'jpeg', 'jpg', 'webp', 'wbmp', 'bmp', 'ico') ) ? '&amp;f='.$ext : '';
-			$conf	= $w . $h . $aoe . $q . $ar . $zc . $f;
-			
-			$base_url = (!preg_match("#^http|^https|^ftp|^/#i", $src)) ?  \Joomla\CMS\Uri\Uri::base(true).'/' : '';
-			$src = $base_url.$src;
-		}
-		
-		if ($src)
-		{
-			$image = '<img src="'.$phpThumbURL.$src.$conf.'" alt="'.$cat->title.'" title="'.$cat->title.'" style="float: ' . $cat_image_float . '" />';
-		}
-		elseif ($default_image)
-		{
-			$image = sprintf($default_image, $cat->title, $cat->title);
+			$ext      = strtolower(pathinfo($extracted, PATHINFO_EXTENSION));
+			$base_url = (!preg_match('#^http|^https|^ftp|^/#i', $extracted))
+				? \Joomla\CMS\Uri\Uri::base(true) . '/' : '';
+			$src = $base_url . $extracted;
 		}
 
-		// Create image display and its link if category image is non-empty
-		if ($cat_link_image && $image)
-		{
-			$image = '<a href="'.$cat->link.'">'.$image.'</a>';
+		if ($src) {
+			$imgNative = $buildThumbUrl($src, $w, $h, $aoe, $q, $ar, $zc, $ext, false);
+			$imgWebp   = $cat_image_webp
+				? $buildThumbUrl($src, $w, $h, $aoe, $q, $ar, $zc, $ext, true)
+				: '';
+		} elseif ($default_img_native) {
+			$imgNative = $default_img_native;
+			$imgWebp   = $default_img_webp;
 		}
 	}
 
-	$cat->image = $image;
-	$cat->image_src = $src;  // Also add image category URL for developers
-	?>
+	$cat->image_src = $src;
 
-	<li <?php if ($_SERVER['REQUEST_URI'] == $cat->link) echo ' class="active"';?>> <?php $levelup = $cat->level - $startLevel - 1; ?>
+	$safeTitle = htmlspecialchars($cat->title, ENT_QUOTES, 'UTF-8');
+	$levelup   = $cat->level - $startLevel - 1;
+	$hLevel    = $item_heading + $levelup;
+	$isActive  = ($_SERVER['REQUEST_URI'] == $cat->link);
 
-		<?php if ($cat->image):?>
-			<div class="mod_fccats_catimg_block">
-				<?php echo $cat->image; ?>
+	// Tag image brut (sans lien)
+	$imgHtml = ($imgNative && $show_cat_image)
+		? $buildImgTag($imgNative, $imgWebp, $cat->title, $cat_card_layout, $cat_image_width, $cat_image_height)
+		: '';
+
+	// Image avec lien optionnel
+	$imgLinked = ($cat_link_image && $imgHtml)
+		? '<a href="' . $cat->link . '" tabindex="-1">' . $imgHtml . '</a>'
+		: $imgHtml;
+
+	// Compteur
+	$counter = ($numitems && isset($globalcats[$cat->id]))
+		? ' <span class="badge bg-secondary fw-normal">' . $totalitems . '</span>'
+		: '';
+
+	// Description
+	$descHtml = '';
+	if ($params->get('show_description', 0)) {
+		$descHtml = '<p class="card-text small text-muted mt-1 mb-0">'
+			. \Joomla\CMS\HTML\HTMLHelper::_('content.prepare', $cat->description, $cat->getParams(), 'mod_flexicategories.content')
+			. '</p>';
+	}
+
+	// Titre
+	$titleHtml = '<h' . $hLevel . ' class="card-title h6 mb-0">'
+		. '<a href="' . $cat->link . '" class="text-decoration-none stretched-link">'
+		. $safeTitle . $counter
+		. '</a>'
+		. '</h' . $hLevel . '>';
+?>
+
+<?php if ($cat_card_layout === 'top') : ?>
+	<?php /* ============================================================
+		LAYOUT TOP : card verticale, image pleine largeur en haut
+		Le <li> est une colonne Bootstrap (col) grâce au row du parent
+	   ============================================================ */ ?>
+	<li class="col<?php echo $isActive ? ' active' : ''; ?>" style="list-style:none">
+		<div class="card h-100 border shadow-sm overflow-hidden">
+			<?php if ($imgLinked) : ?>
+				<div class="mod_fccats_catimg_block">
+					<?php echo $imgLinked; ?>
+				</div>
+			<?php endif; ?>
+			<div class="card-body d-flex flex-column position-relative">
+				<?php echo $titleHtml; ?>
+				<?php echo $descHtml; ?>
 			</div>
-		<?php endif; ?>
-
-		<h<?php echo $item_heading + $levelup; ?>>
-			<a href="<?php echo $cat->link; ?>">
-			<?php echo $cat->title;?>
-				<?php if ($numitems && isset($globalcats[$cat->id])) : ?>
-					(<?php echo $totalitems; ?>)
-				<?php endif; ?>
-			</a>
-		</h<?php echo $item_heading + $levelup; ?>>
-
-		<?php if ($params->get('show_description', 0)) : ?>
-			<?php echo \Joomla\CMS\HTML\HTMLHelper::_('content.prepare', $cat->description, $cat->getParams(), 'mod_flexicategories.content'); ?>
-		<?php endif; ?>
-		<?php if (
-			$params->get('show_children', 0)
-			&& (($params->get('maxlevel', 0) == 0) || ($params->get('maxlevel') >= ($cat->level - $startLevel)))
-			&& count($cat->getChildren())) :
-		?>
-			<?php echo '<ul>'; ?>
-			<?php $temp = $list; ?>
-			<?php $list = $cat->getChildren(); ?>
-			<?php require \Joomla\CMS\Helper\ModuleHelper::getLayoutPath('mod_flexicategories', $params->get('layout', 'default') . '_items'); ?>
-			<?php $list = $temp; ?>
-			<?php echo '</ul>'; ?>
-		<?php endif; ?>
+		</div>
 	</li>
+
+<?php elseif ($cat_card_layout === 'right') : ?>
+	<?php /* ============================================================
+		LAYOUT RIGHT : card horizontale, image à droite
+	   ============================================================ */ ?>
+	<li class="<?php echo $isActive ? 'active ' : ''; ?>" style="list-style:none">
+		<div class="card border shadow-sm overflow-hidden">
+			<div class="d-flex flex-row-reverse align-items-stretch position-relative">
+				<?php if ($imgLinked) : ?>
+					<div class="mod_fccats_catimg_block flex-shrink-0">
+						<?php echo $imgLinked; ?>
+					</div>
+				<?php endif; ?>
+				<div class="card-body py-2 px-3 d-flex flex-column justify-content-center">
+					<?php echo $titleHtml; ?>
+					<?php echo $descHtml; ?>
+				</div>
+			</div>
+		</div>
+	</li>
+
+<?php else : ?>
+	<?php /* ============================================================
+		LAYOUT LEFT (défaut) : card horizontale, image à gauche
+	   ============================================================ */ ?>
+	<li class="<?php echo $isActive ? 'active ' : ''; ?>" style="list-style:none">
+		<div class="card border shadow-sm overflow-hidden">
+			<div class="d-flex flex-row align-items-stretch position-relative">
+				<?php if ($imgLinked) : ?>
+					<div class="mod_fccats_catimg_block flex-shrink-0">
+						<?php echo $imgLinked; ?>
+					</div>
+				<?php endif; ?>
+				<div class="card-body py-2 px-3 d-flex flex-column justify-content-center">
+					<?php echo $titleHtml; ?>
+					<?php echo $descHtml; ?>
+				</div>
+			</div>
+		</div>
+	</li>
+
+<?php endif; ?>
+
+	<?php if (
+		$params->get('show_children', 0)
+		&& (($params->get('maxlevel', 0) == 0) || ($params->get('maxlevel') >= ($cat->level - $startLevel)))
+		&& count($cat->getChildren())
+	) : ?>
+		<ul class="list-unstyled ps-3 mt-2 d-flex flex-column gap-2">
+		<?php $temp = $list; ?>
+		<?php $list = $cat->getChildren(); ?>
+		<?php require \Joomla\CMS\Helper\ModuleHelper::getLayoutPath('mod_flexicategories', $params->get('layout', 'default') . '_items'); ?>
+		<?php $list = $temp; ?>
+		</ul>
+	<?php endif; ?>
+
 <?php endforeach; ?>
